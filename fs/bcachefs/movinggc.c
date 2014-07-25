ce445fd5dc4a52ccbf0b5b19ee4e121f27ac21c2
Slava Pestov <sviatoslavpestov@gmail.com>
2014-07-25 15:04:36 -0700

    bcachefs: disable tiering and moving GC PD controllers until they're fixed
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- fs/bcachefs/movinggc.c
+++ fs/bcachefs/movinggc.c
@@ -32,6 +32,7 @@ static void __update_gc_rate(struct cache *ca)
 	bch_pd_controller_update(&ca->moving_gc_pd,
 				 target << 9,
 				 ca->buckets_free << bucket_bits);
+	ca->moving_gc_pd.rate.rate = NSEC_PER_MSEC;
 }
 
 static void update_gc_rate(struct work_struct *work)
