df6e904845d5600b682905df01e9722eddf43546
Kent Overstreet <kent.overstreet@gmail.com>
2015-07-06 14:59:07 -0700

    Don't show idle kworkers on alt-sysrq-t

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -220,9 +220,10 @@ extern void proc_sched_set_task(struct task_struct *p);
 #define TASK_PARKED		512
 #define TASK_NOLOAD		1024
 #define TASK_NEW		2048
-#define TASK_STATE_MAX		4096
+#define TASK_IDLE_WORKER	4096
+#define TASK_STATE_MAX		8192
 
-#define TASK_STATE_TO_CHAR_STR "RSDTtXZxKWPNn"
+#define TASK_STATE_TO_CHAR_STR "RSDTtXZxKWPNIn"
 
 extern char ___assert_task_state[1 - 2*!!(
 		sizeof(TASK_STATE_TO_CHAR_STR)-1 != ilog2(TASK_STATE_MAX)+1)];
