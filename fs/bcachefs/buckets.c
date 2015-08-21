a7473c759d6ebceb1715a75c28322248841f0b59
Kent Overstreet <kent.overstreet@gmail.com>
2015-08-21 00:15:10 -0800

    bcachefs: trivial cleanup
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- fs/bcachefs/buckets.c
+++ fs/bcachefs/buckets.c
@@ -156,30 +156,25 @@ void bch_mark_data_bucket(struct cache_set *c, struct cache *ca,
 			  bool dirty, bool gc)
 {
 	struct bucket_mark old, new;
-	unsigned bucket_gen;
-	bool stale;
-
-	unsigned long r = PTR_BUCKET_NR(c, k, i);
+	unsigned long bucket_nr = PTR_BUCKET_NR(c, k, i);
 	unsigned gen = PTR_GEN(k, i);
 
-	bucket_cmpxchg(&ca->buckets[r], old, new, ({
+	bucket_cmpxchg(&ca->buckets[bucket_nr], old, new, ({
 		/*
 		 * cmpxchg() only implies a full barrier on success, not
 		 * failure, so we need a read barrier on all iterations
 		 */
 		smp_rmb();
 
-		/*
-		 * Check this after reading bucket mark to guard against
-		 * GC starting between when we check gc_cur_key and when
-		 * the GC zeroes out marks
-		 */
 		if (!gc && gc_will_visit_key(c, k))
 			return;
 
-		bucket_gen = ca->bucket_gens[r];
-		stale = gen_after(bucket_gen, gen);
-		if (stale)
+		/*
+		 * Check this after reading bucket mark to guard against
+		 * the allocator invalidating a bucket after we've already
+		 * checked the gen
+		 */
+		if (gen_after(ca->bucket_gens[bucket_nr], gen))
 			return;
 
 		BUG_ON(old.is_metadata);
