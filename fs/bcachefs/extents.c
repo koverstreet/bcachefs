e235b4152338677fc2b9b4465353d09e4c60e3a0
Kent Overstreet <kent.overstreet@gmail.com>
2015-08-21 00:14:21 -0800

    bcachefs: KEY_DELETED() == true when KEY_SIZE() == 0
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- fs/bcachefs/extents.c
+++ fs/bcachefs/extents.c
@@ -438,6 +438,10 @@ bool bch_cut_front(const struct bkey *where, struct bkey *k)
 
 	BUG_ON(len > KEY_SIZE(k));
 	SET_KEY_SIZE(k, len);
+
+	if (!len)
+		SET_KEY_DELETED(k, true);
+
 	return true;
 }
 
@@ -459,6 +463,10 @@ bool bch_cut_back(const struct bkey *where, struct bkey *k)
 
 	BUG_ON(len > KEY_SIZE(k));
 	SET_KEY_SIZE(k, len);
+
+	if (!len)
+		SET_KEY_DELETED(k, true);
+
 	return true;
 }
 
@@ -722,6 +730,7 @@ static bool bch_extent_insert_fixup(struct btree_keys *b,
 			 * the binary search tree
 			 */
 			SET_KEY_SIZE(k, 0);
+			SET_KEY_DELETED(k, true);
 			if (!bkey_written(b, k))
 				SET_KEY_OFFSET(k, KEY_START(insert));
 
