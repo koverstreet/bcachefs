ec3a4e0c17826b749d790d936a00250c00720818
Kent Overstreet <kent.overstreet@gmail.com>
2015-09-24 21:20:57 -0800

    genradix: handle non power of two sized types

--- include/linux/generic-radix-tree.h
+++ include/linux/generic-radix-tree.h
@@ -5,7 +5,10 @@
  * interior nodes.
  */
 
+#include <asm/page.h>
+#include <linux/bug.h>
 #include <linux/kernel.h>
+#include <linux/log2.h>
 
 struct genradix_node;
 
@@ -26,6 +29,14 @@ struct __genradix {
 			.depth = 0,				\
 		}						\
 	}
+
+/*
+ * We use a 0 size array to stash the type we're storing without taking any
+ * space at runtime - then the various accessor macros can use typeof() to get
+ * to it for casts/sizeof - we also force the alignment so that storing a type
+ * with a ridiculous alignment doesn't blow up the alignment or size of the
+ * genradix.
+ */
 
 #define DECLARE_GENRADIX_TYPE(_name, _type)			\
 struct _name {							\
@@ -53,11 +64,26 @@ void __genradix_free(struct __genradix *);
 
 void *__genradix_ptr(struct __genradix *, size_t);
 
+static inline size_t __idx_to_offset(size_t idx, size_t size)
+{
+	BUILD_BUG_ON(size > PAGE_SIZE);
+
+	if (!is_power_of_2(size)) {
+		size_t objs_per_page = PAGE_SIZE / size;
+
+		return (idx / objs_per_page) * PAGE_SIZE +
+			(idx % objs_per_page) * size;
+	} else {
+		return idx * size;
+	}
+}
+
 /* Returns a pointer to element at @_idx */
 #define genradix_ptr(_radix, _idx)				\
 	((typeof((_radix)->type[0]) *)				\
 	 __genradix_ptr(&(_radix)->tree,			\
-			(_idx) * sizeof((_radix)->type[0])))
+			__idx_to_offset((_idx),			\
+					sizeof((_radix)->type[0]))))
 
 void *__genradix_ptr_alloc(struct __genradix *, size_t, gfp_t);
 
@@ -65,5 +91,6 @@ void *__genradix_ptr_alloc(struct __genradix *, size_t, gfp_t);
 #define genradix_ptr_alloc(_radix, _idx, _gfp)			\
 	((typeof((_radix)->type[0]) *)				\
 	 __genradix_ptr_alloc(&(_radix)->tree,			\
-			      (_idx) * sizeof((_radix)->type[0]),\
-			      _gfp))
+			__idx_to_offset((_idx),			\
+					sizeof((_radix)->type[0])),\
+			_gfp))
