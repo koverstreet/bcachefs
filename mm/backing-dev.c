pichina/linux-bcache

block: Make it legal to call bdi_destroy() on an unitialized bdi
provided the bdi was zeroed, of course

mm/backing-dev.c
@@ -425,6 +425,9 @@ void bdi_destroy(struct backing_dev_info *bdi)
 {
 	int i;
 
+	if (!bdi->work_list.next)
+		return;
+
 	bdi_wb_shutdown(bdi);
 	bdi_set_min_ratio(bdi, 0);
 

koverstreet/bcachefs

mm/backing-dev.c
@@ -853,6 +853,9 @@ static void bdi_remove_from_list(struct backing_dev_info *bdi)
 
 void bdi_unregister(struct backing_dev_info *bdi)
 {
+	if (!bdi->bdi_list.next)
+		return;
+
 	/* make sure nobody finds us on the bdi_list anymore */
 	bdi_remove_from_list(bdi);
 	wb_shutdown(&bdi->wb);