09227ab6027b778a1033e2d0f093acf7a6abf6ec
John Johansen <john.johansen@canonical.com>
2017-09-26 15:03:19 -0400

    UBUNTU: SAUCE: LSM stacking: fixup initialize task->security
    BugLink: http://bugs.launchpad.net/bugs/1763062
    
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -1726,6 +1726,9 @@ static __latent_entropy struct task_struct *copy_process(
 	p->sequential_io	= 0;
 	p->sequential_io_avg	= 0;
 #endif
+#ifdef CONFIG_SECURITY
+	p->security = NULL;
+#endif
 
 	/* Perform scheduler related setup. Assign this task to a CPU. */
 	retval = sched_fork(clone_flags, p);
