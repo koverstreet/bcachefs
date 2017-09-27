f3d9f35c4d3f17b654dcab53bcfb8d29fdd0f05b
John Johansen <john.johansen@canonical.com>
2017-09-27 02:10:17 -0400

    UBUNTU: SAUCE: LSM stacking: fixup apparmor stacking enablement
    BugLink: http://bugs.launchpad.net/bugs/1763062
    
    AppArmor doesn't need to register twice.
    
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- security/apparmor/lsm.c
+++ security/apparmor/lsm.c
@@ -1539,9 +1539,7 @@ static int __init apparmor_init(void)
 		return 0;
 	}
 
-	if (!apparmor_enabled ||
-	    !security_module_enable("apparmor",
-				IS_ENABLED(CONFIG_SECURITY_APPARMOR_STACKED))) {
+	if (!apparmor_enabled) {
 		aa_info_message("AppArmor disabled by boot time parameter");
 		apparmor_enabled = false;
 		return 0;
