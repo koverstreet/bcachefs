f0c40a2a0f535bb69df2040bcb626d1b17df9ff8
Kent Overstreet <kent.overstreet@gmail.com>
2017-11-04 20:08:28 -0400

    bcachefs: pass around struct bch_inode_info *, not struct inode *

--- fs/bcachefs/xattr.h
+++ fs/bcachefs/xattr.h
@@ -9,12 +9,13 @@ extern const struct bkey_ops bch2_bkey_xattr_ops;
 struct dentry;
 struct xattr_handler;
 struct bch_hash_info;
+struct bch_inode_info;
 
-int bch2_xattr_get(struct bch_fs *, struct inode *,
+int bch2_xattr_get(struct bch_fs *, struct bch_inode_info *,
 		  const char *, void *, size_t, int);
 int __bch2_xattr_set(struct bch_fs *, u64, const struct bch_hash_info *,
 		  const char *, const void *, size_t, int, int, u64 *);
-int bch2_xattr_set(struct bch_fs *, struct inode *,
+int bch2_xattr_set(struct bch_fs *, struct bch_inode_info *,
 		  const char *, const void *, size_t, int, int);
 ssize_t bch2_xattr_list(struct dentry *, char *, size_t);
 
