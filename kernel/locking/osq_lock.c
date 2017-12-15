5153779c1368ef7b7f9cdb0a4ff52b5e572f8a78
Kent Overstreet <kent.overstreet@gmail.com>
2017-12-15 16:58:23 -0500

    locking: export osq_lock()/osq_unlock()

--- kernel/locking/osq_lock.c
+++ kernel/locking/osq_lock.c
@@ -202,6 +202,7 @@ unqueue:
 
 	return false;
 }
+EXPORT_SYMBOL_GPL(osq_lock);
 
 void osq_unlock(struct optimistic_spin_queue *lock)
 {
@@ -229,3 +230,4 @@ void osq_unlock(struct optimistic_spin_queue *lock)
 	if (next)
 		WRITE_ONCE(next->locked, 1);
 }
+EXPORT_SYMBOL_GPL(osq_unlock);
