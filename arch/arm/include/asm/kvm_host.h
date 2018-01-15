bb5e4e0c9c25872e22aeaf2f3c4cada511317c59
James Morse <james.morse@arm.com>
2018-01-15 19:39:00 +0000

    KVM: arm/arm64: mask/unmask daif around VHE guests
    BugLink: http://bugs.launchpad.net/bugs/1756096
    
    Non-VHE systems take an exception to EL2 in order to world-switch into the
    guest. When returning from the guest KVM implicitly restores the DAIF
    flags when it returns to the kernel at EL1.
    
    With VHE none of this exception-level jumping happens, so KVMs
    world-switch code is exposed to the host kernel's DAIF values, and KVM
    spills the guest-exit DAIF values back into the host kernel.
    On entry to a guest we have Debug and SError exceptions unmasked, KVM
    has switched VBAR but isn't prepared to handle these. On guest exit
    Debug exceptions are left disabled once we return to the host and will
    stay this way until we enter user space.
    
    Add a helper to mask/unmask DAIF around VHE guests. The unmask can only
    happen after the hosts VBAR value has been synchronised by the isb in
    __vhe_hyp_call (via kvm_call_hyp()). Masking could be as late as
    setting KVMs VBAR value, but is kept here for symmetry.
    
    Acked-by: Marc Zyngier <marc.zyngier@arm.com>
    Signed-off-by: James Morse <james.morse@arm.com>
    Reviewed-by: Christoffer Dall <christoffer.dall@linaro.org>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    (backported from commit 4f5abad9e826bd579b0661efa32682d9c9bc3fa8)
    Signed-off-by: Manoj Iyer <manoj.iyer@canonical.com>
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- arch/arm/include/asm/kvm_host.h
+++ arch/arm/include/asm/kvm_host.h
@@ -301,6 +301,9 @@ int kvm_arm_vcpu_arch_has_attr(struct kvm_vcpu *vcpu,
 /* All host FP/SIMD state is restored on guest exit, so nothing to save: */
 static inline void kvm_fpsimd_flush_cpu_state(void) {}
 
+static inline void kvm_arm_vhe_guest_enter(void) {}
+static inline void kvm_arm_vhe_guest_exit(void) {}
+
 static inline bool kvm_arm_harden_branch_predictor(void)
 {
 	/* No way to detect it yet, pretend it is not there. */
--- arch/arm64/include/asm/kvm_host.h
+++ arch/arm64/include/asm/kvm_host.h
@@ -25,6 +25,7 @@
 #include <linux/types.h>
 #include <linux/kvm_types.h>
 #include <asm/cpufeature.h>
+#include <asm/daifflags.h>
 #include <asm/fpsimd.h>
 #include <asm/kvm.h>
 #include <asm/kvm_asm.h>
@@ -396,6 +397,16 @@ static inline void kvm_fpsimd_flush_cpu_state(void)
 {
 	if (system_supports_sve())
 		sve_flush_cpu_state();
+}
+
+static inline void kvm_arm_vhe_guest_enter(void)
+{
+	local_daif_mask();
+}
+
+static inline void kvm_arm_vhe_guest_exit(void)
+{
+	local_daif_restore(DAIF_PROCCTX_NOIRQ);
 }
 
 static inline bool kvm_arm_harden_branch_predictor(void)
