fc8bb88eba66483dceb0ccc36f0e99893841e864
oulijun <oulijun@huawei.com>
2018-01-30 20:20:44 +0800

    RDMA/hns: Add names to function arguments in function pointers
    BugLink: https://bugs.launchpad.net/bugs/1770974
    
    This patch mainly fix some style warings matched with the new checkpatch
    requirement. The warning as follows:
    
    WARNING: function definition argument 'struct hns_roce_cq *' should also have
    an identifier name
    
    Signed-off-by: Lijun Ou <oulijun@huawei.com>
    Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>
    (cherry picked from commit fd012f1c4f713a34edc574c00ca47c60d72a31a2)
    Signed-off-by: dann frazier <dann.frazier@canonical.com>
    Acked-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>

--- drivers/infiniband/hw/hns/hns_roce_device.h
+++ drivers/infiniband/hw/hns/hns_roce_device.h
@@ -389,8 +389,8 @@ struct hns_roce_cq {
 	u8				db_en;
 	spinlock_t			lock;
 	struct ib_umem			*umem;
-	void (*comp)(struct hns_roce_cq *);
-	void (*event)(struct hns_roce_cq *, enum hns_roce_event);
+	void (*comp)(struct hns_roce_cq *cq);
+	void (*event)(struct hns_roce_cq *cq, enum hns_roce_event event_type);
 
 	struct hns_roce_uar		*uar;
 	u32				cq_depth;
@@ -533,8 +533,8 @@ struct hns_roce_qp {
 	u32                     atomic_rd_en;
 	u32			pkey_index;
 	u32			qkey;
-	void			(*event)(struct hns_roce_qp *,
-					 enum hns_roce_event);
+	void			(*event)(struct hns_roce_qp *qp,
+					 enum hns_roce_event event_type);
 	unsigned long		qpn;
 
 	atomic_t		refcount;
