53926cfeb223ce0e5974c7392ce117bc40f40509
Kent Overstreet <kent.overstreet@gmail.com>
2018-02-08 16:05:43 -0500

    bcachefs: delete dead code
    replaced by bch2_data_job()

--- fs/bcachefs/migrate.h
+++ fs/bcachefs/migrate.h
@@ -1,7 +1,6 @@
 #ifndef _BCACHEFS_MIGRATE_H
 #define _BCACHEFS_MIGRATE_H
 
-int bch2_dev_data_migrate(struct bch_fs *, struct bch_dev *, int);
 int bch2_dev_data_drop(struct bch_fs *, unsigned, int);
 
 #endif /* _BCACHEFS_MIGRATE_H */
