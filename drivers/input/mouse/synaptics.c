168dd4789a0d93dcfd94b9767e579ba05848a5df
Arkadiusz Hiler <arkadiusz.hiler@intel.com>
2018-03-07 15:40:28 -0800

    Revert "Input: synaptics - Lenovo Thinkpad T460p devices should use RMI"
    BugLink: http://bugs.launchpad.net/bugs/1756100
    
    commit 5444a992b4a73aa5246a432c482b20b89bce93a5 upstream.
    
    This reverts commit 48282969826b3d3c76e908182f69724d86d995fe which
    caused the following issues:
    
    1. On T460p with BIOS version 2.22 touchpad and trackpoint stop working
    after suspend-resume cycle. Due to strange state of the device another
    suspend is impossible.
    
    The following dmesg errors can be observed:
    thinkpad_acpi: EC reports that Thermal Table has changed
    rmi4_smbus 7-002c: failed to get SMBus version number!
    rmi4_physical rmi4-00: rmi_driver_reset_handler: Failed to read current IRQ mask.
    rmi4_f01 rmi4-00.fn01: Failed to restore normal operation: -16.
    rmi4_f01 rmi4-00.fn01: Resume failed with code -16.
    rmi4_physical rmi4-00: Failed to suspend functions: -16
    rmi4_smbus 7-002c: Failed to resume device: -16
    PM: resume devices took 0.640 seconds
    rmi4_f03 rmi4-00.fn03: rmi_f03_pt_write: Failed to write to F03 TX register (-16).
    rmi4_physical rmi4-00: rmi_driver_clear_irq_bits: Failed to change enabled interrupts!
    rmi4_physical rmi4-00: rmi_driver_set_irq_bits: Failed to change enabled interrupts!
    psmouse: probe of serio3 failed with error -1
    
    2. On another T460p with BIOS version 2.15 two finger scrolling gesture
    on the touchpad stops working after suspend-resume cycle (about 75%
    reproducibility, when it still works, the scrolling gesture becomes
    laggy). Nothing suspicious appears in the dmesg.
    
    Analysis form Richard Schütz:
    
    "RMI is unreliable on the ThinkPad T460p because the device is affected
    by the firmware behavior addressed in a7ae81952cda ("i2c: i801: Allow
    ACPI SystemIO OpRegion to conflict with PCI BAR")."
    
    The affected devices often show:
    
    i801_smbus 0000:00:1f.4: BIOS is accessing SMBus registers
    i801_smbus 0000:00:1f.4: Driver SMBus register access inhibited
    
    Reported-by: Richard Schütz <rschuetz@uni-koblenz.de>
    Signed-off-by: Arkadiusz Hiler <arkadiusz.hiler@intel.com>
    Tested-by: Martin Peres <martin.peres@linux.intel.com>
    Tested-by: Arkadiusz Hiler <arkadiusz.hiler@intel.com>
    Cc: stable@vger.kernel.org
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Signed-off-by: Thadeu Lima de Souza Cascardo <cascardo@canonical.com>

--- drivers/input/mouse/synaptics.c
+++ drivers/input/mouse/synaptics.c
@@ -174,7 +174,6 @@ static const char * const smbus_pnp_ids[] = {
 	"LEN004a", /* W541 */
 	"LEN0073", /* X1 Carbon 5 */
 	"LEN200f", /* T450s */
-	"LEN2018", /* T460p */
 	"LEN0092", /* X1 Carbon 6 */
 	"LEN0096", /* X280 */
 	NULL
