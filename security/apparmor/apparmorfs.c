a9cfa10d7ccbd738809cd1129a9a13384d3b3de2
Colin Ian King <colin.king@canonical.com>
2018-03-27 14:35:58 +0100

    apparmor: fix memory leak on buffer on error exit path
    BugLink: http://bugs.launchpad.net/bugs/1763427
    
    Currently on the error exit path the allocated buffer is not free'd
    causing a memory leak. Fix this by kfree'ing it.
    
    Detected by CoverityScan, CID#1466876 ("Resource leaks")
    
    Fixes: 1180b4c757aa ("apparmor: fix dangling symlinks to policy rawdata after replacement")
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    (cherry picked from commit 588558eb6d0e0b6edfa65a67e906c2ffeba63ff1
     git://git.kernel.org/pub/scm/linux/kernel/git/jj/linux-apparmor)
    Signed-off-by: Seth Forshee <seth.forshee@canonical.com>

--- security/apparmor/apparmorfs.c
+++ security/apparmor/apparmorfs.c
@@ -1500,8 +1500,10 @@ static char *gen_symlink_name(int depth, const char *dirname, const char *fname)
 	}
 
 	error = snprintf(s, size, "raw_data/%s/%s", dirname, fname);
-	if (error >= size || error < 0)
+	if (error >= size || error < 0) {
+		kfree(buffer);
 		return ERR_PTR(-ENAMETOOLONG);
+	}
 
 	return buffer;
 }
