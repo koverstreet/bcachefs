dbcdd83722691e284d877a55d4d97ca0b68bce7c
Kent Overstreet <kent.overstreet@gmail.com>
2018-04-15 18:31:49 -0400

    bcachefs: Keep track of latency devices are capable of

--- fs/bcachefs/io_types.h
+++ fs/bcachefs/io_types.h
@@ -15,6 +15,7 @@
 struct bch_read_bio {
 	struct bch_fs		*c;
 	u64			start_time;
+	u64			submit_time;
 
 	/*
 	 * Reads will often have to be split, and if the extent being read from
@@ -36,7 +37,6 @@ struct bch_read_bio {
 	 */
 	struct bvec_iter	bvec_iter;
 
-	unsigned		submit_time_us;
 	u16			flags;
 	union {
 	struct {
@@ -71,6 +71,8 @@ struct bch_write_bio {
 	struct bch_fs		*c;
 	struct bch_write_bio	*parent;
 
+	u64			submit_time;
+
 	struct bch_devs_list	failed;
 	u8			order;
 	u8			dev;
@@ -80,8 +82,6 @@ struct bch_write_bio {
 				put_bio:1,
 				have_ioref:1,
 				used_mempool:1;
-
-	unsigned		submit_time_us;
 
 	struct bio		bio;
 };
