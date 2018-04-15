6fdb3778063ab62c9a023e2c8f2a71e704db49f6
Kent Overstreet <kent.overstreet@gmail.com>
2018-04-15 18:00:09 -0400

    bcachefs: Keep outstanding promotes in a hash table
    this is so we don't end up promoting the same extent multiple times
    while the first promote is still in flight

--- fs/bcachefs/io.c
+++ fs/bcachefs/io.c
@@ -856,9 +856,25 @@ void bch2_write(struct closure *cl)
 
 struct promote_op {
 	struct closure		cl;
+
+	struct rhash_head	hash;
+	struct bpos		pos;
+
 	struct migrate_write	write;
 	struct bio_vec		bi_inline_vecs[0]; /* must be last */
 };
+
+static const struct rhashtable_params bch_promote_params = {
+	.head_offset	= offsetof(struct promote_op, hash),
+	.key_offset	= offsetof(struct promote_op, pos),
+	.key_len	= sizeof(struct bpos),
+};
+
+static void promote_free(struct bch_fs *c, struct promote_op *op)
+{
+	rhashtable_remove_fast(&c->promote_table, &op->hash, bch_promote_params);
+	kfree(op);
+}
 
 static void promote_done(struct closure *cl)
 {
@@ -868,7 +884,7 @@ static void promote_done(struct closure *cl)
 
 	percpu_ref_put(&c->writes);
 	bch2_bio_free_pages_pool(c, &op->write.op.wbio.bio);
-	kfree(op);
+	promote_free(c, op);
 }
 
 static void promote_start(struct promote_op *op, struct bch_read_bio *rbio)
@@ -919,6 +935,14 @@ static struct promote_op *promote_alloc(struct bch_read_bio *rbio,
 	if (!op)
 		return NULL;
 
+	op->pos = k.k->p;
+
+	if (rhashtable_lookup_insert_fast(&c->promote_table, &op->hash,
+					  bch_promote_params)) {
+		kfree(op);
+		return NULL;
+	}
+
 	bio = &op->write.op.wbio.bio;
 	bio_init(bio, NULL, bio->bi_inline_vecs, pages, 0);
 
@@ -953,7 +977,14 @@ static bool should_promote(struct bch_fs *c, struct bkey_s_c k,
 	if (!bkey_extent_is_data(k.k))
 		return false;
 
-	return bch2_extent_has_target(c, bkey_s_c_to_extent(k), target) == NULL;
+	if (bch2_extent_has_target(c, bkey_s_c_to_extent(k), target))
+		return false;
+
+	if (rhashtable_lookup_fast(&c->promote_table, &k.k->p,
+				   bch_promote_params))
+		return false;
+
+	return true;
 }
 
 /* Read */
@@ -993,7 +1024,7 @@ static inline struct bch_read_bio *bch2_rbio_free(struct bch_read_bio *rbio)
 	BUG_ON(rbio->bounce && !rbio->split);
 
 	if (rbio->promote)
-		kfree(rbio->promote);
+		promote_free(rbio->c, rbio->promote);
 	rbio->promote = NULL;
 
 	if (rbio->bounce)
@@ -1434,9 +1465,10 @@ int __bch2_read_extent(struct bch_fs *c, struct bch_read_bio *orig,
 	}
 
 	promote = should_promote(c, k, flags, orig->opts.promote_target);
-	/* could also set read_full */
-	if (promote)
+	if (promote) {
+		read_full = true;
 		bounce = true;
+	}
 
 	if (!read_full) {
 		EBUG_ON(pick.crc.compression_type);
@@ -1654,6 +1686,8 @@ void bch2_read(struct bch_fs *c, struct bch_read_bio *rbio, u64 inode)
 
 void bch2_fs_io_exit(struct bch_fs *c)
 {
+	if (c->promote_table.tbl)
+		rhashtable_destroy(&c->promote_table);
 	mempool_exit(&c->bio_bounce_pages);
 	bioset_exit(&c->bio_write);
 	bioset_exit(&c->bio_read_split);
@@ -1672,7 +1706,8 @@ int bch2_fs_io_init(struct bch_fs *c)
 				   max_t(unsigned,
 					 c->opts.btree_node_size,
 					 c->sb.encoded_extent_max) /
-				   PAGE_SECTORS, 0))
+				   PAGE_SECTORS, 0) ||
+	    rhashtable_init(&c->promote_table, &bch_promote_params))
 		return -ENOMEM;
 
 	return 0;
