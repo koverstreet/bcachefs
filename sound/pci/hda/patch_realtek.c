431b427b3acd78ffeb07c4bd6a995d15204fa9f9
Hui Wang <hui.wang@canonical.com>
2018-04-24 07:10:00 +0200

    ALSA: hda/realtek - set PINCFG_HEADSET_MIC to parse_flags
    BugLink: https://bugs.launchpad.net/bugs/1766398
    
    Otherwise, the pin will be regarded as microphone, and the jack name
    is "Mic Phantom", it is always on in the pulseaudio even nothing is
    plugged into the jack. So the UI is confusing to users since the
    microphone always shows up in the UI even there is no microphone
    plugged.
    
    After adding this flag, the jack name is "Headset Mic Phantom", then
    the pulseaudio can handle its detection correctly.
    
    Fixes: f0ba9d699e5c ("ALSA: hda/realtek - Fix Dell headset Mic can't record")
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Hui Wang <hui.wang@canonical.com>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    (cherry picked from commit 3ce0d5aa265bcc0a4b281cb0cabf92491276101b)
    Signed-off-by: Hui Wang <hui.wang@canonical.com>
    Acked-by: Po-Hsu Lin <po-hsu.lin@canonical.com>
    Acked-by: Aaron Ma <aaron.ma@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- sound/pci/hda/patch_realtek.c
+++ sound/pci/hda/patch_realtek.c
@@ -6365,6 +6365,8 @@ static const struct hda_fixup alc269_fixups[] = {
 			{ 0x19, 0x01a1913c }, /* use as headset mic, without its own jack detect */
 			{ }
 		},
+		.chained = true,
+		.chain_id = ALC269_FIXUP_HEADSET_MIC
 	},
 };
 
