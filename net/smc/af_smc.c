e411f707a8b42b9debb231574112a37913dfeca2
Stefan Raspl <stefan.raspl@linux.ibm.com>
2018-05-03 17:57:39 +0200

    smc: fix sendpage() call
    BugLink: http://bugs.launchpad.net/bugs/1794889
    
    [ Upstream commit bda27ff5c4526f80a7620a94ecfe8dca153e3696 ]
    
    The sendpage() call grabs the sock lock before calling the default
    implementation - which tries to grab it once again.
    
    Signed-off-by: Stefan Raspl <raspl@linux.ibm.com>
    Signed-off-by: Ursula Braun <ubraun@linux.ibm.com><
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Sasha Levin <alexander.levin@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- net/smc/af_smc.c
+++ net/smc/af_smc.c
@@ -1284,8 +1284,11 @@ static ssize_t smc_sendpage(struct socket *sock, struct page *page,
 
 	smc = smc_sk(sk);
 	lock_sock(sk);
-	if (sk->sk_state != SMC_ACTIVE)
+	if (sk->sk_state != SMC_ACTIVE) {
+		release_sock(sk);
 		goto out;
+	}
+	release_sock(sk);
 	if (smc->use_fallback)
 		rc = kernel_sendpage(smc->clcsock, page, offset,
 				     size, flags);
@@ -1293,7 +1296,6 @@ static ssize_t smc_sendpage(struct socket *sock, struct page *page,
 		rc = sock_no_sendpage(sock, page, offset, size, flags);
 
 out:
-	release_sock(sk);
 	return rc;
 }
 
