f8b34633dff5a6c7afa5def89ad68bdf53cfea56
Kent Overstreet <kent.overstreet@gmail.com>
2018-05-08 17:24:28 -0400

    bcachefs: Disable broken ioctls, add some comments

--- fs/bcachefs/chardev.c
+++ fs/bcachefs/chardev.c
@@ -54,6 +54,7 @@ static struct bch_dev *bch2_device_lookup(struct bch_fs *c, u64 dev,
 	return ca;
 }
 
+#if 0
 static long bch2_ioctl_assemble(struct bch_ioctl_assemble __user *user_arg)
 {
 	struct bch_ioctl_assemble arg;
@@ -127,14 +128,17 @@ static long bch2_ioctl_incremental(struct bch_ioctl_incremental __user *user_arg
 
 	return 0;
 }
+#endif
 
 static long bch2_global_ioctl(unsigned cmd, void __user *arg)
 {
 	switch (cmd) {
+#if 0
 	case BCH_IOCTL_ASSEMBLE:
 		return bch2_ioctl_assemble(arg);
 	case BCH_IOCTL_INCREMENTAL:
 		return bch2_ioctl_incremental(arg);
+#endif
 	default:
 		return -ENOTTY;
 	}
@@ -148,6 +152,7 @@ static long bch2_ioctl_query_uuid(struct bch_fs *c,
 			    sizeof(c->sb.user_uuid));
 }
 
+#if 0
 static long bch2_ioctl_start(struct bch_fs *c, struct bch_ioctl_start arg)
 {
 	if (arg.flags || arg.pad)
@@ -161,6 +166,7 @@ static long bch2_ioctl_stop(struct bch_fs *c)
 	bch2_fs_stop(c);
 	return 0;
 }
+#endif
 
 static long bch2_ioctl_disk_add(struct bch_fs *c, struct bch_ioctl_disk arg)
 {
@@ -419,7 +425,7 @@ static long bch2_ioctl_usage(struct bch_fs *c,
 
 		if (ca->dev_idx >= arg.nr_devices) {
 			percpu_ref_put(&ca->ref);
-			return -ENOSPC;
+			return -ERANGE;
 		}
 
 		if (percpu_ref_tryget(&ca->io_ref)) {
@@ -539,10 +545,12 @@ long bch2_fs_ioctl(struct bch_fs *c, unsigned cmd, void __user *arg)
 		return -EPERM;
 
 	switch (cmd) {
+#if 0
 	case BCH_IOCTL_START:
 		BCH_IOCTL(start, struct bch_ioctl_start);
 	case BCH_IOCTL_STOP:
 		return bch2_ioctl_stop(c);
+#endif
 	case BCH_IOCTL_READ_SUPER:
 		BCH_IOCTL(read_super, struct bch_ioctl_read_super);
 	case BCH_IOCTL_DISK_GET_IDX:
