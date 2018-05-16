03230e5a7bb9cc72d8f7672c9cb7047c4da91376
Jason Wang <jasowang@redhat.com>
2018-05-16 20:39:33 +0800

    tuntap: fix use after free during release
    BugLink: https://bugs.launchpad.net/bugs/1836117
    
    commit 7063efd33bb15abc0160347f89eb5aba6b7d000e upstream.
    
    After commit b196d88aba8a ("tun: fix use after free for ptr_ring") we
    need clean up tx ring during release(). But unfortunately, it tries to
    do the cleanup blindly after socket were destroyed which will lead
    another use-after-free. Fix this by doing the cleanup before dropping
    the last reference of the socket in __tun_detach().
    
    Backport Note :-
    Upstream commit moves the ptr_ring_cleanup call from tun_chr_close to
    __tun_detach. Upstream applied that patch after replacing skb_array with
    ptr_ring. This patch moves the skb_array_cleanup call from
    tun_chr_close to __tun_detach.
    
    Reported-by: Andrei Vagin <avagin@virtuozzo.com>
    Acked-by: Andrei Vagin <avagin@virtuozzo.com>
    Fixes: b196d88aba8a ("tun: fix use after free for ptr_ring")
    Signed-off-by: Jason Wang <jasowang@redhat.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Zubin Mithra <zsm@chromium.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    (cherry picked from commit 2a522aef03a4e11d6fdc05a107a023943b103e39)
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- drivers/net/tun.c
+++ drivers/net/tun.c
@@ -657,6 +657,7 @@ static void __tun_detach(struct tun_file *tfile, bool clean)
 			    tun->dev->reg_state == NETREG_REGISTERED)
 				unregister_netdevice(tun->dev);
 		}
+		skb_array_cleanup(&tfile->tx_array);
 		sock_put(&tfile->sk);
 	}
 }
@@ -2894,7 +2895,6 @@ static int tun_chr_close(struct inode *inode, struct file *file)
 	struct tun_file *tfile = file->private_data;
 
 	tun_detach(tfile, true);
-	skb_array_cleanup(&tfile->tx_array);
 
 	return 0;
 }
