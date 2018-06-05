f19d1814352bcf032315ed38532c6edf191d919f
Golan Ben Ami <golan.ben.ami@intel.com>
2018-06-05 11:58:13 +0300

    iwlwifi: pcie: fit reclaim msg to MAX_MSG_LEN
    BugLink: https://bugs.launchpad.net/bugs/1853915
    
    [ Upstream commit 81f0c66187e1ebb7b63529d82faf7ff1e0ef428a ]
    
    Today, the length of a debug message in iwl_trans_pcie_reclaim
    may pass the MAX_MSG_LEN, which is 110.
    An example for this kind of message is:
    
    'iwl_trans_pcie_reclaim: Read index for DMA queue txq id (2),
    last_to_free 65535 is out of range [0-65536] 2 2.'
    
    Cut the message a bit so it will fit the allowed MAX_MSG_LEN.
    
    Signed-off-by: Golan Ben Ami <golan.ben.ami@intel.com>
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>

--- drivers/net/wireless/intel/iwlwifi/pcie/tx.c
+++ drivers/net/wireless/intel/iwlwifi/pcie/tx.c
@@ -1108,7 +1108,7 @@ void iwl_trans_pcie_reclaim(struct iwl_trans *trans, int txq_id, int ssn,
 
 	if (!iwl_queue_used(txq, last_to_free)) {
 		IWL_ERR(trans,
-			"%s: Read index for DMA queue txq id (%d), last_to_free %d is out of range [0-%d] %d %d.\n",
+			"%s: Read index for txq id (%d), last_to_free %d is out of range [0-%d] %d %d.\n",
 			__func__, txq_id, last_to_free, TFD_QUEUE_SIZE_MAX,
 			txq->write_ptr, txq->read_ptr);
 		goto out;
