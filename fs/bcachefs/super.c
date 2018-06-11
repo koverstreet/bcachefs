e432499b5a9854489dae7d2a792eb800431b2a0b
Kent Overstreet <kent.overstreet@gmail.com>
2018-06-11 08:24:18 -0400

    bcachefs: bch_sb_field_clean
    Implement a superblock field so we don't have to read the journal after
    a clean shutdown (and more importantly, we can verify what we find in
    the journal after a clean shutdown)
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- fs/bcachefs/super.c
+++ fs/bcachefs/super.c
@@ -169,16 +169,6 @@ struct bch_fs *bch2_uuid_to_fs(uuid_le uuid)
  * - allocator depends on the journal (when it rewrites prios and gens)
  */
 
-static void bch2_fs_mark_clean(struct bch_fs *c, bool clean)
-{
-	mutex_lock(&c->sb_lock);
-	if (BCH_SB_CLEAN(c->disk_sb.sb) != clean) {
-		SET_BCH_SB_CLEAN(c->disk_sb.sb, clean);
-		bch2_write_super(c);
-	}
-	mutex_unlock(&c->sb_lock);
-}
-
 static void __bch2_fs_read_only(struct bch_fs *c)
 {
 	struct bch_dev *ca;
@@ -281,10 +271,8 @@ void bch2_fs_read_only(struct bch_fs *c)
 
 	if (!bch2_journal_error(&c->journal) &&
 	    !test_bit(BCH_FS_ERROR, &c->flags) &&
-	    !test_bit(BCH_FS_EMERGENCY_RO, &c->flags)) {
-
+	    !test_bit(BCH_FS_EMERGENCY_RO, &c->flags))
 		bch2_fs_mark_clean(c, true);
-	}
 
 	if (c->state != BCH_FS_STOPPING)
 		c->state = BCH_FS_RO;
