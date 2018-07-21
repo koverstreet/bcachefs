b12d802f72196336afaf5bfc05309a5df882d96c
Kent Overstreet <kent.overstreet@gmail.com>
2018-07-20 22:23:42 -0400

    bcachefs: Use ei_update_lock consistently
    This is prep work for using deferred btree updates for inode updates -
    the way inodes are done now we're relying on btree locking for ei_inode
    and ei_update_lock could probably be removed, but it'll actually be
    needed when we switch to deferred updates.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- fs/bcachefs/acl.c
+++ fs/bcachefs/acl.c
@@ -300,13 +300,14 @@ int bch2_set_acl(struct inode *vinode, struct posix_acl *acl, int type)
 	umode_t mode = inode->v.i_mode;
 	int ret;
 
+	mutex_lock(&inode->ei_update_lock);
+	bch2_trans_init(&trans, c);
+
 	if (type == ACL_TYPE_ACCESS && acl) {
 		ret = posix_acl_update_mode(&inode->v, &mode, &acl);
 		if (ret)
-			return ret;
+			goto err;
 	}
-
-	bch2_trans_init(&trans, c);
 retry:
 	bch2_trans_begin(&trans);
 
@@ -332,6 +333,7 @@ retry:
 	set_cached_acl(&inode->v, type, acl);
 err:
 	bch2_trans_exit(&trans);
+	mutex_unlock(&inode->ei_update_lock);
 
 	return ret;
 }
