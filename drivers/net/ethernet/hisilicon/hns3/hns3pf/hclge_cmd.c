885f9b8be933f57d2fa7cf4b47325ac0d3ea6727
dann frazier <dann.frazier@canonical.com>
2018-08-16 13:19:14 -0600

    Revert "UBUNTU: SAUCE: {topost} net: hns3: prevent sending command during global or core reset"
    BugLink: https://bugs.launchpad.net/bugs/1787477
    
    This reverts commit 79d505d3bc14e4692a5fbbf21eee0342644cb228.
    Signed-off-by: dann frazier <dann.frazier@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_cmd.c
+++ drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_cmd.c
@@ -200,8 +200,7 @@ int hclge_cmd_send(struct hclge_hw *hw, struct hclge_desc *desc, int num)
 
 	spin_lock_bh(&hw->cmq.csq.lock);
 
-	if (num > hclge_ring_space(&hw->cmq.csq) ||
-	    test_bit(HCLGE_STATE_CMD_DISABLE, &hdev->state)) {
+	if (num > hclge_ring_space(&hw->cmq.csq)) {
 		spin_unlock_bh(&hw->cmq.csq.lock);
 		return -EBUSY;
 	}
@@ -339,7 +338,6 @@ int hclge_cmd_init(struct hclge_dev *hdev)
 	spin_lock_init(&hdev->hw.cmq.crq.lock);
 
 	hclge_cmd_init_regs(&hdev->hw);
-	clear_bit(HCLGE_STATE_CMD_DISABLE, &hdev->state);
 
 	ret = hclge_cmd_query_firmware_version(&hdev->hw, &version);
 	if (ret) {
