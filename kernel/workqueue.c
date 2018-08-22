ad40caf5fdcbb5c570a46d77bce1bd4c5943f1cb
Johannes Berg <johannes.berg@intel.com>
2018-08-22 11:49:04 +0200

    workqueue: re-add lockdep dependencies for flushing
    BugLink: https://bugs.launchpad.net/bugs/1835972
    
    [ Upstream commit 87915adc3f0acdf03c776df42e308e5a155c19af ]
    
    In flush_work(), we need to create a lockdep dependency so that
    the following scenario is appropriately tagged as a problem:
    
      work_function()
      {
        mutex_lock(&mutex);
        ...
      }
    
      other_function()
      {
        mutex_lock(&mutex);
        flush_work(&work); // or cancel_work_sync(&work);
      }
    
    This is a problem since the work might be running and be blocked
    on trying to acquire the mutex.
    
    Similarly, in flush_workqueue().
    
    These were removed after cross-release partially caught these
    problems, but now cross-release was reverted anyway. IMHO the
    removal was erroneous anyway though, since lockdep should be
    able to catch potential problems, not just actual ones, and
    cross-release would only have caught the problem when actually
    invoking wait_for_completion().
    
    Fixes: fd1a5b04dfb8 ("workqueue: Remove now redundant lock acquisitions wrt. workqueue flushes")
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Sasha Levin <alexander.levin@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -2639,6 +2639,9 @@ void flush_workqueue(struct workqueue_struct *wq)
 	if (WARN_ON(!wq_online))
 		return;
 
+	lock_map_acquire(&wq->lockdep_map);
+	lock_map_release(&wq->lockdep_map);
+
 	mutex_lock(&wq->mutex);
 
 	/*
@@ -2891,6 +2894,11 @@ static bool __flush_work(struct work_struct *work, bool from_cancel)
 
 	if (WARN_ON(!wq_online))
 		return false;
+
+	if (!from_cancel) {
+		lock_map_acquire(&work->lockdep_map);
+		lock_map_release(&work->lockdep_map);
+	}
 
 	if (start_flush_work(work, &barr, from_cancel)) {
 		wait_for_completion(&barr.done);
