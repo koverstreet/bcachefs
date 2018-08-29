10a54c205e6e8c365287314587b5c5521f35d393
Kent Overstreet <kent.overstreet@gmail.com>
2018-08-28 20:03:51 -0400

    block: optimize bvec_iter_advance()
    This results in fewer branches/cmov instructions in the inner loop
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- include/linux/bvec.h
+++ include/linux/bvec.h
@@ -87,26 +87,35 @@ struct bvec_iter_all {
 static inline bool bvec_iter_advance(const struct bio_vec *bv,
 		struct bvec_iter *iter, unsigned bytes)
 {
+	unsigned bvec_len;
+
 	if (WARN_ONCE(bytes > iter->bi_size,
 		     "Attempted to advance past end of bvec iter\n")) {
 		iter->bi_size = 0;
 		return false;
 	}
 
-	while (bytes) {
-		const struct bio_vec *cur = bv + iter->bi_idx;
-		unsigned len = min3(bytes, iter->bi_size,
-				    cur->bv_len - iter->bi_bvec_done);
+	iter->bi_size -= bytes;
 
-		bytes -= len;
-		iter->bi_size -= len;
-		iter->bi_bvec_done += len;
+	bv += iter->bi_idx;
 
-		if (iter->bi_bvec_done == cur->bv_len) {
-			iter->bi_bvec_done = 0;
-			iter->bi_idx++;
-		}
+	while (1) {
+		/* don't deref bv when bytes == 0: */
+		if (!bytes)
+			break;
+
+		bvec_len = bv->bv_len - iter->bi_bvec_done;
+		if (bytes < bvec_len)
+			break;
+
+		bytes -= bvec_len;
+
+		iter->bi_bvec_done = 0;
+		iter->bi_idx++;
+		bv++;
 	}
+
+	iter->bi_bvec_done	+= bytes;
 	return true;
 }
 
