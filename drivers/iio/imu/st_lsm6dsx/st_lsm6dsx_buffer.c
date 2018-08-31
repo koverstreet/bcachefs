036960057b55ca3e8f409f384678423e5e574e4d
Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
2018-08-31 10:51:14 +0200

    iio: imu: st_lsm6dsx: take into account ts samples in wm configuration
    BugLink: https://bugs.launchpad.net/bugs/1836426
    
    [ Upstream commit a13bf65f3f2e36008ea60b49d3bda2527e09fd9c ]
    
    Take into account hw timer samples in pattern length computation done
    in st_lsm6dsx_update_watermark routine for watermark configuration.
    Moreover use samples in pattern (sip) already computed in
    st_lsm6dsx_update_decimators routine
    
    Fixes: 213451076bd3 ("iio: imu: st_lsm6dsx: add hw timestamp support")
    Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Signed-off-by: Sasha Levin <alexander.levin@microsoft.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- drivers/iio/imu/st_lsm6dsx/st_lsm6dsx_buffer.c
+++ drivers/iio/imu/st_lsm6dsx/st_lsm6dsx_buffer.c
@@ -160,12 +160,15 @@ static int st_lsm6dsx_set_fifo_odr(struct st_lsm6dsx_sensor *sensor,
 
 int st_lsm6dsx_update_watermark(struct st_lsm6dsx_sensor *sensor, u16 watermark)
 {
-	u16 fifo_watermark = ~0, cur_watermark, sip = 0, fifo_th_mask;
+	u16 fifo_watermark = ~0, cur_watermark, fifo_th_mask;
 	struct st_lsm6dsx_hw *hw = sensor->hw;
 	struct st_lsm6dsx_sensor *cur_sensor;
 	__le16 wdata;
 	int i, err;
 	u8 data;
+
+	if (!hw->sip)
+		return 0;
 
 	for (i = 0; i < ST_LSM6DSX_ID_MAX; i++) {
 		cur_sensor = iio_priv(hw->iio_devs[i]);
@@ -177,14 +180,10 @@ int st_lsm6dsx_update_watermark(struct st_lsm6dsx_sensor *sensor, u16 watermark)
 						       : cur_sensor->watermark;
 
 		fifo_watermark = min_t(u16, fifo_watermark, cur_watermark);
-		sip += cur_sensor->sip;
 	}
 
-	if (!sip)
-		return 0;
-
-	fifo_watermark = max_t(u16, fifo_watermark, sip);
-	fifo_watermark = (fifo_watermark / sip) * sip;
+	fifo_watermark = max_t(u16, fifo_watermark, hw->sip);
+	fifo_watermark = (fifo_watermark / hw->sip) * hw->sip;
 	fifo_watermark = fifo_watermark * hw->settings->fifo_ops.th_wl;
 
 	mutex_lock(&hw->lock);
