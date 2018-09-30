fe24b83941ad5a7a0ffa72c37b08cf952c52db1b
Colin Ian King <colin.king@canonical.com>
2018-10-01 00:03:07 +0100

    scsi: arcmsr: clean up clang warning on extraneous parentheses
    BugLink: https://bugs.launchpad.net/bugs/1854216
    
    [ Upstream commit ca2ade24157693b4e533ccec69df00ef719d4aad ]
    
    There are extraneous parantheses that are causing clang to produce a
    warning so remove these.
    
    Clean up 3 clang warnings:
    equality comparison with extraneous parentheses [-Wparentheses-equality]
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Ching Huang <ching2048@areca.com.tw>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>

--- drivers/scsi/arcmsr/arcmsr_hba.c
+++ drivers/scsi/arcmsr/arcmsr_hba.c
@@ -3645,9 +3645,9 @@ static void arcmsr_hardware_reset(struct AdapterControlBlock *acb)
 		pci_read_config_byte(acb->pdev, i, &value[i]);
 	}
 	/* hardware reset signal */
-	if ((acb->dev_id == 0x1680)) {
+	if (acb->dev_id == 0x1680) {
 		writel(ARCMSR_ARC1680_BUS_RESET, &pmuA->reserved1[0]);
-	} else if ((acb->dev_id == 0x1880)) {
+	} else if (acb->dev_id == 0x1880) {
 		do {
 			count++;
 			writel(0xF, &pmuC->write_sequence);
@@ -3658,7 +3658,7 @@ static void arcmsr_hardware_reset(struct AdapterControlBlock *acb)
 			writel(0xD, &pmuC->write_sequence);
 		} while (((readl(&pmuC->host_diagnostic) & ARCMSR_ARC1880_DiagWrite_ENABLE) == 0) && (count < 5));
 		writel(ARCMSR_ARC1880_RESET_ADAPTER, &pmuC->host_diagnostic);
-	} else if ((acb->dev_id == 0x1214)) {
+	} else if (acb->dev_id == 0x1214) {
 		writel(0x20, pmuD->reset_request);
 	} else {
 		pci_write_config_byte(acb->pdev, 0x84, 0x20);
