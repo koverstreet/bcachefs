798d67ea6b1d7e57625c6092b06d358abed8b20b
ndesaulniers@google.com <ndesaulniers@google.com>
2018-10-31 12:39:01 -0700

    include/linux/compiler*.h: define asm_volatile_goto
    BugLink: https://bugs.launchpad.net/bugs/1838700
    
    commit 8bd66d147c88bd441178c7b4c774ae5a185f19b8 upstream.
    
    asm_volatile_goto should also be defined for other compilers that support
    asm goto.
    
    Fixes commit 815f0ddb346c ("include/linux/compiler*.h: make compiler-*.h
    mutually exclusive").
    
    Signed-off-by: Nick Desaulniers <ndesaulniers@google.com>
    Signed-off-by: Miguel Ojeda <miguel.ojeda.sandonis@gmail.com>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>

--- include/linux/compiler_types.h
+++ include/linux/compiler_types.h
@@ -273,6 +273,10 @@ struct ftrace_likely_data {
 #endif
 
 
+#ifndef asm_volatile_goto
+#define asm_volatile_goto(x...) asm goto(x)
+#endif
+
 /* Are two types/vars the same type (ignoring qualifiers)? */
 #ifndef __same_type
 # define __same_type(a, b) __builtin_types_compatible_p(typeof(a), typeof(b))
