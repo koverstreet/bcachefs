397e945b2a8d2b34690c2ca50b72a40f45f5efc3
zhong jiang <zhongjiang@huawei.com>
2018-10-31 13:54:15 -0400

    s390: qeth_core_mpc: Use ARRAY_SIZE instead of reimplementing its function
    BugLink: https://bugs.launchpad.net/bugs/1800641
    
    Use the common code ARRAY_SIZE macro instead of a private implementation.
    
    Reviewed-by: Jean Delvare <jdelvare@suse.de>
    Signed-off-by: zhong jiang <zhongjiang@huawei.com>
    Signed-off-by: Martin Schwidefsky <schwidefsky@de.ibm.com>
    Signed-off-by: Julian Wiedmann <jwi@linux.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 065a2cdcbdf8eb9aefb66e1a24b2d684b8b8852b)
    Signed-off-by: Joseph Salisbury <joseph.salisbury@canonical.com>
    Acked-by: Kleber Souza <kleber.souza@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>

--- drivers/s390/net/qeth_core_mpc.c
+++ drivers/s390/net/qeth_core_mpc.c
@@ -233,8 +233,7 @@ static struct ipa_rc_msg qeth_ipa_rc_msg[] = {
 char *qeth_get_ipa_msg(enum qeth_ipa_return_codes rc)
 {
 	int x = 0;
-	qeth_ipa_rc_msg[sizeof(qeth_ipa_rc_msg) /
-			sizeof(struct ipa_rc_msg) - 1].rc = rc;
+	qeth_ipa_rc_msg[ARRAY_SIZE(qeth_ipa_rc_msg) - 1].rc = rc;
 	while (qeth_ipa_rc_msg[x].rc != rc)
 		x++;
 	return qeth_ipa_rc_msg[x].msg;
@@ -281,9 +280,7 @@ static struct ipa_cmd_names qeth_ipa_cmd_names[] = {
 char *qeth_get_ipa_cmd_name(enum qeth_ipa_cmds cmd)
 {
 	int x = 0;
-	qeth_ipa_cmd_names[
-		sizeof(qeth_ipa_cmd_names) /
-			sizeof(struct ipa_cmd_names)-1].cmd = cmd;
+	qeth_ipa_cmd_names[ARRAY_SIZE(qeth_ipa_cmd_names) - 1].cmd = cmd;
 	while (qeth_ipa_cmd_names[x].cmd != cmd)
 		x++;
 	return qeth_ipa_cmd_names[x].name;
