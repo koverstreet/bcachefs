62617f29f031ff234bb1e03b9616c96d6ca7581f
Breno Leitao <leitao@debian.org>
2018-10-31 11:38:20 -0300

    selftests/powerpc: Allocate base registers
    BugLink: https://bugs.launchpad.net/bugs/1857158
    
    [ Upstream commit 5249497a7bb6334fcc128588d6a7e1e21786515a ]
    
    Some ptrace selftests are passing input operands using a constraint that
    can allocate any register for the operand, and using these registers on
    load/store operations.
    
    If the register allocated by the compiler happens to be zero (r0), it might
    cause an invalid memory address access, since load and store operations
    consider the content of 0x0 address if the base register is r0, instead of
    the content of the r0 register. For example:
    
    	r1 := 0xdeadbeef
    	r0 := 0xdeadbeef
    
    	ld r2, 0(1) /* will load into r2 the content of r1 address */
    	ld r2, 0(0) /* will load into r2 the content of 0x0 */
    
    In order to avoid this possible problem, the inline assembly constraint
    should be aware that these registers will be used as a base register, thus,
    r0 should not be allocated.
    
    Other than that, this patch removes inline assembly operands that are not
    used by the tests.
    
    Signed-off-by: Breno Leitao <leitao@debian.org>
    Reviewed-by: Segher Boessenkool <segher@kernel.crashing.org>
    Signed-off-by: Michael Ellerman <mpe@ellerman.id.au>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- tools/testing/selftests/powerpc/ptrace/ptrace-tm-spr.c
+++ tools/testing/selftests/powerpc/ptrace/ptrace-tm-spr.c
@@ -74,7 +74,7 @@ trans:
 
 		"3: ;"
 		: [tfhar] "=r" (tfhar), [res] "=r" (result),
-		[texasr] "=r" (texasr), [cptr1] "=r" (cptr1)
+		[texasr] "=r" (texasr), [cptr1] "=b" (cptr1)
 		: [sprn_texasr] "i"  (SPRN_TEXASR)
 		: "memory", "r0", "r1", "r2", "r3", "r4",
 		"r8", "r9", "r10", "r11", "r31"
