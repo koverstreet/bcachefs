84c099ab671e5487f787605be2e52008b03e4d35
Kent Overstreet <kent.overstreet@gmail.com>
2018-11-01 15:10:01 -0400

    bcachefs: Make bkey types globally unique
    this lets us get rid of a lot of extra switch statements - in a lot of
    places we dispatch on the btree node type, and then the key type, so
    this is a nice cleanup across a lot of code.
    
    Also improve the on disk format versioning stuff.

--- fs/bcachefs/opts.h
+++ fs/bcachefs/opts.h
@@ -181,6 +181,9 @@ enum opt_type {
 		NO_SB_OPT,			false)			\
 	BCH_OPT(nostart,		u8,	OPT_INTERNAL,		\
 		OPT_BOOL(),						\
+		NO_SB_OPT,			false)			\
+	BCH_OPT(version_upgrade,	u8,	OPT_MOUNT,		\
+		OPT_BOOL(),						\
 		NO_SB_OPT,			false)
 
 struct bch_opts {
