def3f826f8c22e87812cd1c11a7eeac3b8fad6df
Tim Schlueter <schlueter.tim@linux.com>
2018-11-03 12:01:16 -0700

    Fixed an out of bounds access for small btree node sizes

--- fs/bcachefs/btree_io.c
+++ fs/bcachefs/btree_io.c
@@ -1425,7 +1425,7 @@ void bch2_btree_node_read(struct bch_fs *c, struct btree *b,
 
 	ca = bch_dev_bkey_exists(c, pick.ptr.dev);
 
-	bio = bio_alloc_bioset(GFP_NOIO, btree_pages(c), &c->btree_bio);
+	bio = bio_alloc_bioset(GFP_NOIO, btree_pages(c) + 1, &c->btree_bio);
 	rb = container_of(bio, struct btree_read_bio, bio);
 	rb->c			= c;
 	rb->start_time		= local_clock();
