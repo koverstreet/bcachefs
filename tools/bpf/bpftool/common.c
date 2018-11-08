e7cfe3a26aceed246cbbfeb793d29b6570a368df
Quentin Monnet <quentin.monnet@netronome.com>
2018-11-08 11:52:27 +0000

    tools: bpftool: pass an argument to silence open_obj_pinned()
    BugLink: https://bugs.launchpad.net/bugs/1854975
    
    [ Upstream commit f120919f9905a2cad9dea792a28a11fb623f72c1 ]
    
    Function open_obj_pinned() prints error messages when it fails to open a
    link in the BPF virtual file system. However, in some occasions it is
    not desirable to print an error, for example when we parse all links
    under the bpffs root, and the error is due to some paths actually being
    symbolic links.
    
    Example output:
    
        # ls -l /sys/fs/bpf/
        lrwxrwxrwx 1 root root 0 Oct 18 19:00 ip -> /sys/fs/bpf/tc/
        drwx------ 3 root root 0 Oct 18 19:00 tc
        lrwxrwxrwx 1 root root 0 Oct 18 19:00 xdp -> /sys/fs/bpf/tc/
    
        # bpftool --bpffs prog show
        Error: bpf obj get (/sys/fs/bpf): Permission denied
        Error: bpf obj get (/sys/fs/bpf): Permission denied
    
        # strace -e bpf bpftool --bpffs prog show
        bpf(BPF_OBJ_GET, {pathname="/sys/fs/bpf/ip", bpf_fd=0}, 72) = -1 EACCES (Permission denied)
        Error: bpf obj get (/sys/fs/bpf): Permission denied
        bpf(BPF_OBJ_GET, {pathname="/sys/fs/bpf/xdp", bpf_fd=0}, 72) = -1 EACCES (Permission denied)
        Error: bpf obj get (/sys/fs/bpf): Permission denied
        ...
    
    To fix it, pass a bool as a second argument to the function, and prevent
    it from printing an error when the argument is set to true.
    
    Signed-off-by: Quentin Monnet <quentin.monnet@netronome.com>
    Reviewed-by: Jakub Kicinski <jakub.kicinski@netronome.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- tools/bpf/bpftool/common.c
+++ tools/bpf/bpftool/common.c
@@ -124,16 +124,17 @@ static int mnt_bpffs(const char *target, char *buff, size_t bufflen)
 	return 0;
 }
 
-int open_obj_pinned(char *path)
+int open_obj_pinned(char *path, bool quiet)
 {
 	int fd;
 
 	fd = bpf_obj_get(path);
 	if (fd < 0) {
-		p_err("bpf obj get (%s): %s", path,
-		      errno == EACCES && !is_bpffs(dirname(path)) ?
-		    "directory not in bpf file system (bpffs)" :
-		    strerror(errno));
+		if (!quiet)
+			p_err("bpf obj get (%s): %s", path,
+			      errno == EACCES && !is_bpffs(dirname(path)) ?
+			    "directory not in bpf file system (bpffs)" :
+			    strerror(errno));
 		return -1;
 	}
 
@@ -145,7 +146,7 @@ int open_obj_pinned_any(char *path, enum bpf_obj_type exp_type)
 	enum bpf_obj_type type;
 	int fd;
 
-	fd = open_obj_pinned(path);
+	fd = open_obj_pinned(path, false);
 	if (fd < 0)
 		return -1;
 
@@ -354,7 +355,7 @@ int build_pinned_obj_table(struct pinned_obj_table *tab,
 		while ((ftse = fts_read(fts))) {
 			if (!(ftse->fts_info & FTS_F))
 				continue;
-			fd = open_obj_pinned(ftse->fts_path);
+			fd = open_obj_pinned(ftse->fts_path, true);
 			if (fd < 0)
 				continue;
 
