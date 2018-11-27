101f2ce08f2d73d92a298f032b3f994f0e65028b
Kent Overstreet <kent.overstreet@gmail.com>
2018-11-27 11:27:19 -0500

    fixup! fs: insert_inode_locked2()

--- fs/inode.c
+++ fs/inode.c
@@ -1434,13 +1434,48 @@ EXPORT_SYMBOL(find_inode_nowait);
 
 int insert_inode_locked(struct inode *inode)
 {
-	struct inode *old = insert_inode_locked2(inode);
+	struct super_block *sb = inode->i_sb;
+	ino_t ino = inode->i_ino;
+	struct hlist_head *head = inode_hashtable + hash(sb, ino);
 
-	if (!old)
-		return 0;
-
-	iput(old);
-	return -EBUSY;
+	while (1) {
+		struct inode *old = NULL;
+		spin_lock(&inode_hash_lock);
+		hlist_for_each_entry(old, head, i_hash) {
+			if (old->i_ino != ino)
+				continue;
+			if (old->i_sb != sb)
+				continue;
+			spin_lock(&old->i_lock);
+			if (old->i_state & (I_FREEING|I_WILL_FREE)) {
+				spin_unlock(&old->i_lock);
+				continue;
+			}
+			break;
+		}
+		if (likely(!old)) {
+			spin_lock(&inode->i_lock);
+			inode->i_state |= I_NEW | I_CREATING;
+			hlist_add_head(&inode->i_hash, head);
+			spin_unlock(&inode->i_lock);
+			spin_unlock(&inode_hash_lock);
+			return 0;
+		}
+		if (unlikely(old->i_state & I_CREATING)) {
+			spin_unlock(&old->i_lock);
+			spin_unlock(&inode_hash_lock);
+			return -EBUSY;
+		}
+		__iget(old);
+		spin_unlock(&old->i_lock);
+		spin_unlock(&inode_hash_lock);
+		wait_on_inode(old);
+		if (unlikely(!inode_unhashed(old))) {
+			iput(old);
+			return -EBUSY;
+		}
+		iput(old);
+	}
 }
 EXPORT_SYMBOL(insert_inode_locked);
 
@@ -1472,11 +1507,6 @@ struct inode *insert_inode_locked2(struct inode *inode)
 			spin_unlock(&inode->i_lock);
 			spin_unlock(&inode_hash_lock);
 			return NULL;
-		}
-		if (unlikely(old->i_state & I_CREATING)) {
-			spin_unlock(&old->i_lock);
-			spin_unlock(&inode_hash_lock);
-			return -EBUSY;
 		}
 		__iget(old);
 		spin_unlock(&old->i_lock);
