c6b7e6a2e4336bb2b09f917a12a720333e510fd9
Marc Zyngier <marc.zyngier@arm.com>
2018-12-20 13:07:40 +0000

    arm/arm64: KVM: Don't panic on failure to properly reset system registers
    BugLink: https://bugs.launchpad.net/bugs/1837952
    
    [ Upstream commit 20589c8cc47dce5854c8bf1b44a9fc63d798d26d ]
    
    Failing to properly reset system registers is pretty bad. But not
    quite as bad as bringing the whole machine down... So warn loudly,
    but slightly more gracefully.
    
    Signed-off-by: Marc Zyngier <marc.zyngier@arm.com>
    Acked-by: Christoffer Dall <christoffer.dall@arm.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>

--- arch/arm64/kvm/sys_regs.c
+++ arch/arm64/kvm/sys_regs.c
@@ -2420,7 +2420,9 @@ void kvm_reset_sys_regs(struct kvm_vcpu *vcpu)
 	table = get_target_table(vcpu->arch.target, true, &num);
 	reset_sys_reg_descs(vcpu, table, num);
 
-	for (num = 1; num < NR_SYS_REGS; num++)
-		if (vcpu_sys_reg(vcpu, num) == 0x4242424242424242)
-			panic("Didn't reset vcpu_sys_reg(%zi)", num);
+	for (num = 1; num < NR_SYS_REGS; num++) {
+		if (WARN(vcpu_sys_reg(vcpu, num) == 0x4242424242424242,
+			 "Didn't reset vcpu_sys_reg(%zi)\n", num))
+			break;
+	}
 }
