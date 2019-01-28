3c60b80f5ab58d87583d75d2f0b6051b89a1ac90
Jussi Laako <jussi@sonarnerd.net>
2019-01-29 00:47:01 +0200

    ALSA: usb-audio: Cleanup DSD whitelist
    BugLink: https://bugs.launchpad.net/bugs/1852492
    
    [ Upstream commit 202e69e645545e8dcec5e239658125276a7a315a ]
    
    XMOS/Thesycon family of USB Audio Class firmware flags DSD altsetting
    separate from the PCM ones. Thus the DSD altsetting can be auto-detected
    based on the flag and doesn't need maintaining specific altsetting
    whitelist.
    
    In addition, static VID:PID-to-altsetting whitelisting causes problems
    when firmware update changes the altsetting, or same VID:PID is reused
    for another device that has different kind of firmware.
    
    This patch removes existing explicit whitelist mappings for XMOS VID
    (0x20b1) and Thesycon VID (0x152a).
    
    Also corrects placement of Hegel HD12 and NuPrime DAC-10 to keep list
    sorted based on VID.
    
    Signed-off-by: Jussi Laako <jussi@sonarnerd.net>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>

--- sound/usb/quirks.c
+++ sound/usb/quirks.c
@@ -1349,21 +1349,13 @@ u64 snd_usb_interface_dsd_format_quirks(struct snd_usb_audio *chip,
 
 	/* XMOS based USB DACs */
 	switch (chip->usb_id) {
-	case USB_ID(0x20b1, 0x3008): /* iFi Audio micro/nano iDSD */
-	case USB_ID(0x20b1, 0x2008): /* Matrix Audio X-Sabre */
-	case USB_ID(0x20b1, 0x300a): /* Matrix Audio Mini-i Pro */
 	case USB_ID(0x22d9, 0x0416): /* OPPO HA-1 */
 	case USB_ID(0x2772, 0x0230): /* Pro-Ject Pre Box S2 Digital */
 		if (fp->altsetting == 2)
 			return SNDRV_PCM_FMTBIT_DSD_U32_BE;
 		break;
 
-	case USB_ID(0x20b1, 0x000a): /* Gustard DAC-X20U */
-	case USB_ID(0x20b1, 0x2009): /* DIYINHK DSD DXD 384kHz USB to I2S/DSD */
-	case USB_ID(0x20b1, 0x2023): /* JLsounds I2SoverUSB */
-	case USB_ID(0x20b1, 0x3023): /* Aune X1S 32BIT/384 DSD DAC */
 	case USB_ID(0x10cb, 0x0103): /* The Bit Opus #3; with fp->dsd_raw */
-	case USB_ID(0x152a, 0x85de): /* SMSL D1 DAC */
 	case USB_ID(0x2616, 0x0106): /* PS Audio NuWave DAC */
 		if (fp->altsetting == 3)
 			return SNDRV_PCM_FMTBIT_DSD_U32_BE;
