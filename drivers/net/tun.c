b9cbbf6bef46ee0cfa32edf0db9b7916ceda754d
George Amanakis <gamanakis@gmail.com>
2019-01-29 22:50:13 -0500

    tun: move the call to tun_set_real_num_queues
    BugLink: https://bugs.launchpad.net/bugs/1837664
    
    [ Upstream commit 3a03cb8456cc1d61c467a5375e0a10e5207b948c ]
    
    Call tun_set_real_num_queues() after the increment of tun->numqueues
    since the former depends on it. Otherwise, the number of queues is not
    correctly accounted for, which results to warnings similar to:
    "vnet0 selects TX queue 11, but real number of TX queues is 11".
    
    Fixes: 0b7959b62573 ("tun: publish tfile after it's fully initialized")
    Reported-and-tested-by: George Amanakis <gamanakis@gmail.com>
    Signed-off-by: George Amanakis <gamanakis@gmail.com>
    Signed-off-by: Stanislav Fomichev <sdf@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>
    Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>

--- drivers/net/tun.c
+++ drivers/net/tun.c
@@ -765,8 +765,6 @@ static int tun_attach(struct tun_struct *tun, struct file *file,
 		tun_napi_init(tun, tfile, napi, napi_frags);
 	}
 
-	tun_set_real_num_queues(tun);
-
 	/* device is allowed to go away first, so no need to hold extra
 	 * refcnt.
 	 */
@@ -778,6 +776,7 @@ static int tun_attach(struct tun_struct *tun, struct file *file,
 	rcu_assign_pointer(tfile->tun, tun);
 	rcu_assign_pointer(tun->tfiles[tun->numqueues], tfile);
 	tun->numqueues++;
+	tun_set_real_num_queues(tun);
 out:
 	return err;
 }
