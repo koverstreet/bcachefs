abf9852b1f7a4185c54b03c5aa1b8b515fefcc90
Thomas Gleixner <tglx@linutronix.de>
2019-02-27 12:48:14 +0100

    x86/kvm/vmx: Add MDS protection when L1D Flush is not active
    CPUs which are affected by L1TF and MDS mitigate MDS with the L1D Flush on
    VMENTER when updated microcode is installed.
    
    If a CPU is not affected by L1TF or if the L1D Flush is not in use, then
    MDS mitigation needs to be invoked explicit.
    
    For these cases, follow the host mitigation state and invoke the MDS
    mitigation before VMENTER.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    CVE-2018-12126
    CVE-2018-12127
    CVE-2018-12130
    
    (backported from commit e3f1fbbd5ffb7ca71bf13d447983c03cf4e27030)
    [tyhicks: Backport to 4.18:
     - Adjust path of vmx.c which was changed in upstream commit a821bab2d1ee]
    Signed-off-by: Tyler Hicks <tyhicks@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -9734,6 +9734,8 @@ static void __noclone vmx_vcpu_run(struct kvm_vcpu *vcpu)
 
 	if (static_branch_unlikely(&vmx_l1d_should_flush))
 		vmx_l1d_flush(vcpu);
+	else if (static_branch_unlikely(&mds_user_clear))
+		mds_clear_cpu_buffers();
 
 	asm(
 		/* Store host registers */
