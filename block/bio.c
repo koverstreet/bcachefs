132e59c1c24cc442245dc5005726c181b85dc7f0
Kent Overstreet <kent.overstreet@gmail.com>
2019-04-04 12:02:01 -0400

    block: Add some exports for bcachefs
    bcachefs has its own direct IO code.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- block/bio.c
+++ block/bio.c
@@ -1361,6 +1361,7 @@ void bio_set_pages_dirty(struct bio *bio)
 			set_page_dirty_lock(bvec->bv_page);
 	}
 }
+EXPORT_SYMBOL_GPL(bio_set_pages_dirty);
 
 /*
  * bio_check_pages_dirty() will check that all the BIO's pages are still dirty.
@@ -1420,6 +1421,7 @@ defer:
 	spin_unlock_irqrestore(&bio_dirty_lock, flags);
 	schedule_work(&bio_dirty_work);
 }
+EXPORT_SYMBOL_GPL(bio_check_pages_dirty);
 
 static inline bool bio_remaining_done(struct bio *bio)
 {
