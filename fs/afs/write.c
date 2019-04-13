9fe2a5c72a2934766297f1bd093eb0b711ce0c93
Marc Dionne <marc.dionne@auristor.com>
2019-04-13 08:37:37 +0100

    afs: Unlock pages for __pagevec_release()
    BugLink: https://bugs.launchpad.net/bugs/1838576
    
    [ Upstream commit 21bd68f196ca91fc0f3d9bd1b32f6e530e8c1c88 ]
    
    __pagevec_release() complains loudly if any page in the vector is still
    locked.  The pages need to be locked for generic_error_remove_page(), but
    that function doesn't actually unlock them.
    
    Unlock the pages afterwards.
    
    Signed-off-by: Marc Dionne <marc.dionne@auristor.com>
    Signed-off-by: David Howells <dhowells@redhat.com>
    Tested-by: Jonathan Billings <jsbillin@umich.edu>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>

--- fs/afs/write.c
+++ fs/afs/write.c
@@ -247,6 +247,7 @@ static void afs_kill_pages(struct address_space *mapping,
 				first = page->index + 1;
 			lock_page(page);
 			generic_error_remove_page(mapping, page);
+			unlock_page(page);
 		}
 
 		__pagevec_release(&pv);
