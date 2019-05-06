60872709e5f457b6746cf472c75f616b4c884b1a
Kleber Sacilotto de Souza <kleber.souza@canonical.com>
2019-05-06 17:28:21 +0200

    UBUNTU: [Packaging] remove snapdragon dead files
    BugLink: https://bugs.launchpad.net/bugs/1827880
    
    Remove all files and references that were previously used to build the
    snapdragon binaries. They are not needed anymore since we have split it
    to its own topic branch.
    
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>
    Acked-by: Connor Kuehl <connor.kuehl@canonical.com>
    Acked-by: Thadeu Lima de Souza Cascardo <cascardo@canonical.com>
    Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>

--- debian.master/rules.d/arm64.mk
+++ debian.master/rules.d/arm64.mk
@@ -8,11 +8,6 @@ kernel_file	= arch/$(build_arch)/boot/Image.gz
 install_file	= vmlinuz
 no_dumpfile = true
 
-# The uboot used in ubuntu core can't handle Image.gz, so
-# create this flavour to generate a Image just for them
-build_image_snapdragon	= Image
-kernel_file_snapdragon	= arch/$(build_arch)/boot/Image
-
 loader		= grub
 vdso		= vdso_install
 
