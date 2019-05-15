a5fd539aa68cb537035e9586c9e9a5ef5931da3b
Kent Overstreet <kent.overstreet@gmail.com>
2019-05-15 10:54:43 -0400

    bcachefs: More work to avoid transaction restarts
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- fs/bcachefs/acl.c
+++ fs/bcachefs/acl.c
@@ -222,7 +222,7 @@ struct posix_acl *bch2_get_acl(struct user_namespace *mnt_userns,
 	struct bkey_s_c_xattr xattr;
 	struct posix_acl *acl = NULL;
 
-	bch2_trans_init(&trans, c);
+	bch2_trans_init(&trans, c, 0, 0);
 retry:
 	bch2_trans_begin(&trans);
 
@@ -305,7 +305,7 @@ int bch2_set_acl(struct user_namespace *mnt_userns,
 	int ret;
 
 	mutex_lock(&inode->ei_update_lock);
-	bch2_trans_init(&trans, c);
+	bch2_trans_init(&trans, c, 0, 0);
 
 	if (type == ACL_TYPE_ACCESS && acl) {
 		ret = posix_acl_update_mode(mnt_userns, &inode->v, &mode, &acl);
