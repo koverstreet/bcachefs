e8f55d436c3b12333f8f41bcbf63e2ca5ce57556
Keith Busch <keith.busch@intel.com>
2019-05-27 01:29:01 +0900

    nvme: Export get and set features
    BugLink: https://bugs.launchpad.net/bugs/1808957
    
    Future use intends to make use of both, so export these functions. And
    since their implementation is identical except for the opcode, provide a
    new function that implement both.
    
    [akinobu.mita@gmail.com>: fix line over 80 characters]
    Signed-off-by: Keith Busch <keith.busch@intel.com>
    Signed-off-by: Akinobu Mita <akinobu.mita@gmail.com>
    Reviewed-by: Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
    Reviewed-by: Minwoo Im <minwoo.im.dev@gmail.com>
    Signed-off-by: Sagi Grimberg <sagi@grimberg.me>
    (backported from commit 82b17aebf68d572d73359a68376024ec1b5294c5 git://git.infradead.org/nvme nvme-5.3)
    Signed-off-by: Kai-Heng Feng <kai.heng.feng@canonical.com>
    Acked-by: Timo Aaltonen <tjaalton@ubuntu.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- drivers/nvme/host/nvme.h
+++ drivers/nvme/host/nvme.h
@@ -400,6 +400,12 @@ int __nvme_submit_sync_cmd(struct request_queue *q, struct nvme_command *cmd,
 		union nvme_result *result, void *buffer, unsigned bufflen,
 		unsigned timeout, int qid, int at_head,
 		blk_mq_req_flags_t flags);
+int nvme_set_features(struct nvme_ctrl *dev, unsigned int fid,
+		      unsigned int dword11, void *buffer, size_t buflen,
+		      u32 *result);
+int nvme_get_features(struct nvme_ctrl *dev, unsigned int fid,
+		      unsigned int dword11, void *buffer, size_t buflen,
+		      u32 *result);
 int nvme_set_queue_count(struct nvme_ctrl *ctrl, int *count);
 void nvme_start_keep_alive(struct nvme_ctrl *ctrl);
 void nvme_stop_keep_alive(struct nvme_ctrl *ctrl);
