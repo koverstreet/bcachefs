c2d04495fdb8863f89e2aabb11a350b4c61a5fbf
Hsin-Yi Wang <hsinyi@chromium.org>
2019-05-29 18:25:55 +0800

    drm/mediatek: clear num_pipes when unbind driver
    BugLink: https://bugs.launchpad.net/bugs/1838824
    
    [ Upstream commit a4cd1d2b016d5d043ab2c4b9c4ec50a5805f5396 ]
    
    num_pipes is used for mutex created in mtk_drm_crtc_create(). If we
    don't clear num_pipes count, when rebinding driver, the count will
    be accumulated. From mtk_disp_mutex_get(), there can only be at most
    10 mutex id. Clear this number so it starts from 0 in every rebind.
    
    Fixes: 119f5173628a ("drm/mediatek: Add DRM Driver for Mediatek SoC MT8173.")
    Signed-off-by: Hsin-Yi Wang <hsinyi@chromium.org>
    Signed-off-by: CK Hu <ck.hu@mediatek.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>

--- drivers/gpu/drm/mediatek/mtk_drm_drv.c
+++ drivers/gpu/drm/mediatek/mtk_drm_drv.c
@@ -353,6 +353,7 @@ static void mtk_drm_unbind(struct device *dev)
 	drm_dev_unregister(private->drm);
 	mtk_drm_kms_deinit(private->drm);
 	drm_dev_unref(private->drm);
+	private->num_pipes = 0;
 	private->drm = NULL;
 }
 
