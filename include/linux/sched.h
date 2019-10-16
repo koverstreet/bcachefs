819244893218df59beb13a25b1a41e31ea70246c
Kent Overstreet <kent.overstreet@gmail.com>
2019-10-16 15:03:50 -0400

    sched: Add task_struct->faults_disabled_mapping
    This is needed to fix a page cache coherency issue with O_DIRECT writes.
    
    O_DIRECT writes (and other filesystem operations that modify file data
    while bypassing the page cache) need to shoot down ranges of the page
    cache - and additionally, need locking to prevent those pages from
    pulled back in.
    
    But O_DIRECT writes invoke the page fault handler (via get_user_pages),
    and the page fault handler will need to take that same lock - this is a
    classic recursive deadlock if userspace has mmaped the file they're DIO
    writing to and uses those pages for the buffer to write from, and it's a
    lock ordering deadlock in general.
    
    Thus we need a way to signal from the dio code to the page fault handler
    when we already are holding the pagecache add lock on an address space -
    this patch just adds a member to task_struct for this purpose. For now
    only bcachefs is implementing this locking, though it may be moved out
    of bcachefs and made available to other filesystems in the future.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -857,6 +857,7 @@ struct task_struct {
 
 	struct mm_struct		*mm;
 	struct mm_struct		*active_mm;
+	struct address_space		*faults_disabled_mapping;
 
 	/* Per-thread vma caching: */
 	struct vmacache			vmacache;
