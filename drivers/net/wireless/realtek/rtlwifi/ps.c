1bf07a28686b8e022517e8a7b206282668d90e68
Tyler Hicks <tyhicks@canonical.com>
2019-10-18 09:13:00 +0200

    UBUNTU: SAUCE: rtlwifi: Fix potential overflow on P2P code
    Nicolas Waisman noticed that even though noa_len is checked for
    a compatible length it's still possible to overrun the buffers
    of p2pinfo since there's no check on the upper bound of noa_num.
    Bounds check noa_num against P2P_MAX_NOA_NUM using the minimum of the
    two.
    
    CVE-2019-17666
    
    Reported-by: Nicolas Waisman <nico@semmle.com>
    Suggested-by: Ping-Ke Shih <pkshih@realtek.com>
    [tyhicks: Reuse nearly all of a commit message written by Laura Abbott]
    Signed-off-by: Tyler Hicks <tyhicks@canonical.com>
    Acked-by: Andrea Righi <andrea.righi@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- drivers/net/wireless/realtek/rtlwifi/ps.c
+++ drivers/net/wireless/realtek/rtlwifi/ps.c
@@ -773,7 +773,8 @@ static void rtl_p2p_noa_ie(struct ieee80211_hw *hw, void *data,
 					 noa_len);
 				return;
 			} else {
-				noa_num = (noa_len - 2) / 13;
+				noa_num = min((noa_len - 2) / 13,
+					      P2P_MAX_NOA_NUM);
 			}
 			noa_index = ie[3];
 			if (rtlpriv->psc.p2p_ps_info.p2p_ps_mode ==
@@ -867,7 +868,8 @@ static void rtl_p2p_action_ie(struct ieee80211_hw *hw, void *data,
 					 noa_len);
 				return;
 			} else {
-				noa_num = (noa_len - 2) / 13;
+				noa_num = min((noa_len - 2) / 13,
+					      P2P_MAX_NOA_NUM);
 			}
 			noa_index = ie[3];
 			if (rtlpriv->psc.p2p_ps_info.p2p_ps_mode ==
