460a7d9255c2dff4a6ebf7645011fecf47d5d9e2
Sean Christopherson <sean.j.christopherson@intel.com>
2019-12-18 13:55:05 -0800

    KVM: x86: Remove spurious kvm_mmu_unload() from vcpu destruction path
    BugLink: https://bugs.launchpad.net/bugs/1868623
    
    commit 9d979c7e6ff43ca3200ffcb74f57415fd633a2da upstream.
    
    x86 does not load its MMU until KVM_RUN, which cannot be invoked until
    after vCPU creation succeeds.  Given that kvm_arch_vcpu_destroy() is
    called if and only if vCPU creation fails, it is impossible for the MMU
    to be loaded.
    
    Note, the bogus kvm_mmu_unload() call was added during an unrelated
    refactoring of vCPU allocation, i.e. was presumably added as an
    opportunstic "fix" for a perceived leak.
    
    Fixes: fb3f0f51d92d1 ("KVM: Dynamically allocate vcpus")
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -8095,13 +8095,7 @@ void kvm_arch_vcpu_postcreate(struct kvm_vcpu *vcpu)
 
 void kvm_arch_vcpu_destroy(struct kvm_vcpu *vcpu)
 {
-	int r;
 	vcpu->arch.apf.msr_val = 0;
-
-	r = vcpu_load(vcpu);
-	BUG_ON(r);
-	kvm_mmu_unload(vcpu);
-	vcpu_put(vcpu);
 
 	kvm_arch_vcpu_free(vcpu);
 }
