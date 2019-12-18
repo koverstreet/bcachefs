6d3befd2ff4f18c780c24c3ba0b4afbdc19811c9
dann frazier <dann.frazier@canonical.com>
2019-12-18 07:31:26 -0700

    UBUNTU: SAUCE: md/raid0: Link to wiki with guidance on multi-zone RAID0 layout migration
    BugLink: https://bugs.launchpad.net/bugs/1850540
    
    Helping an administrator understand this issue and how to deal with it
    requires more text than achievable in a kernel error message. Let's
    clarify the issue in the Ubuntu wiki, and have the kernel emit a link
    to it.
    
    I've submitted a similar change upstream:
      https://marc.info/?l=linux-raid&m=157360088014027&w=2
    Should it get merged, we should consider replacing this patch with that one.
    Otherwise, it is probably safe to drop this SAUCE patch after focal.
    
    Signed-off-by: dann frazier <dann.frazier@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Marcelo Henrique Cerri <marcelo.cerri@canonical.com>
    Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>

--- drivers/md/raid0.c
+++ drivers/md/raid0.c
@@ -162,6 +162,8 @@ static int create_strip_zones(struct mddev *mddev, struct r0conf **private_conf)
 		pr_err("md/raid0:%s: cannot assemble multi-zone RAID0 with default_layout setting\n",
 		       mdname(mddev));
 		pr_err("md/raid0: please set raid0.default_layout to 1 or 2\n");
+		pr_err("md/raid0: Read the following page for more information:\n");
+		pr_err("md/raid0: https://wiki.ubuntu.com/Kernel/Raid0LayoutMigration\n");
 		err = -ENOTSUPP;
 		goto abort;
 	}
