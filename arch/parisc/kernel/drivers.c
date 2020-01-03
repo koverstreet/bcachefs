19a75c1d552421a2c9e4de6f0bb99467f6fd3bd0
Krzysztof Kozlowski <krzk@kernel.org>
2020-01-03 17:39:25 +0100

    parisc: Use proper printk format for resource_size_t
    BugLink: https://bugs.launchpad.net/bugs/1864904
    
    [ Upstream commit 4f80b70e1953cb846dbdd1ce72cb17333d4c8d11 ]
    
    resource_size_t should be printed with its own size-independent format
    to fix warnings when compiling on 64-bit platform (e.g. with
    COMPILE_TEST):
    
        arch/parisc/kernel/drivers.c: In function 'print_parisc_device':
        arch/parisc/kernel/drivers.c:892:9: warning:
            format '%p' expects argument of type 'void *',
            but argument 4 has type 'resource_size_t {aka unsigned int}' [-Wformat=]
    
    Signed-off-by: Krzysztof Kozlowski <krzk@kernel.org>
    Signed-off-by: Helge Deller <deller@gmx.de>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>

--- arch/parisc/kernel/drivers.c
+++ arch/parisc/kernel/drivers.c
@@ -875,8 +875,8 @@ static void print_parisc_device(struct parisc_device *dev)
 	static int count;
 
 	print_pa_hwpath(dev, hw_path);
-	printk(KERN_INFO "%d. %s at 0x%px [%s] { %d, 0x%x, 0x%.3x, 0x%.5x }",
-		++count, dev->name, (void*) dev->hpa.start, hw_path, dev->id.hw_type,
+	printk(KERN_INFO "%d. %s at %pap [%s] { %d, 0x%x, 0x%.3x, 0x%.5x }",
+		++count, dev->name, &(dev->hpa.start), hw_path, dev->id.hw_type,
 		dev->id.hversion_rev, dev->id.hversion, dev->id.sversion);
 
 	if (dev->num_addrs) {
