ce0108d298bd20011c5557fd77a8d7973fa245c6
Juerg Haefliger <juerg.haefliger@canonical.com>
2020-01-22 08:32:00 +0100

    UBUNTU: [Packaging] Fix config file assembly
    Commit 'UBUNTU: [Packaging] dkms -- switch to a consistent build prefix
    length and strip' introduced a helper tool fix-filenames which was added as
    a prerequisite to the $(stampdir)/stamp-prepare-tree-% rule. This rule
    generates the config file by concatenating all prereqs together
    (cat $^ ...) including the fix-filenames binary blob which results in
    warnings like:
    
    scripts/kconfig/conf  --silentoldconfig Kconfig
    .config:8536:warning: unexpected data: ^?ELF^B^A^A
    .config:8073:warning: unexpected data:
    .config:8537:warning: unexpected data:
    .config:8538:warning: unexpected data:
    .config:8539:warning: unexpected data:
    
    This is certainly not what we want, fix it.
    
    Ignore: yes
    Signed-off-by: Juerg Haefliger <juergh@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Seth Forshee <seth.forshee@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- debian/rules.d/2-binary-arch.mk
+++ debian/rules.d/2-binary-arch.mk
@@ -30,7 +30,7 @@ $(stampdir)/stamp-prepare-tree-%: $(commonconfdir)/config.common.$(family) $(arc
 	touch $(builddir)/build-$*/ubuntu-build
 	[ "$(do_full_source)" != 'true' ] && true || \
 		rsync -a --exclude debian --exclude debian.master --exclude $(DEBIAN) * $(builddir)/build-$*
-	cat $^ | sed -e 's/.*CONFIG_VERSION_SIGNATURE.*/CONFIG_VERSION_SIGNATURE="Ubuntu $(release)-$(revision)-$* $(raw_kernelversion)"/' > $(builddir)/build-$*/.config
+	cat $(wordlist 1,3,$^) | sed -e 's/.*CONFIG_VERSION_SIGNATURE.*/CONFIG_VERSION_SIGNATURE="Ubuntu $(release)-$(revision)-$* $(raw_kernelversion)"/' > $(builddir)/build-$*/.config
 	find $(builddir)/build-$* -name "*.ko" | xargs rm -f
 	$(build_cd) $(kmake) $(build_O) -j1 silentoldconfig prepare scripts
 	touch $@
