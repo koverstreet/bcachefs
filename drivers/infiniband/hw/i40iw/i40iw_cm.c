7c68d77b1225f19dc65f4bb39a196d50bee5c91a
Denis V. Lunev <den@openvz.org>
2020-04-28 16:15:11 +0300

    IB/i40iw: Remove bogus call to netdev_master_upper_dev_get()
    BugLink: https://bugs.launchpad.net/bugs/1883167
    
    [ Upstream commit 856ec7f64688387b100b7083cdf480ce3ac41227 ]
    
    Local variable netdev is not used in these calls.
    
    It should be noted, that this change is required to work in bonded mode.
    Otherwise we would get the following assert:
    
     "RTNL: assertion failed at net/core/dev.c (5665)"
    
    With the calltrace as follows:
    	dump_stack+0x19/0x1b
    	netdev_master_upper_dev_get+0x61/0x70
    	i40iw_addr_resolve_neigh+0x1e8/0x220
    	i40iw_make_cm_node+0x296/0x700
    	? i40iw_find_listener.isra.10+0xcc/0x110
    	i40iw_receive_ilq+0x3d4/0x810
    	i40iw_puda_poll_completion+0x341/0x420
    	i40iw_process_ceq+0xa5/0x280
    	i40iw_ceq_dpc+0x1e/0x40
    	tasklet_action+0x83/0x140
    	__do_softirq+0x125/0x2bb
    	call_softirq+0x1c/0x30
    	do_softirq+0x65/0xa0
    	irq_exit+0x105/0x110
    	do_IRQ+0x56/0xf0
    	common_interrupt+0x16a/0x16a
    	? cpuidle_enter_state+0x57/0xd0
    	cpuidle_idle_call+0xde/0x230
    	arch_cpu_idle+0xe/0xc0
    	cpu_startup_entry+0x14a/0x1e0
    	start_secondary+0x1f7/0x270
    	start_cpu+0x5/0x14
    
    Link: https://lore.kernel.org/r/20200428131511.11049-1-den@openvz.org
    Signed-off-by: Denis V. Lunev <den@openvz.org>
    Acked-by: Shiraz Saleem <shiraz.saleem@intel.com>
    Signed-off-by: Jason Gunthorpe <jgg@mellanox.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>

--- drivers/infiniband/hw/i40iw/i40iw_cm.c
+++ drivers/infiniband/hw/i40iw/i40iw_cm.c
@@ -1961,7 +1961,6 @@ static int i40iw_addr_resolve_neigh(struct i40iw_device *iwdev,
 	struct rtable *rt;
 	struct neighbour *neigh;
 	int rc = arpindex;
-	struct net_device *netdev = iwdev->netdev;
 	__be32 dst_ipaddr = htonl(dst_ip);
 	__be32 src_ipaddr = htonl(src_ip);
 
@@ -1970,9 +1969,6 @@ static int i40iw_addr_resolve_neigh(struct i40iw_device *iwdev,
 		i40iw_pr_err("ip_route_output\n");
 		return rc;
 	}
-
-	if (netif_is_bond_slave(netdev))
-		netdev = netdev_master_upper_dev_get(netdev);
 
 	neigh = dst_neigh_lookup(&rt->dst, &dst_ipaddr);
 
@@ -2039,7 +2035,6 @@ static int i40iw_addr_resolve_neigh_ipv6(struct i40iw_device *iwdev,
 {
 	struct neighbour *neigh;
 	int rc = arpindex;
-	struct net_device *netdev = iwdev->netdev;
 	struct dst_entry *dst;
 	struct sockaddr_in6 dst_addr;
 	struct sockaddr_in6 src_addr;
@@ -2059,9 +2054,6 @@ static int i40iw_addr_resolve_neigh_ipv6(struct i40iw_device *iwdev,
 		}
 		return rc;
 	}
-
-	if (netif_is_bond_slave(netdev))
-		netdev = netdev_master_upper_dev_get(netdev);
 
 	neigh = dst_neigh_lookup(dst, &dst_addr);
 
