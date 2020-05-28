2c26f14f67fdec1817d162142464b111f5730b28
Julian Wiedmann <jwi@linux.vnet.ibm.com>
2020-05-28 14:35:19 +0200

    s390/qeth: improve fallback to random MAC address
    BugLink: https://bugs.launchpad.net/bugs/1880834
    
    If READ MAC fails to fetch a valid MAC address, allow some more device
    types (IQD and z/VM OSD) to fall back to a random address.
    Also use eth_hw_addr_random(), for indicating to userspace that the
    address type is NET_ADDR_RANDOM.
    
    Note that while z/VM has various protection schemes to prohibit
    custom addresses on its NICs, they are all optional. So we should at
    least give it a try.
    
    Signed-off-by: Julian Wiedmann <jwi@linux.vnet.ibm.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (backported from commit 21b1702af12eb62bb40d994c9c1546ecacfc8456)
    [ jwi: backport to Ubuntu 4.15. Don't use eth_hw_addr_random(),
          it can trigger races on this old code level. ]
    Signed-off-by: Julian Wiedmann <jwi@linux.ibm.com>
    Signed-off-by: Frank Heimes <frank.heimes@canonical.com>
    Acked-by: Kleber Souza <kleber.souza@canonical.com>
    Acked-by: Kelsey Skunberg <kelsey.skunberg@canonical.com>
    Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>

--- drivers/s390/net/qeth_l2_main.c
+++ drivers/s390/net/qeth_l2_main.c
@@ -503,16 +503,21 @@ static int qeth_l2_request_initial_mac(struct qeth_card *card)
 	    card->info.type == QETH_CARD_TYPE_OSX ||
 	    card->info.guestlan) {
 		rc = qeth_setadpparms_change_macaddr(card);
-		if (rc) {
-			QETH_DBF_MESSAGE(2, "couldn't get MAC address on "
-				"device %x: %#x\n", CARD_DEVID(card), rc);
-			QETH_DBF_TEXT_(SETUP, 2, "1err%04x", rc);
-			return rc;
-		}
-	} else {
-		eth_random_addr(card->dev->dev_addr);
-		memcpy(card->dev->dev_addr, vendor_pre, 3);
+		if (!rc)
+			goto out;
+		QETH_DBF_MESSAGE(2, "READ_MAC Assist failed on device %x: %#x\n",
+				 CARD_DEVID(card), rc);
+		QETH_DBF_TEXT_(SETUP, 2, "1err%04x", rc);
+		/* fall back once more: */
 	}
+
+	/* some devices don't support a custom MAC address: */
+	if (card->info.type == QETH_CARD_TYPE_OSM ||
+	    card->info.type == QETH_CARD_TYPE_OSX)
+		return (rc) ? rc : -EADDRNOTAVAIL;
+	eth_random_addr(card->dev->dev_addr);
+	memcpy(card->dev->dev_addr, vendor_pre, 3);
+
 out:
 	QETH_DBF_HEX(SETUP, 2, card->dev->dev_addr, card->dev->addr_len);
 	return 0;
