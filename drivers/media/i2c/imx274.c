e599566ee3ef973197e7dca838500cea25725fbc
Hans Verkuil <hverkuil@xs4all.nl>
2020-07-03 11:20:32 +0200

    media: imx274: fix frame interval handling
    BugLink: https://bugs.launchpad.net/bugs/1904791
    
    [ Upstream commit 49b20d981d723fae5a93843c617af2b2c23611ec ]
    
    1) the numerator and/or denominator might be 0, in that case
       fall back to the default frame interval. This is per the spec
       and this caused a v4l2-compliance failure.
    
    2) the updated frame interval wasn't returned in the s_frame_interval
       subdev op.
    
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Reviewed-by: Luca Ceresoli <luca@lucaceresoli.net>
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Signed-off-by: Mauro Carvalho Chehab <mchehab+huawei@kernel.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: William Breathitt Gray <william.gray@canonical.com>

--- drivers/media/i2c/imx274.c
+++ drivers/media/i2c/imx274.c
@@ -970,6 +970,8 @@ static int imx274_s_frame_interval(struct v4l2_subdev *sd,
 	ret = imx274_set_frame_interval(imx274, fi->interval);
 
 	if (!ret) {
+		fi->interval = imx274->frame_interval;
+
 		/*
 		 * exposure time range is decided by frame interval
 		 * need to update it after frame interal changes
@@ -1541,9 +1543,9 @@ static int imx274_set_frame_interval(struct stimx274 *priv,
 		__func__, frame_interval.numerator,
 		frame_interval.denominator);
 
-	if (frame_interval.numerator == 0) {
-		err = -EINVAL;
-		goto fail;
+	if (frame_interval.numerator == 0 || frame_interval.denominator == 0) {
+		frame_interval.denominator = IMX274_DEF_FRAME_RATE;
+		frame_interval.numerator = 1;
 	}
 
 	req_frame_rate = (u32)(frame_interval.denominator
