6892f49252e465ea6e72e904b64f694cac59acfc
Yunsheng Lin <linyunsheng@huawei.com>
2020-07-09 15:31:42 +0800

    net: hns3: unify maybe_stop_tx for TSO and non-TSO case
    BugLink: https://launchpad.net/bugs/1859756
    
    Currently, maybe_stop_tx ops for TSO and non-TSO case share some BD
    calculation code, so this patch unifies the maybe_stop_tx by removing
    the maybe_stop_tx ops. skb_is_gso() can be used to differentiate the
    case between TSO and non-TSO case if there is need to handle special
    case for TSO case.
    
    This patch also add tx_copy field in "ethtool --statistics" to help
    better debug the performance issue caused by calling skb_copy.
    
    Signed-off-by: Yunsheng Lin <linyunsheng@huawei.com>
    Signed-off-by: Huazhong Tan <tanhuazhong@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (backported from commit 3d5f3741895291d3317e33718d96eb78294c8941)
    Signed-off-by: Ike Panhc <ike.pan@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Acked-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Kelsey Skunberg <kelsey.skunberg@canonical.com>

--- drivers/net/ethernet/hisilicon/hns3/hns3_enet.h
+++ drivers/net/ethernet/hisilicon/hns3/hns3_enet.h
@@ -367,6 +367,7 @@ struct ring_stats {
 			u64 tx_err_cnt;
 			u64 restart_queue;
 			u64 tx_busy;
+			u64 tx_copy;
 		};
 		struct {
 			u64 rx_pkts;
@@ -433,11 +434,6 @@ struct hns3_nic_ring_data {
 	int (*poll_one)(struct hns3_nic_ring_data *, int, void *);
 	void (*ex_process)(struct hns3_nic_ring_data *, struct sk_buff *);
 	void (*fini_process)(struct hns3_nic_ring_data *);
-};
-
-struct hns3_nic_ops {
-	int (*maybe_stop_tx)(struct sk_buff **out_skb,
-			     int *bnum, struct hns3_enet_ring *ring);
 };
 
 enum hns3_flow_level_range {
@@ -529,7 +525,6 @@ struct hns3_nic_priv {
 	u32 port_id;
 	struct net_device *netdev;
 	struct device *dev;
-	struct hns3_nic_ops ops;
 
 	/**
 	 * the cb for nic to manage the ring buffer, the first half of the
