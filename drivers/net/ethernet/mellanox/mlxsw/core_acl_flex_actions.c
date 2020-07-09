koverstreet/bcachefs

commit 2b8b72daf074590874c0a6145cf2ac86af8cbe91

mlxsw: Fix some IS_ERR() vs NULL bugs
BugLink: https://bugs.launchpad.net/bugs/1877461

[ Upstream commit c391eb8366ae052d571bb2841f1ccb4d39f3ceb8 ]

The mlxsw_sp_acl_rulei_create() function is supposed to return an error
pointer from mlxsw_afa_block_create().  The problem is that these
functions both return NULL instead of error pointers.  Half the callers
expect NULL and half expect error pointers so it could lead to a NULL
dereference on failure.

This patch changes both of them to return error pointers and changes all
the callers which checked for NULL to check for IS_ERR() instead.

Fixes: 4cda7d8d7098 ("mlxsw: core: Introduce flexible actions support")
Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
Reviewed-by: Ido Schimmel <idosch@mellanox.com>
Signed-off-by: David S. Miller <davem@davemloft.net>
Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
Signed-off-by: Kamal Mostafa <kamal@canonical.com>
Signed-off-by: Stefan Bader <stefan.bader@canonical.com>

drivers/net/ethernet/mellanox/mlxsw/core_acl_flex_actions.c
@@ -332,7 +332,7 @@ struct mlxsw_afa_block *mlxsw_afa_block_create(struct mlxsw_afa *mlxsw_afa)
 
 err_first_set_create:
 	kfree(block);
-	return NULL;
+	return ERR_PTR(-ENOMEM);
 }
 EXPORT_SYMBOL(mlxsw_afa_block_create);
 

drivers/net/ethernet/mellanox/mlxsw/spectrum_acl.c
@@ -331,7 +331,8 @@ mlxsw_sp_acl_rulei_create(struct mlxsw_sp_acl *acl)
 
 	rulei = kzalloc(sizeof(*rulei), GFP_KERNEL);
 	if (!rulei)
-		return NULL;
+		return ERR_PTR(-ENOMEM);
+
 	rulei->act_block = mlxsw_afa_block_create(acl->mlxsw_sp->afa);
 	if (IS_ERR(rulei->act_block)) {
 		err = PTR_ERR(rulei->act_block);

drivers/net/ethernet/mellanox/mlxsw/spectrum_mr_tcam.c
@@ -240,8 +240,8 @@ mlxsw_sp_mr_tcam_afa_block_create(struct mlxsw_sp *mlxsw_sp,
 	int err;
 
 	afa_block = mlxsw_afa_block_create(mlxsw_sp->afa);
-	if (!afa_block)
-		return ERR_PTR(-ENOMEM);
+	if (IS_ERR(afa_block))
+		return afa_block;
 
 	err = mlxsw_afa_block_append_counter(afa_block, counter_index);
 	if (err)

commit a7ffdfc73e5fab3ea92e72f090208acf70bca336

UBUNTU: SAUCE: mlxsw: Add missmerged ERR_PTR hunk
BugLink: https://bugs.launchpad.net/bugs/1877461

While backporting this stable patch to Ubuntu 18.04/Bionic,
a hunk went missing which causes mlxsw_afa_block_create() to
return NULL still in some cases.

Fixes: 2b8b72daf074 ("mlxsw: Fix some IS_ERR() vs NULL bugs")
Reported-by: Vegard Nossum <vegard.nossum@oracle.com>
Signed-off-by: Stefan Bader <stefan.bader@canonical.com>
Acked-by: Kleber Souza <kleber.souza@canonical.com>
Acked-by: Kamal Mostafa <kamal@canonical.com>
Signed-off-by: Khalid Elmously <khalid.elmously@canonical.com>

drivers/net/ethernet/mellanox/mlxsw/core_acl_flex_actions.c
@@ -319,7 +319,7 @@ struct mlxsw_afa_block *mlxsw_afa_block_create(struct mlxsw_afa *mlxsw_afa)
 
 	block = kzalloc(sizeof(*block), GFP_KERNEL);
 	if (!block)
-		return NULL;
+		return ERR_PTR(-ENOMEM);
 	INIT_LIST_HEAD(&block->fwd_entry_ref_list);
 	block->afa = mlxsw_afa;
 