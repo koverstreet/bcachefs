6e3113c41daf499572425b1ee29a97b5e838bcdf
Kent Overstreet <kent.overstreet@gmail.com>
2020-10-11 16:33:49 -0400

    bcachefs: Fix a lockdep splat
    We can't allocate memory with GFP_FS while holding the btree cache lock,
    and vfree() can allocate memory.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/btree_cache.c
+++ fs/bcachefs/btree_cache.c
@@ -351,11 +351,13 @@ void bch2_fs_btree_cache_exit(struct bch_fs *c)
 {
 	struct btree_cache *bc = &c->btree_cache;
 	struct btree *b;
-	unsigned i;
+	unsigned i, flags;
 
 	if (bc->shrink.list.next)
 		unregister_shrinker(&bc->shrink);
 
+	/* vfree() can allocate memory: */
+	flags = memalloc_nofs_save();
 	mutex_lock(&bc->lock);
 
 #ifdef CONFIG_BCACHEFS_DEBUG
@@ -391,6 +393,7 @@ void bch2_fs_btree_cache_exit(struct bch_fs *c)
 	}
 
 	mutex_unlock(&bc->lock);
+	memalloc_nofs_restore(flags);
 
 	if (bc->table_init_done)
 		rhashtable_destroy(&bc->table);
