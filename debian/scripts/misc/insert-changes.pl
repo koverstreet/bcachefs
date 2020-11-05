49e15cb93965301086e930ba8d2b5e8a6e9be502
Thadeu Lima de Souza Cascardo <cascardo@canonical.com>
2020-11-05 17:16:00 +0100

    UBUNTU: [Packaging] insertchanges: avoid double newline
    BugLink: https://bugs.launchpad.net/bugs/1903293
    
    When some changes have been already added to the changelog, like when using
    insert-ubuntu-changes, and there are no other changes, we end up with two
    newlines right after the stanza header.
    
    Add a $skip_newline variable that allows us to skip that extra newline when
    there are no other changes.
    
    Signed-off-by: Thadeu Lima de Souza Cascardo <cascardo@canonical.com>
    Acked-by: Andrea Righi <andrea.righi@canonical.com>
    Acked-by: Kelsey Skunberg <kelsey.skunberg@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- debian/scripts/misc/insert-changes.pl
+++ debian/scripts/misc/insert-changes.pl
@@ -13,17 +13,24 @@ open(CHANGES, "< $debian/changes") or die "Cannot open new changes";
 open(NEW, "> $debian/changelog.new") or die "Cannot open new changelog";
 
 $printed = 0;
+my $skip_newline = 0;
 
 while (<CHANGELOG>) {
 	if (/^  CHANGELOG: /) {
 		next if $printed;
 
+		$skip_newline = 1;
 		while (<CHANGES>) {
+			$skip_newline = 0;
 			print NEW;
 		}
 
 		$printed = 1;
 	} else {
+		if (/^$/ && $skip_newline == 1) {
+			$skip_newline = 0;
+			next;
+		}
 		print NEW;
 	}
 }
