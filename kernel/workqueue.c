e8d04560dc2a6cf7e0bc2920b371c0b6c7b666bb
Peter Zijlstra <peterz@infradead.org>
2021-01-15 19:08:36 +0100

    workqueue: Restrict affinity change to rescuer
    BugLink: https://bugs.launchpad.net/bugs/1918330
    
    [ Upstream commit 640f17c82460e9724fd256f0a1f5d99e7ff0bda4 ]
    
    create_worker() will already set the right affinity using
    kthread_bind_mask(), this means only the rescuer will need to change
    it's affinity.
    
    Howveer, while in cpu-hot-unplug a regular task is not allowed to run
    on online&&!active as it would be pushed away quite agressively. We
    need KTHREAD_IS_PER_CPU to survive in that environment.
    
    Therefore set the affinity after getting that magic flag.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Valentin Schneider <valentin.schneider@arm.com>
    Tested-by: Valentin Schneider <valentin.schneider@arm.com>
    Link: https://lkml.kernel.org/r/20210121103506.826629830@infradead.org
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -1731,18 +1731,15 @@ static void worker_attach_to_pool(struct worker *worker,
 	mutex_lock(&pool->attach_mutex);
 
 	/*
-	 * set_cpus_allowed_ptr() will fail if the cpumask doesn't have any
-	 * online CPUs.  It'll be re-applied when any of the CPUs come up.
-	 */
-	set_cpus_allowed_ptr(worker->task, pool->attrs->cpumask);
-
-	/*
 	 * The pool->attach_mutex ensures %POOL_DISASSOCIATED remains
 	 * stable across this function.  See the comments above the
 	 * flag definition for details.
 	 */
 	if (pool->flags & POOL_DISASSOCIATED)
 		worker->flags |= WORKER_UNBOUND;
+
+	if (worker->rescue_wq)
+		set_cpus_allowed_ptr(worker->task, pool->attrs->cpumask);
 
 	list_add_tail(&worker->node, &pool->workers);
 
