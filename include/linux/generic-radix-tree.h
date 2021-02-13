0a1fe54187569c2f36d23b4174b527295db3c9d0
Kent Overstreet <kent.overstreet@gmail.com>
2021-02-12 20:11:25 -0500

    lib/generic-radix-tree.c: Don't overflow in peek()
    When we started spreading new inode numbers throughout most of the 64
    bit inode space, that triggered some corner case bugs, in particular
    some integer overflows related to the radix tree code. Oops.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- include/linux/generic-radix-tree.h
+++ include/linux/generic-radix-tree.h
@@ -184,6 +184,12 @@ void *__genradix_iter_peek(struct genradix_iter *, struct __genradix *, size_t);
 static inline void __genradix_iter_advance(struct genradix_iter *iter,
 					   size_t obj_size)
 {
+	if (iter->offset + obj_size < iter->offset) {
+		iter->offset	= SIZE_MAX;
+		iter->pos	= SIZE_MAX;
+		return;
+	}
+
 	iter->offset += obj_size;
 
 	if (!is_power_of_2(obj_size) &&
