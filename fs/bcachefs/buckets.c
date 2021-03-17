d392b925832f5964fc3a36f3c9ab606c09fa0ad6
Kent Overstreet <kent.overstreet@gmail.com>
2021-03-17 02:21:24 -0400

    bcachefs: Update triggers code for snapshots
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- fs/bcachefs/buckets.c
+++ fs/bcachefs/buckets.c
@@ -1239,6 +1239,10 @@ int bch2_mark_update(struct btree_trans *trans, struct btree_path *path,
 
 	old = bch2_btree_path_peek_slot(path, &unpacked);
 
+	/* Not actually overwriting? */
+	if (new->k.p.snapshot != old.k->p.snapshot)
+		old = deleted;
+
 	if (old.k->type == new->k.type &&
 	    ((1U << old.k->type) & BTREE_TRIGGER_WANTS_OLD_AND_NEW)) {
 		ret   = bch2_mark_key_locked(c, old, bkey_i_to_s_c(new),
@@ -1809,6 +1813,10 @@ int bch2_trans_mark_update(struct btree_trans *trans,
 		return 0;
 
 	old = bch2_btree_path_peek_slot(path, &unpacked);
+
+	/* Not actually overwriting? */
+	if (new->k.p.snapshot != old.k->p.snapshot)
+		old = deleted;
 
 	if (old.k->type == new->k.type &&
 	    ((1U << old.k->type) & BTREE_TRIGGER_WANTS_OLD_AND_NEW)) {
