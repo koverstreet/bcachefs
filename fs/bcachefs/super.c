be9ce30685b16e8117c237b95814f3856c5af08b
Kent Overstreet <kent.overstreet@gmail.com>
2021-04-17 20:24:54 -0400

    bcachefs: Rip out copygc pd controller
    We have a separate mechanism for ratelimiting copygc now - the pd
    controller has only been causing problems.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- fs/bcachefs/super.c
+++ fs/bcachefs/super.c
@@ -294,7 +294,6 @@ void bch2_fs_read_only(struct bch_fs *c)
 	percpu_ref_kill(&c->writes);
 
 	cancel_work_sync(&c->ec_stripe_delete_work);
-	cancel_delayed_work(&c->pd_controllers_update);
 
 	/*
 	 * If we're not doing an emergency shutdown, we want to wait on
@@ -378,8 +377,6 @@ static int bch2_fs_read_write_late(struct bch_fs *c)
 		bch_err(c, "error starting rebalance thread");
 		return ret;
 	}
-
-	schedule_delayed_work(&c->pd_controllers_update, 5 * HZ);
 
 	schedule_work(&c->ec_stripe_delete_work);
 
@@ -572,7 +569,6 @@ void __bch2_fs_stop(struct bch_fs *c)
 		cancel_work_sync(&ca->io_error_work);
 
 	cancel_work_sync(&c->btree_write_error_work);
-	cancel_delayed_work_sync(&c->pd_controllers_update);
 	cancel_work_sync(&c->read_only_work);
 
 	for (i = 0; i < c->sb.nr_devices; i++)
