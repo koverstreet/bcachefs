67965b90d964a25bb621e1769b2bff53c64cc227
Kent Overstreet <kent.overstreet@gmail.com>
2021-05-27 23:52:41 -0400

    fixup! bcachefs: Fix locking in __bch2_set_nr_journal_buckets()

--- fs/bcachefs/journal.c
+++ fs/bcachefs/journal.c
@@ -807,10 +807,8 @@ static int __bch2_set_nr_journal_buckets(struct bch_dev *ca, unsigned nr,
 		long b;
 
 		if (new_fs) {
-			percpu_down_read(&c->mark_lock);
 			b = bch2_bucket_alloc_new_fs(ca);
 			if (b < 0) {
-				percpu_up_read(&c->mark_lock);
 				ret = -ENOSPC;
 				goto err;
 			}
@@ -827,7 +825,8 @@ static int __bch2_set_nr_journal_buckets(struct bch_dev *ca, unsigned nr,
 			b = sector_to_bucket(ca, ob->ptr.offset);
 		}
 
-		spin_lock(&c->journal.lock);
+		if (c)
+			spin_lock(&c->journal.lock);
 
 		/*
 		 * XXX
@@ -854,14 +853,14 @@ static int __bch2_set_nr_journal_buckets(struct bch_dev *ca, unsigned nr,
 		if (pos <= ja->cur_idx)
 			ja->cur_idx = (ja->cur_idx + 1) % ja->nr;
 
-		spin_unlock(&c->journal.lock);
+		if (c)
+			spin_unlock(&c->journal.lock);
 
 		if (new_fs) {
 			bch2_mark_metadata_bucket(c, ca, b, BCH_DATA_journal,
 						  ca->mi.bucket_size,
 						  gc_phase(GC_PHASE_SB),
 						  0);
-			percpu_up_read(&c->mark_lock);
 		} else {
 			ret = bch2_trans_do(c, NULL, NULL, BTREE_INSERT_NOFAIL,
 				bch2_trans_mark_metadata_bucket(&trans, ca,
