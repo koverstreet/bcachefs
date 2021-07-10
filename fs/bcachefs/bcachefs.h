15178a6479955d5aeaa124c0de9dabac87ffcca0
Kent Overstreet <kent.overstreet@gmail.com>
2021-07-10 13:44:42 -0400

    bcachefs: Update btree ptrs after every write
    This closes a significant hole (and last known hole) in our ability to
    verify metadata. Previously, since btree nodes are log structured, we
    couldn't detect lost btree writes that weren't the first write to a
    given node. Additionally, this seems to have lead to some significant
    metadata corruption on multi device filesystems with metadata
    replication: since a write may have made it to one device and not
    another, if we read that btree node back from the replica that did have
    that write and started appending after that point, the other replica
    would have a gap in the bset entries and reading from that replica
    wouldn't find the rest of the bsets.
    
    But, since updates to interior btree nodes are now journalled, we can
    close this hole by updating pointers to btree nodes after every write
    with the currently written number of sectors, without negatively
    affecting performance. This means we will always detect lost or corrupt
    metadata - it also means that our btree is now a curious hybrid of COW
    and non COW btrees, with all the benefits of both (excluding
    complexity).
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- fs/bcachefs/bcachefs.h
+++ fs/bcachefs/bcachefs.h
@@ -675,7 +675,7 @@ struct bch_fs {
 	struct btree_key_cache	btree_key_cache;
 
 	struct workqueue_struct	*btree_update_wq;
-	struct workqueue_struct	*btree_error_wq;
+	struct workqueue_struct	*btree_io_complete_wq;
 	/* copygc needs its own workqueue for index updates.. */
 	struct workqueue_struct	*copygc_wq;
 
@@ -826,8 +826,6 @@ struct bch_fs {
 
 	atomic64_t		btree_writes_nr;
 	atomic64_t		btree_writes_sectors;
-	struct bio_list		btree_write_error_list;
-	struct work_struct	btree_write_error_work;
 	spinlock_t		btree_write_error_lock;
 
 	/* ERRORS */
