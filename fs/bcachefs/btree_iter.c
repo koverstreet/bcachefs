6d8f8c70e35d93658b5f8ae9ec885fbf344fc5b9
Kent Overstreet <kent.overstreet@gmail.com>
2021-08-29 19:34:37 -0400

    bcachefs: Kill BTREE_ITER_SET_POS_AFTER_COMMIT
    BTREE_ITER_SET_POS_AFTER_COMMIT is used internally to automagically
    advance extent btree iterators on sucessful commit.
    
    But with the upcomnig btree_path patch it's getting more awkward to
    support, and it adds overhead to core data structures that's only used
    in a few places, and can be easily done by the caller instead.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- fs/bcachefs/btree_iter.c
+++ fs/bcachefs/btree_iter.c
@@ -2349,7 +2349,6 @@ static void btree_iter_copy(struct btree_trans *trans, struct btree_iter *dst,
 					   __btree_lock_want(dst, i));
 
 	dst->flags &= ~BTREE_ITER_KEEP_UNTIL_COMMIT;
-	dst->flags &= ~BTREE_ITER_SET_POS_AFTER_COMMIT;
 	trans->iters_sorted = false;
 }
 
@@ -2566,8 +2565,7 @@ void bch2_trans_begin(struct btree_trans *trans)
 	struct btree_iter *iter;
 
 	trans_for_each_iter(trans, iter)
-		iter->flags &= ~(BTREE_ITER_KEEP_UNTIL_COMMIT|
-				 BTREE_ITER_SET_POS_AFTER_COMMIT);
+		iter->flags &= ~BTREE_ITER_KEEP_UNTIL_COMMIT;
 
 	/*
 	 * XXX: we shouldn't be doing this if the transaction was restarted, but
