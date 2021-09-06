0b6ce517bb1068c03fa5da692463e52f196cb856
Kent Overstreet <kent.overstreet@gmail.com>
2021-09-06 17:00:35 -0400

    bcachefs: Only mark iterators nopreserve that aren't in use
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- fs/bcachefs/btree_iter.c
+++ fs/bcachefs/btree_iter.c
@@ -2473,6 +2473,8 @@ void bch2_trans_begin(struct btree_trans *trans)
 	}
 
 	trans_for_each_path(trans, path) {
+		path->should_be_locked = false;
+
 		/*
 		 * XXX: we probably shouldn't be doing this if the transaction
 		 * was restarted, but currently we still overflow transaction
@@ -2480,8 +2482,8 @@ void bch2_trans_begin(struct btree_trans *trans)
 		 */
 		if (!path->ref && !path->preserve)
 			__bch2_path_free(trans, path);
-		else
-			path->preserve = path->should_be_locked = false;
+		else if (!path->ref)
+			path->preserve = false;
 	}
 
 	bch2_trans_cond_resched(trans);
