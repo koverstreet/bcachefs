57c886016f828830d02e9d6eac352ad8d5229565
Jason Gunthorpe <jgg@nvidia.com>
2021-10-12 00:08:00 +0200

    RDMA/ucma: Fix the locking of ctx->file
    ctx->file is changed under the file->mut lock by ucma_migrate_id(), which
    is impossible to lock correctly. Instead change ctx->file under the
    handler_lock and ctx_table lock and revise all places touching ctx->file
    to use this locking when reading ctx->file.
    
    Link: https://lore.kernel.org/r/20200818120526.702120-9-leon@kernel.org
    Signed-off-by: Leon Romanovsky <leonro@mellanox.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    (backported from commit 09e328e47a695b0d346598f5d6593ee598e64885)
    [cascardo: commit 95fe51096b7adf1d1e7315c49c75e2f75f162584 moved the
     file locks from ucma_cleanup_mc_events callers to inside it, keep it
     out of the function]
    CVE-2020-36385
    Signed-off-by: Thadeu Lima de Souza Cascardo <cascardo@canonical.com>
    Acked-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>
    Acked-by: Stefan Bader <stefan.bader@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- drivers/infiniband/core/ucma.c
+++ drivers/infiniband/core/ucma.c
@@ -530,6 +530,7 @@ static void ucma_cleanup_mc_events(struct ucma_multicast *mc)
 {
 	struct ucma_event *uevent, *tmp;
 
+	rdma_lock_handler(mc->ctx->cm_id);
 	list_for_each_entry_safe(uevent, tmp, &mc->ctx->file->event_list, list) {
 		if (uevent->mc != mc)
 			continue;
@@ -537,6 +538,7 @@ static void ucma_cleanup_mc_events(struct ucma_multicast *mc)
 		list_del(&uevent->list);
 		kfree(uevent);
 	}
+	rdma_unlock_handler(mc->ctx->cm_id);
 }
 
 /*
@@ -1536,7 +1538,7 @@ static ssize_t ucma_leave_multicast(struct ucma_file *file,
 	mc = idr_find(&multicast_idr, cmd.id);
 	if (!mc)
 		mc = ERR_PTR(-ENOENT);
-	else if (mc->ctx->file != file)
+	else if (READ_ONCE(mc->ctx->file) != file)
 		mc = ERR_PTR(-EINVAL);
 	else if (!atomic_inc_not_zero(&mc->ctx->ref))
 		mc = ERR_PTR(-ENXIO);
@@ -1631,6 +1633,7 @@ static ssize_t ucma_migrate_id(struct ucma_file *new_file,
 		goto file_put;
 	}
 
+	rdma_lock_handler(ctx->cm_id);
 	cur_file = ctx->file;
 	if (cur_file == new_file) {
 		mutex_lock(&cur_file->mut);
@@ -1659,6 +1662,7 @@ response:
 			 &resp, sizeof(resp)))
 		ret = -EFAULT;
 
+	rdma_unlock_handler(ctx->cm_id);
 	ucma_put_ctx(ctx);
 file_put:
 	fdput(f);
