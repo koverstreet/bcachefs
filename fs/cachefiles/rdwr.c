c9e7271519d63226293cdefb954669a30638e683
Andrea Righi <andrea.righi@canonical.com>
2021-10-19 17:27:00 +0200

    Revert "UBUNTU: SAUCE: cachefiles: Page leaking in cachefiles_read_backing_file while vmscan is active"
    BugLink: https://bugs.launchpad.net/bugs/1947709
    
    This reverts commit 3f467f6131cf27c4ead53797f4562e2cf7272711.
    
    Signed-off-by: Andrea Righi <andrea.righi@canonical.com>
    Acked-by: Kamal Mostafa <kamal@canonical.com>
    Acked-by: Tim Gardner <tim.gardner@canonical.com>
    Signed-off-by: Kleber Sacilotto de Souza <kleber.souza@canonical.com>

--- fs/cachefiles/rdwr.c
+++ fs/cachefiles/rdwr.c
@@ -512,8 +512,6 @@ static int cachefiles_read_backing_file(struct cachefiles_object *object,
 				goto installed_new_backing_page;
 			if (ret != -EEXIST)
 				goto nomem;
-			put_page(newpage);
-			newpage = NULL;
 		}
 
 		/* we've installed a new backing page, so now we need
@@ -538,10 +536,7 @@ static int cachefiles_read_backing_file(struct cachefiles_object *object,
 					    netpage->index, cachefiles_gfp);
 		if (ret < 0) {
 			if (ret == -EEXIST) {
-				put_page(backpage);
-				backpage = NULL;
 				put_page(netpage);
-				netpage = NULL;
 				fscache_retrieval_complete(op, 1);
 				continue;
 			}
@@ -614,10 +609,7 @@ static int cachefiles_read_backing_file(struct cachefiles_object *object,
 					    netpage->index, cachefiles_gfp);
 		if (ret < 0) {
 			if (ret == -EEXIST) {
-				put_page(backpage);
-				backpage = NULL;
 				put_page(netpage);
-				netpage = NULL;
 				fscache_retrieval_complete(op, 1);
 				continue;
 			}
