465a0af9c42e45ac49ac506a52e4ab81a20f7a2a
Kent Overstreet <kent.overstreet@gmail.com>
2021-10-21 12:05:21 -0400

    bcachefs: Fix restart handling in for_each_btree_key()
    Code that uses for_each_btree_key often wants transaction restarts to be
    handled locally and not returned. Originally, we wouldn't return
    transaction restarts if there was a single iterator in the transaction -
    the reasoning being if there weren't other iterators being invalidated,
    and the current iterator was being advanced/retraversed, there weren't
    any locks or iterators we were required to preserve.
    
    But with the btree_path conversion that approach doesn't work anymore -
    even when we're using for_each_btree_key() with a single iterator there
    will still be two paths in the transaction, since we now always preserve
    the path at the pos the iterator was initialized at - the reason being
    that on restart we often restart from the same place.
    
    And it turns out there's now a lot of for_each_btree_key() uses that _do
    not_ want transaction restarts handled locally, and should be returning
    them.
    
    This patch splits out for_each_btree_key_norestart() and
    for_each_btree_key_continue_norestart(), and converts existing users as
    appropriate. for_each_btree_key(), for_each_btree_key_continue(), and
    for_each_btree_node() now handle transaction restarts themselves by
    calling bch2_trans_begin() when necessary - and the old hack to not
    return transaction restarts when there's a single path in the
    transaction has been deleted.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- fs/bcachefs/fs-io.c
+++ fs/bcachefs/fs-io.c
@@ -1814,7 +1814,7 @@ retry:
 	if (err)
 		goto err;
 
-	for_each_btree_key(&trans, iter, BTREE_ID_extents,
+	for_each_btree_key_norestart(&trans, iter, BTREE_ID_extents,
 			   SPOS(inum.inum, offset, snapshot),
 			   BTREE_ITER_SLOTS, k, err) {
 		if (bkey_cmp(bkey_start_pos(k.k), POS(inum.inum, end)) >= 0)
@@ -2190,7 +2190,7 @@ retry:
 	if (ret)
 		goto err;
 
-	for_each_btree_key(&trans, iter, BTREE_ID_extents, start, 0, k, ret) {
+	for_each_btree_key_norestart(&trans, iter, BTREE_ID_extents, start, 0, k, ret) {
 		if (bkey_cmp(bkey_start_pos(k.k), end) >= 0)
 			break;
 
@@ -3093,7 +3093,7 @@ retry:
 	if (ret)
 		goto err;
 
-	for_each_btree_key(&trans, iter, BTREE_ID_extents,
+	for_each_btree_key_norestart(&trans, iter, BTREE_ID_extents,
 			   SPOS(inode->v.i_ino, offset >> 9, snapshot), 0, k, ret) {
 		if (k.k->p.inode != inode->v.i_ino) {
 			break;
@@ -3200,7 +3200,7 @@ retry:
 	if (ret)
 		goto err;
 
-	for_each_btree_key(&trans, iter, BTREE_ID_extents,
+	for_each_btree_key_norestart(&trans, iter, BTREE_ID_extents,
 			   SPOS(inode->v.i_ino, offset >> 9, snapshot),
 			   BTREE_ITER_SLOTS, k, ret) {
 		if (k.k->p.inode != inode->v.i_ino) {
