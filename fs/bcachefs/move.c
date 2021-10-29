91117b23a8abb518897c01b95c464ffc0abcede4
Kent Overstreet <kent.overstreet@gmail.com>
2021-10-29 16:29:13 -0400

    bcachefs: Assorted ec fixes
    - The backpointer that ec_stripe_update_ptrs() uses now needs to include
      the snapshot ID, which means we have to change where we add the
      backpointer to after getting the snapshot ID for the new extents
    
    - ec_stripe_update_ptrs() needs to be calling bch2_trans_begin()
    
    - improve error message in bch2_mark_stripe()
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- fs/bcachefs/move.c
+++ fs/bcachefs/move.c
@@ -8,6 +8,7 @@
 #include "btree_update_interior.h"
 #include "buckets.h"
 #include "disk_groups.h"
+#include "ec.h"
 #include "inode.h"
 #include "io.h"
 #include "journal_reclaim.h"
@@ -136,6 +137,7 @@ static int bch2_migrate_index_update(struct bch_write_op *op)
 	struct btree_iter iter;
 	struct migrate_write *m =
 		container_of(op, struct migrate_write, op);
+	struct open_bucket *ec_ob = ec_open_bucket(c, &op->open_buckets);
 	struct keylist *keys = &op->insert_keys;
 	struct bkey_buf _new, _insert;
 	int ret = 0;
@@ -253,6 +255,8 @@ static int bch2_migrate_index_update(struct bch_write_op *op)
 		if (!ret) {
 			bch2_btree_iter_set_pos(&iter, next_pos);
 			atomic_long_inc(&c->extent_migrate_done);
+			if (ec_ob)
+				bch2_ob_add_backpointer(c, ec_ob, &insert->k);
 		}
 err:
 		if (ret == -EINTR)
