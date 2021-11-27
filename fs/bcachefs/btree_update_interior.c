553a20e8b6810d557847f31852461d5613e1e554
Kent Overstreet <kent.overstreet@gmail.com>
2021-11-27 16:13:41 -0500

    bcachefs: Improve tracing of btree_path leaks
    This patch plumbs the btree_path->ip_allocated field back to where the
    btree_iter that owns it was first initialized - meaning it will be much
    easier to figure out which btree_iter wasn't exited properly.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- fs/bcachefs/btree_update_interior.c
+++ fs/bcachefs/btree_update_interior.c
@@ -1590,8 +1590,8 @@ int __bch2_foreground_maybe_merge(struct btree_trans *trans,
 		? bpos_predecessor(b->data->min_key)
 		: bpos_successor(b->data->max_key);
 
-	sib_path = bch2_path_get(trans, false, path->btree_id,
-				 sib_pos, U8_MAX, level, true);
+	sib_path = bch2_path_get(trans, false, path->btree_id, sib_pos,
+				 U8_MAX, level, true, _THIS_IP_);
 	ret = bch2_btree_path_traverse(trans, sib_path, false);
 	if (ret)
 		goto err;
@@ -1888,7 +1888,8 @@ static int __bch2_btree_node_update_key(struct btree_trans *trans,
 		bch2_trans_copy_iter(&iter2, iter);
 
 		iter2.path = bch2_btree_path_make_mut(trans, iter2.path,
-				iter2.flags & BTREE_ITER_INTENT);
+				iter2.flags & BTREE_ITER_INTENT,
+				_THIS_IP_);
 
 		BUG_ON(iter2.path->level != b->c.level);
 		BUG_ON(bpos_cmp(iter2.path->pos, new_key->k.p));
