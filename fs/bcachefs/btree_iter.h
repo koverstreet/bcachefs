e9f4d538f0f551eef3ccdf1f77083847ca4062be
Kent Overstreet <kent.overstreet@gmail.com>
2021-11-27 16:13:41 -0500

    bcachefs: Improve tracing of btree_path leaks
    This patch plumbs the btree_path->ip_allocated field back to where the
    btree_iter that owns it was first initialized - meaning it will be much
    easier to figure out which btree_iter wasn't exited properly.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- fs/bcachefs/btree_iter.h
+++ fs/bcachefs/btree_iter.h
@@ -130,11 +130,13 @@ __trans_next_path_with_node(struct btree_trans *trans, struct btree *b,
 						 (_path)->idx + 1))
 
 struct btree_path * __must_check
-bch2_btree_path_make_mut(struct btree_trans *, struct btree_path *, bool);
+bch2_btree_path_make_mut(struct btree_trans *, struct btree_path *,
+			 bool, unsigned long);
 int __must_check bch2_btree_path_traverse(struct btree_trans *,
 					  struct btree_path *, unsigned);
 struct btree_path *bch2_path_get(struct btree_trans *, bool, enum btree_id,
-				 struct bpos, unsigned, unsigned, bool);
+				 struct bpos, unsigned, unsigned, bool,
+				 unsigned long);
 inline struct bkey_s_c bch2_btree_path_peek_slot(struct btree_path *, struct bkey *);
 
 #ifdef CONFIG_BCACHEFS_DEBUG
