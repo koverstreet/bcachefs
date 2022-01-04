2a7de33cfdb44ed0ad89589597fa4c603e50bb73
Kent Overstreet <kent.overstreet@gmail.com>
2022-01-04 00:33:52 -0500

    bcachefs: Switch to __func__for recording where btree_trans was initialized
    Symbol decoding, via %ps, isn't supported in userspace - this will also
    be faster when we're using trans->fn in the fast path, as with the new
    BCH_JSET_ENTRY_log journal messages.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- fs/bcachefs/btree_types.h
+++ fs/bcachefs/btree_types.h
@@ -368,6 +368,7 @@ struct btree_trans_commit_hook {
 
 struct btree_trans {
 	struct bch_fs		*c;
+	const char		*fn;
 	struct list_head	list;
 	struct btree		*locking;
 	unsigned		locking_path_idx;
@@ -375,7 +376,6 @@ struct btree_trans {
 	u8			locking_btree_id;
 	u8			locking_level;
 	pid_t			pid;
-	unsigned long		ip;
 	int			srcu_idx;
 
 	u8			nr_sorted;
