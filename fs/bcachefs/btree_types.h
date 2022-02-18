72bb0432d30da1fbd045c060d7980b0d338a9482
Kent Overstreet <kent.overstreet@gmail.com>
2022-02-18 00:47:45 -0500

    bcachefs: Fix failure to allocate btree node in cache
    The error code when we fail to allocate a node in the btree node cache
    doesn't make it to bch2_btree_path_traverse_all(). Instead, we need to
    stash a flag in btree_trans so we know we have to take the cannibalize
    lock.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- fs/bcachefs/btree_types.h
+++ fs/bcachefs/btree_types.h
@@ -386,6 +386,7 @@ struct btree_trans {
 	bool			used_mempool:1;
 	bool			in_traverse_all:1;
 	bool			restarted:1;
+	bool			memory_allocation_failure:1;
 	bool			journal_transaction_names:1;
 	/*
 	 * For when bch2_trans_update notices we'll be splitting a compressed
