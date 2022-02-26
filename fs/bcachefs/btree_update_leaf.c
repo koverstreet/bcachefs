25a8bb24a0b065afe779bcdce986f1d8ff27229f
Kent Overstreet <kent.overstreet@gmail.com>
2022-02-25 22:33:01 -0500

    bcachefs: Fix btree path sorting
    In btree_update_interior.c, we were changing a path's level directly -
    which affects path sort order - without re-sorting paths, leading to
    assertions when bch2_path_get() verified paths were sorted correctly.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- fs/bcachefs/btree_update_leaf.c
+++ fs/bcachefs/btree_update_leaf.c
@@ -1539,13 +1539,11 @@ int __must_check bch2_trans_update(struct btree_trans *trans, struct btree_iter 
 			if (!iter->key_cache_path)
 				iter->key_cache_path =
 					bch2_path_get(trans, path->btree_id, path->pos, 1, 0,
-						      BTREE_ITER_INTENT|
-						      BTREE_ITER_CACHED, _THIS_IP_);
+						      BTREE_ITER_INTENT|BTREE_ITER_CACHED);
 
 			iter->key_cache_path =
 				bch2_btree_path_set_pos(trans, iter->key_cache_path, path->pos,
-							iter->flags & BTREE_ITER_INTENT,
-							_THIS_IP_);
+							iter->flags & BTREE_ITER_INTENT);
 
 			ret = bch2_btree_path_traverse(trans, iter->key_cache_path,
 						       BTREE_ITER_CACHED);
