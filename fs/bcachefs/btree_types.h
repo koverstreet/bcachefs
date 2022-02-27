04335f8b293fb87dcc3f294500223d4384f44b8c
Kent Overstreet <kent.overstreet@gmail.com>
2022-02-26 19:48:37 -0500

    bcachefs: Fix an assert in the initial GC path
    With lockdep enabled, we (rarely) hit an assertion in
    bch2_gc_init_recurse() -> bch2_gc_mark_key() -> bch2_trans_unlock():
    this is because initial GC doesn't use btree iterators for btree
    walking, it does its own thing for various (complicated) reasons - but
    we don't have to worry about deadlocks because we're only taking read
    locks.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- fs/bcachefs/btree_types.h
+++ fs/bcachefs/btree_types.h
@@ -396,6 +396,7 @@ struct btree_trans {
 	bool			restarted:1;
 	bool			memory_allocation_failure:1;
 	bool			journal_transaction_names:1;
+	bool			is_initial_gc:1;
 	/*
 	 * For when bch2_trans_update notices we'll be splitting a compressed
 	 * extent:
