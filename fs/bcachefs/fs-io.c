9f901d3da27bf672f0447a2775dbadd94ded2de7
Kent Overstreet <kent.overstreet@gmail.com>
2022-03-09 15:37:42 -0500

    bcachefs: Fix dio write path with loopback dio mode
    When the iov_iter is a bvec iter, it's possible the IO was submitted
    from a kthread that didn't have an mm to switch to.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- fs/bcachefs/fs-io.c
+++ fs/bcachefs/fs-io.c
@@ -2085,7 +2085,7 @@ static long bch2_dio_write_loop(struct dio_write *dio)
 	while (1) {
 		iter_count = dio->iter.count;
 
-		if (kthread)
+		if (kthread && dio->mm)
 			kthread_use_mm(dio->mm);
 		BUG_ON(current->faults_disabled_mapping);
 		current->faults_disabled_mapping = mapping;
@@ -2095,7 +2095,7 @@ static long bch2_dio_write_loop(struct dio_write *dio)
 		dropped_locks = fdm_dropped_locks();
 
 		current->faults_disabled_mapping = NULL;
-		if (kthread)
+		if (kthread && dio->mm)
 			kthread_unuse_mm(dio->mm);
 
 		/*
