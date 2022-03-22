6fa58c4918844bafd2b0420c9e6249107e5446ce
Kent Overstreet <kent.overstreet@gmail.com>
2022-03-22 01:08:08 -0400

    mm: Add a .to_text() method for shrinkers
    This adds a new callback method to shrinkers which they can use to
    describe anything relevant to memory reclaim about their internal state,
    for example object dirtyness.
    
    This uses the new printbufs to output to heap allocated strings, so that
    the .to_text() methods can be used both for messages logged to the
    console, and also sysfs/debugfs.
    
    This patch also adds shrinkers_to_text(), which reports on the top 10
    shrinkers - by object count - in sorted order, to be used in OOM
    reporting.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -55,6 +55,7 @@
 #include <linux/ctype.h>
 #include <linux/debugfs.h>
 #include <linux/khugepaged.h>
+#include <linux/seq_buf.h>
 
 #include <asm/tlbflush.h>
 #include <asm/div64.h>
@@ -646,7 +647,6 @@ static int __prealloc_shrinker(struct shrinker *shrinker)
 	return 0;
 }
 
-#ifdef CONFIG_SHRINKER_DEBUG
 int prealloc_shrinker(struct shrinker *shrinker, const char *fmt, ...)
 {
 	va_list ap;
@@ -666,19 +666,12 @@ int prealloc_shrinker(struct shrinker *shrinker, const char *fmt, ...)
 
 	return err;
 }
-#else
-int prealloc_shrinker(struct shrinker *shrinker, const char *fmt, ...)
-{
-	return __prealloc_shrinker(shrinker);
-}
-#endif
 
 void free_prealloced_shrinker(struct shrinker *shrinker)
 {
-#ifdef CONFIG_SHRINKER_DEBUG
 	kfree_const(shrinker->name);
 	shrinker->name = NULL;
-#endif
+
 	if (shrinker->flags & SHRINKER_MEMCG_AWARE) {
 		down_write(&shrinker_rwsem);
 		unregister_memcg_shrinker(shrinker);
@@ -709,7 +702,6 @@ static int __register_shrinker(struct shrinker *shrinker)
 	return 0;
 }
 
-#ifdef CONFIG_SHRINKER_DEBUG
 int register_shrinker(struct shrinker *shrinker, const char *fmt, ...)
 {
 	va_list ap;
@@ -728,12 +720,6 @@ int register_shrinker(struct shrinker *shrinker, const char *fmt, ...)
 	}
 	return err;
 }
-#else
-int register_shrinker(struct shrinker *shrinker, const char *fmt, ...)
-{
-	return __register_shrinker(shrinker);
-}
-#endif
 EXPORT_SYMBOL(register_shrinker);
 
 /*
@@ -775,6 +761,76 @@ void synchronize_shrinkers(void)
 	up_write(&shrinker_rwsem);
 }
 EXPORT_SYMBOL(synchronize_shrinkers);
+
+void shrinker_to_text(struct seq_buf *out, struct shrinker *shrinker)
+{
+	struct shrink_control sc = { .gfp_mask = GFP_KERNEL, };
+
+	seq_buf_puts(out, shrinker->name);
+	seq_buf_printf(out, " objects: %lu\n", shrinker->count_objects(shrinker, &sc));
+
+	if (shrinker->to_text) {
+		shrinker->to_text(out, shrinker);
+		seq_buf_puts(out, "\n");
+	}
+}
+
+/**
+ * shrinkers_to_text - Report on shrinkers with highest usage
+ *
+ * This reports on the top 10 shrinkers, by object counts, in sorted order:
+ * intended to be used for OOM reporting.
+ */
+void shrinkers_to_text(struct seq_buf *out)
+{
+	struct shrinker *shrinker;
+	struct shrinker_by_mem {
+		struct shrinker	*shrinker;
+		unsigned long	mem;
+	} shrinkers_by_mem[10];
+	int i, nr = 0;
+
+	if (!down_read_trylock(&shrinker_rwsem)) {
+		seq_buf_puts(out, "(couldn't take shrinker lock)");
+		return;
+	}
+
+	list_for_each_entry(shrinker, &shrinker_list, list) {
+		struct shrink_control sc = { .gfp_mask = GFP_KERNEL, };
+		unsigned long mem = shrinker->count_objects(shrinker, &sc);
+
+		if (!mem || mem == SHRINK_STOP || mem == SHRINK_EMPTY)
+			continue;
+
+		for (i = 0; i < nr; i++)
+			if (mem < shrinkers_by_mem[i].mem)
+				break;
+
+		if (nr < ARRAY_SIZE(shrinkers_by_mem)) {
+			memmove(&shrinkers_by_mem[i + 1],
+				&shrinkers_by_mem[i],
+				sizeof(shrinkers_by_mem[0]) * (nr - i));
+			nr++;
+		} else if (i) {
+			i--;
+			memmove(&shrinkers_by_mem[0],
+				&shrinkers_by_mem[1],
+				sizeof(shrinkers_by_mem[0]) * i);
+		} else {
+			continue;
+		}
+
+		shrinkers_by_mem[i] = (struct shrinker_by_mem) {
+			.shrinker = shrinker,
+			.mem = mem,
+		};
+	}
+
+	for (i = nr - 1; i >= 0; --i)
+		shrinker_to_text(out, shrinkers_by_mem[i].shrinker);
+
+	up_read(&shrinker_rwsem);
+}
 
 #define SHRINK_BATCH 128
 
