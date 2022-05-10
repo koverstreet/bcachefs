7be108a7d3d61476b7ff4c2fda2c969500f2aec9
Kent Overstreet <kent.overstreet@gmail.com>
2022-05-09 20:44:07 -0400

    lib/printbuf: Heap allocation
    This makes printbufs optionally heap allocated: a printbuf initialized
    with the PRINTBUF initializer will automatically heap allocate and
    resize as needed.
    
    Allocations are done with GFP_KERNEL: code should use e.g.
    memalloc_nofs_save()/restore() as needed. Since we do not currently have
    memalloc_nowait_save()/restore(), in contexts where it is not safe to
    block we provide the helpers
    
      printbuf_atomic_inc()
      printbuf_atomic_dec()
    
    When the atomic count is nonzero, memory allocations will be done with
    GFP_NOWAIT.
    
    On memory allocation failure, output will be truncated. Code that wishes
    to check for memory allocation failure (in contexts where we should
    return -ENOMEM) should check if printbuf->allocation_failure is set.
    Since printbufs are expected to be typically used for log messages and
    on a best effort basis, we don't return errors directly.
    
    Other helpers provided by this patch:
    
     - printbuf_make_room(buf, extra)
       Reallocates if necessary to make room for @extra bytes (not including
       terminating null).
    
     - printbuf_str(buf)
       Returns a null terminated string equivalent to the contents of @buf.
       If @buf was never allocated (or allocation failed), returns a
       constant empty string.
    
     - printbuf_exit(buf)
       Releases memory allocated by a printbuf.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- include/linux/printbuf.h
+++ include/linux/printbuf.h
@@ -4,17 +4,67 @@
 #ifndef _LINUX_PRINTBUF_H
 #define _LINUX_PRINTBUF_H
 
-#include <linux/string.h>
-
 /*
- * Printbufs: String buffer for outputting (printing) to, for vsnprintf
+ * Printbufs: Simple strings for printing to, with optional heap allocation
+ *
+ * This code has provisions for use in userspace, to aid in making other code
+ * portable between kernelspace and userspace.
+ *
+ * Basic example:
+ *   struct printbuf buf = PRINTBUF;
+ *
+ *   pr_buf(&buf, "foo=");
+ *   foo_to_text(&buf, foo);
+ *   printk("%s", buf.buf);
+ *   printbuf_exit(&buf);
+ *
+ * Or
+ *   struct printbuf buf = PRINTBUF_EXTERN(char_buf, char_buf_size)
+ *
+ * We can now write pretty printers instead of writing code that dumps
+ * everything to the kernel log buffer, and then those pretty-printers can be
+ * used by other code that outputs to kernel log, sysfs, debugfs, etc.
+ *
+ * Memory allocation: Outputing to a printbuf may allocate memory. This
+ * allocation is done with GFP_KERNEL, by default: use the newer
+ * memalloc_*_(save|restore) functions as needed.
+ *
+ * Since no equivalent yet exists for GFP_ATOMIC/GFP_NOWAIT, memory allocations
+ * will be done with GFP_NOWAIT if printbuf->atomic is nonzero.
+ *
+ * Memory allocation failures: We don't return errors directly, because on
+ * memory allocation failure we usually don't want to bail out and unwind - we
+ * want to print what we've got, on a best-effort basis. But code that does want
+ * to return -ENOMEM may check printbuf.allocation_failure.
  */
+
+#include <linux/string.h>
 
 struct printbuf {
 	char			*buf;
 	unsigned		size;
 	unsigned		pos;
+	/*
+	 * If nonzero, allocations will be done with GFP_ATOMIC:
+	 */
+	u8			atomic;
+	bool			allocation_failure:1;
+	bool			heap_allocated:1;
 };
+
+int printbuf_make_room(struct printbuf *, unsigned);
+const char *printbuf_str(const struct printbuf *);
+void printbuf_exit(struct printbuf *);
+
+/* Initializer for a heap allocated printbuf: */
+#define PRINTBUF ((struct printbuf) { .heap_allocated = true })
+
+/* Initializer a printbuf that points to an external buffer: */
+#define PRINTBUF_EXTERN(_buf, _size)			\
+((struct printbuf) {					\
+	.buf	= _buf,					\
+	.size	= _size,				\
+})
 
 static inline unsigned printbuf_remaining(struct printbuf *out)
 {
@@ -28,26 +78,42 @@ static inline unsigned printbuf_written(struct printbuf *out)
 
 static inline void printbuf_nul_terminate(struct printbuf *out)
 {
+	printbuf_make_room(out, 1);
+
 	if (out->pos < out->size)
 		out->buf[out->pos] = 0;
 	else if (out->size)
 		out->buf[out->size - 1] = 0;
 }
 
-static inline void pr_chars(struct printbuf *out, char c, unsigned n)
+static inline void __pr_chars_reserved(struct printbuf *out, char c, unsigned n)
 {
 	memset(out->buf + out->pos,
 	       c,
 	       min(n, printbuf_remaining(out)));
 	out->pos += n;
+}
+
+static inline void pr_chars(struct printbuf *out, char c, unsigned n)
+{
+	printbuf_make_room(out, n);
+	__pr_chars_reserved(out, c, n);
 	printbuf_nul_terminate(out);
 }
 
-static inline void __pr_char(struct printbuf *out, char c)
+/* Doesn't call printbuf_make_room(), doesn't nul terminate: */
+static inline void __pr_char_reserved(struct printbuf *out, char c)
 {
 	if (printbuf_remaining(out))
 		out->buf[out->pos] = c;
 	out->pos++;
+}
+
+/* Doesn't nul terminate: */
+static inline void __pr_char(struct printbuf *out, char c)
+{
+	printbuf_make_room(out, 1);
+	__pr_char_reserved(out, c);
 }
 
 static inline void pr_char(struct printbuf *out, char c)
@@ -58,6 +124,8 @@ static inline void pr_char(struct printbuf *out, char c)
 
 static inline void pr_bytes(struct printbuf *out, const void *b, unsigned n)
 {
+	printbuf_make_room(out, n);
+
 	memcpy(out->buf + out->pos,
 	       b,
 	       min(n, printbuf_remaining(out)));
@@ -72,22 +140,43 @@ static inline void pr_str(struct printbuf *out, const char *str)
 
 static inline void pr_hex_byte(struct printbuf *out, u8 byte)
 {
-	__pr_char(out, hex_asc_hi(byte));
-	__pr_char(out, hex_asc_lo(byte));
+	printbuf_make_room(out, 2);
+	__pr_char_reserved(out, hex_asc_hi(byte));
+	__pr_char_reserved(out, hex_asc_lo(byte));
 	printbuf_nul_terminate(out);
 }
 
 static inline void pr_hex_byte_upper(struct printbuf *out, u8 byte)
 {
-	__pr_char(out, hex_asc_upper_hi(byte));
-	__pr_char(out, hex_asc_upper_lo(byte));
+	printbuf_make_room(out, 2);
+	__pr_char_reserved(out, hex_asc_upper_hi(byte));
+	__pr_char_reserved(out, hex_asc_upper_lo(byte));
 	printbuf_nul_terminate(out);
 }
 
-#define PRINTBUF_EXTERN(_buf, _size)			\
-((struct printbuf) {					\
-	.buf	= _buf,					\
-	.size	= _size,				\
-})
+/**
+ * printbuf_reset - re-use a printbuf without freeing and re-initializing it:
+ */
+static inline void printbuf_reset(struct printbuf *buf)
+{
+	buf->pos		= 0;
+	buf->allocation_failure	= 0;
+}
+
+/**
+ * printbuf_atomic_inc - mark as entering an atomic section
+ */
+static inline void printbuf_atomic_inc(struct printbuf *buf)
+{
+	buf->atomic++;
+}
+
+/**
+ * printbuf_atomic_inc - mark as leaving an atomic section
+ */
+static inline void printbuf_atomic_dec(struct printbuf *buf)
+{
+	buf->atomic--;
+}
 
 #endif /* _LINUX_PRINTBUF_H */
