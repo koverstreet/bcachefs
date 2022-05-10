ad98cd9b707577effd88433264af7e397963682e
Kent Overstreet <kent.overstreet@gmail.com>
2022-05-09 22:25:42 -0400

    lib/pretty-printers: pr_string_option(), pr_bitflags()
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

---
+++ lib/pretty-printers.c
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: LGPL-2.1+
+/* Copyright (C) 2022 Kent Overstreet */
+
+#include <linux/kernel.h>
+#include <linux/printbuf.h>
+
+/**
+ * pr_string_option - Given a list of strings, print out the list and indicate
+ * which option is selected, with square brackets (sysfs style)
+ *
+ * @out: The printbuf to output to
+ * @list: List of strings to choose from
+ * @selected: The option to highlight, with square brackets
+ */
+void pr_string_option(struct printbuf *out,
+		      const char * const list[],
+		      size_t selected)
+{
+	size_t i;
+
+	for (i = 0; list[i]; i++) {
+		if (i)
+			pr_char(out, ' ');
+		if (i == selected)
+			pr_char(out, '[');
+		pr_str(out, list[i]);
+		if (i == selected)
+			pr_char(out, ']');
+	}
+}
+EXPORT_SYMBOL(pr_string_option);
+
+/**
+ * pr_bitflags: Given a bitmap and a list of names for each bit, print out which
+ * bits are on, comma separated
+ *
+ * @out: The printbuf to output to
+ * @list: List of names for each bit
+ * @flags: Bits to print
+ */
+void pr_bitflags(struct printbuf *out,
+		 const char * const list[], u64 flags)
+{
+	unsigned bit, nr = 0;
+	bool first = true;
+
+	while (list[nr])
+		nr++;
+
+	while (flags && (bit = __ffs(flags)) < nr) {
+		if (!first)
+			pr_buf(out, ",");
+		first = false;
+		pr_str(out, list[bit]);
+		flags ^= 1 << bit;
+	}
+}
+EXPORT_SYMBOL(pr_bitflags);
