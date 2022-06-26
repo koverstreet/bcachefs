08c867c965b4f6054ce40a3902de212ebe08c84a
Kent Overstreet <kent.overstreet@gmail.com>
2022-06-26 18:31:51 -0400

    bcachefs: Use BTREE_INSERT_LAZY_RW in bch2_check_alloc_info()
    This runs before we go rw for journal replay, but after we're allowed to
    go rw. It might be time to consider killing BTREE_INSERT_LAZY_RW,
    though.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- fs/bcachefs/alloc_background.c
+++ fs/bcachefs/alloc_background.c
@@ -863,7 +863,9 @@ int bch2_check_alloc_info(struct bch_fs *c)
 
 	for_each_btree_key(&trans, iter, BTREE_ID_alloc, POS_MIN,
 			   BTREE_ITER_PREFETCH, k, ret) {
-		ret = __bch2_trans_do(&trans, NULL, NULL, 0,
+		ret = __bch2_trans_do(&trans, NULL, NULL,
+				      BTREE_INSERT_NOFAIL|
+				      BTREE_INSERT_LAZY_RW,
 			bch2_check_alloc_key(&trans, &iter));
 		if (ret)
 			break;
@@ -876,7 +878,9 @@ int bch2_check_alloc_info(struct bch_fs *c)
 	bch2_trans_iter_init(&trans, &iter, BTREE_ID_need_discard, POS_MIN,
 			     BTREE_ITER_PREFETCH);
 	while (1) {
-		ret = __bch2_trans_do(&trans, NULL, NULL, 0,
+		ret = __bch2_trans_do(&trans, NULL, NULL,
+				      BTREE_INSERT_NOFAIL|
+				      BTREE_INSERT_LAZY_RW,
 			bch2_check_discard_freespace_key(&trans, &iter));
 		if (ret)
 			break;
@@ -891,7 +895,9 @@ int bch2_check_alloc_info(struct bch_fs *c)
 	bch2_trans_iter_init(&trans, &iter, BTREE_ID_freespace, POS_MIN,
 			     BTREE_ITER_PREFETCH);
 	while (1) {
-		ret = __bch2_trans_do(&trans, NULL, NULL, 0,
+		ret = __bch2_trans_do(&trans, NULL, NULL,
+				      BTREE_INSERT_NOFAIL|
+				      BTREE_INSERT_LAZY_RW,
 			bch2_check_discard_freespace_key(&trans, &iter));
 		if (ret)
 			break;
