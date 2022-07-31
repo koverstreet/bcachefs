dd69442124e8c925f5c4e8ff6af8169049bd0cca
Suren Baghdasaryan <surenb@google.com>
2022-07-31 11:23:24 -0700

    lib: introduce page allocation tagging
    Introduce CONFIG_PAGE_ALLOC_TAGGING which provides helper functions to
    easily instrument page allocators and adds a page_ext field to store a
    pointer to the allocation tag associated with the code that allocated
    the page.
    
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Co-developed-by: Kent Overstreet <kent.overstreet@linux.dev>
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- lib/alloc_tag.c
+++ lib/alloc_tag.c
@@ -4,6 +4,7 @@
 #include <linux/fs.h>
 #include <linux/gfp.h>
 #include <linux/module.h>
+#include <linux/page_ext.h>
 #include <linux/seq_buf.h>
 #include <linux/uaccess.h>
 
@@ -145,6 +146,22 @@ static void alloc_tag_module_unload(struct codetag_type *cttype, struct codetag_
 		lazy_percpu_counter_exit(&tag->bytes_allocated);
 	}
 }
+
+static __init bool need_page_alloc_tagging(void)
+{
+	return true;
+}
+
+static __init void init_page_alloc_tagging(void)
+{
+}
+
+struct page_ext_operations page_alloc_tagging_ops = {
+	.size = sizeof(union codetag_ref),
+	.need = need_page_alloc_tagging,
+	.init = init_page_alloc_tagging,
+};
+EXPORT_SYMBOL(page_alloc_tagging_ops);
 
 static int __init alloc_tag_init(void)
 {
