82fedde300f5011f7cf1e8a2ac1b93facdfc67e8
Kent Overstreet <kent.overstreet@linux.dev>
2022-08-03 16:29:27 -0400

    Improved symbolic error names
    This patch adds per-error-site error codes, with error strings that
    include their file and line number.
    
    To use, change code that returns an error, e.g.
        return -ENOMEM;
    to
        return -ERR(ENOMEM);
    
    Then, errname() will return a string that includes the file and line
    number of the ERR() call, for example
        printk("Got error %s!\n", errname(err));
    will result in
        Got error ENOMEM at foo.c:1234
    
    To convert back to the original error code (before returning it to
    outside code that does not understand dynamic error codes), use
        return error_class(err);
    
    To test if an error is of some type, replace
        if (err == -ENOMEM)
    with
        if (error_matches(err, ENOMEM))
    
    Implementation notes:
    
    Error codes are allocated dynamically on module load and deallocated on
    module unload. On memory allocation failure (i.e. the data structures
    for indexing error strings and error parents), ERR() will fall back to
    returning the error code that it was passed.
    
    MAX_ERRNO has been raised from 4096 to 1 million, which should be
    sufficient given the number of lines of code and the fraction that throw
    errors in the kernel codebase.
    
    This has implications for ERR_PTR(), since the range of the address
    space reserved for errors is unavailable for other purposes. Since
    ERR_PTR() ptrs are at the top of the address space there should not be
    any major difficulties.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- include/linux/errname.h
+++ include/linux/errname.h
@@ -5,12 +5,62 @@
 #include <linux/stddef.h>
 
 #ifdef CONFIG_SYMBOLIC_ERRNAME
+
 const char *errname(int err);
+
+#include <linux/codetag.h>
+
+struct codetag_error_code {
+	const char		*str;
+	int			err;
+};
+
+/**
+ * ERR - return an error code that records the error site
+ *
+ * E.g., instead of
+ *   return -ENOMEM;
+ * Use
+ *   return -ERR(ENOMEM);
+ *
+ * Then, when a caller prints out the error with errname(), the error string
+ * will include the file and line number.
+ */
+#define ERR(_err)							\
+({									\
+	static struct codetag_error_code				\
+	__used								\
+	__section("error_code_tags")					\
+	__aligned(8) e = {						\
+		.str	= #_err " at " __FILE__ ":" __stringify(__LINE__),\
+		.err	= _err,						\
+	};								\
+									\
+	e.err;								\
+})
+
+int error_class(int err);
+bool error_matches(int err, int class);
+
 #else
+
+static inline int error_class(int err)
+{
+	return err;
+}
+
+static inline bool error_matches(int err, int class)
+{
+	return err == class;
+}
+
+#define ERR(_err)	_err
+
 static inline const char *errname(int err)
 {
 	return NULL;
 }
+
 #endif
 
 #endif /* _LINUX_ERRNAME_H */
