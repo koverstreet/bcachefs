e297869784f7d2af614e6ce7d5d5ee5212c74459
Suren Baghdasaryan <surenb@google.com>
2022-08-07 23:32:31 +0000

    lib: code tagging module support
    Add support for code tagging from dynamically loaded modules.
    
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Co-developed-by: Kent Overstreet <kent.overstreet@linux.dev>
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- kernel/module/main.c
+++ kernel/module/main.c
@@ -53,6 +53,7 @@
 #include <linux/bsearch.h>
 #include <linux/dynamic_debug.h>
 #include <linux/audit.h>
+#include <linux/codetag.h>
 #include <linux/cfi.h>
 #include <uapi/linux/module.h>
 #include "internal.h"
@@ -1147,6 +1148,7 @@ static void free_module(struct module *mod)
 {
 	trace_module_free(mod);
 
+	codetag_unload_module(mod);
 	mod_sysfs_teardown(mod);
 
 	/*
@@ -2865,6 +2867,8 @@ static int load_module(struct load_info *info, const char __user *uargs,
 
 	/* Get rid of temporary copy. */
 	free_copy(info, flags);
+
+	codetag_load_module(mod);
 
 	/* Done! */
 	trace_module_load(mod);
