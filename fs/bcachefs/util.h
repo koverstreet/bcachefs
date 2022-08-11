6b901906d95ef550b10ad18244dd4ccf43cf5f9d
Daniel Hill <daniel@gluo.nz>
2022-08-12 09:03:28 +1200

    bcachefs: time stats now uses the mean_and_variance module.
    Signed-off-by: Daniel Hill <daniel@gluo.nz>
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/util.h
+++ fs/bcachefs/util.h
@@ -17,6 +17,7 @@
 #include <linux/slab.h>
 #include <linux/vmalloc.h>
 #include <linux/workqueue.h>
+#include <linux/mean_and_variance.h>
 
 struct closure;
 
@@ -407,14 +408,18 @@ struct bch2_time_stat_buffer {
 
 struct bch2_time_stats {
 	spinlock_t	lock;
-	u64		count;
 	/* all fields are in nanoseconds */
-	u64		average_duration;
-	u64		average_frequency;
 	u64		max_duration;
+	u64             min_duration;
+	u64             max_freq;
+	u64             min_freq;
 	u64		last_event;
 	struct bch2_quantiles quantiles;
 
+	struct mean_and_variance	  duration_stats;
+	struct mean_and_variance_weighted duration_stats_weighted;
+	struct mean_and_variance	  freq_stats;
+	struct mean_and_variance_weighted freq_stats_weighted;
 	struct bch2_time_stat_buffer __percpu *buffer;
 };
 
