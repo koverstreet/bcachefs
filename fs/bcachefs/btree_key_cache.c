fb92832a4287ae1fdffa7aefdf20748e8f952f92
Kent Overstreet <kent.overstreet@gmail.com>
2022-08-11 12:23:21 -0400

    bcachefs: Kill BTREE_ITER_CACHED_(NOFILL|NOCREATE)
    These were used more prior to getting rid of the in-memory bucket arrays
    - they don't serve much purpose anymore, and deleting them lets us write
    better assertions.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@gmail.com>

--- fs/bcachefs/btree_key_cache.c
+++ fs/bcachefs/btree_key_cache.c
@@ -371,11 +371,6 @@ int bch2_btree_path_traverse_cached(struct btree_trans *trans, struct btree_path
 retry:
 	ck = bch2_btree_key_cache_find(c, path->btree_id, path->pos);
 	if (!ck) {
-		if (flags & BTREE_ITER_CACHED_NOCREATE) {
-			path->l[0].b = NULL;
-			return 0;
-		}
-
 		ck = btree_key_cache_create(c, path->btree_id, path->pos);
 		ret = PTR_ERR_OR_ZERO(ck);
 		if (ret)
@@ -411,7 +406,7 @@ retry:
 	path->l[0].lock_seq	= ck->c.lock.state.seq;
 	path->l[0].b		= (void *) ck;
 fill:
-	if (!ck->valid && !(flags & BTREE_ITER_CACHED_NOFILL)) {
+	if (!ck->valid) {
 		/*
 		 * Using the underscore version because we haven't set
 		 * path->uptodate yet:
@@ -432,6 +427,7 @@ fill:
 		set_bit(BKEY_CACHED_ACCESSED, &ck->flags);
 
 	path->uptodate = BTREE_ITER_UPTODATE;
+	BUG_ON(!ck->valid);
 	BUG_ON(btree_node_locked_type(path, 0) != btree_lock_want(path, 0));
 
 	return ret;
@@ -461,8 +457,6 @@ static int btree_key_cache_flush_pos(struct btree_trans *trans,
 			     BTREE_ITER_ALL_SNAPSHOTS);
 	bch2_trans_iter_init(trans, &c_iter, key.btree_id, key.pos,
 			     BTREE_ITER_CACHED|
-			     BTREE_ITER_CACHED_NOFILL|
-			     BTREE_ITER_CACHED_NOCREATE|
 			     BTREE_ITER_INTENT);
 	b_iter.flags &= ~BTREE_ITER_WITH_KEY_CACHE;
 
