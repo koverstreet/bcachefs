a969665827f7ccc7b5f25b9362fafec2ccd3a9e1
Kent Overstreet <kent.overstreet@linux.dev>
2022-08-29 13:01:10 -0400

    fixup! Code tagging based latency tracking

--- lib/codetag_time_stats.c
+++ lib/codetag_time_stats.c
@@ -52,7 +52,7 @@ static int time_stats_open(struct inode *inode, struct file *file)
 		return -ENOMEM;
 
 	codetag_lock_module_list(cttype, true);
-	iter->ct_iter = codetag_get_ct_iter(cttype);
+	codetag_init_iter(&iter->ct_iter, cttype);
 	codetag_lock_module_list(cttype, false);
 
 	file->private_data = iter;
