1c05040a0f0890bcc1ec4e19065f022bf71b281b
Kent Overstreet <kent.overstreet@linux.dev>
2022-09-27 17:17:23 -0400

    bcachefs: Factor out bch2_write_drop_io_error_ptrs()
    Move slowpath code to a separate, non-inline function.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/io.h
+++ fs/bcachefs/io.h
@@ -40,6 +40,7 @@ enum bch_write_flags {
 	BCH_WRITE_JOURNAL_SEQ_PTR	= (1 << 10),
 	BCH_WRITE_SKIP_CLOSURE_PUT	= (1 << 11),
 	BCH_WRITE_DONE			= (1 << 12),
+	BCH_WRITE_IO_ERROR		= (1 << 13),
 };
 
 static inline u64 *op_journal_seq(struct bch_write_op *op)
