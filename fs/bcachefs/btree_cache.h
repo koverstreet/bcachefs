af19ba832cfeaed0610436c7c1f2f3ad01d5750b
Daniel Hill <daniel@gluo.nz>
2022-09-30 16:37:15 +1300

    bcachefs: add counters for failed shrinker reclaim
    These counters should help us debug OOM issues.
    
    Signed-off-by: Daniel Hill <daniel@gluo.nz>

--- fs/bcachefs/btree_cache.h
+++ fs/bcachefs/btree_cache.h
@@ -125,6 +125,6 @@ static inline struct btree *btree_node_root(struct bch_fs *c, struct btree *b)
 
 void bch2_btree_node_to_text(struct printbuf *, struct bch_fs *,
 			     const struct btree *);
-void bch2_btree_cache_to_text(struct printbuf *, const struct bch_fs *);
+void bch2_btree_cache_to_text(struct printbuf *, const struct btree_cache *);
 
 #endif /* _BCACHEFS_BTREE_CACHE_H */
