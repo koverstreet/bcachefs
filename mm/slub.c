d99e40df863cf26bf908e7c254f1f5c44b5bd233
Kent Overstreet <kent.overstreet@linux.dev>
2022-11-06 17:33:24 -0500

    mm/slub: Mark slab_free_freelist_hook() __always_inline
    It seems we need to be more forceful with the compiler on this one.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- mm/slub.c
+++ mm/slub.c
@@ -1781,7 +1781,7 @@ static __always_inline bool slab_free_hook(struct kmem_cache *s,
 	return kasan_slab_free(s, x, init);
 }
 
-static inline bool slab_free_freelist_hook(struct kmem_cache *s,
+static __always_inline bool slab_free_freelist_hook(struct kmem_cache *s,
 					   void **head, void **tail,
 					   int *cnt)
 {
