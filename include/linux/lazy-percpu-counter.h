63d97cb1e113b2df088e986dd145a779cfa42c2d
Kent Overstreet <kent.overstreet@linux.dev>
2022-11-06 17:34:15 -0500

    lazy_percpu_counters: Inline fastpath
    This inlines the __lazy_percpu_counter_add() fastpath, when the counter
    is in percpu mode.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- include/linux/lazy-percpu-counter.h
+++ include/linux/lazy-percpu-counter.h
@@ -29,9 +29,55 @@ struct raw_lazy_percpu_counter {
 };
 
 void __lazy_percpu_counter_exit(struct raw_lazy_percpu_counter *c);
-void __lazy_percpu_counter_add(struct raw_lazy_percpu_counter *c,
+void lazy_percpu_counter_add_slowpath(struct raw_lazy_percpu_counter *c,
 			       unsigned long *last_wrap, s64 i);
 s64 __lazy_percpu_counter_read(struct raw_lazy_percpu_counter *c);
+
+/*
+ * We use the high bits of the atomic counter for a secondary counter, which is
+ * incremented every time the counter is touched. When the secondary counter
+ * wraps, we check the time the counter last wrapped, and if it was recent
+ * enough that means the update frequency has crossed our threshold and we
+ * switch to percpu mode:
+ */
+#define COUNTER_MOD_BITS		8
+#define COUNTER_MOD_MASK		~(~0ULL >> COUNTER_MOD_BITS)
+#define COUNTER_MOD_BITS_START		(64 - COUNTER_MOD_BITS)
+
+/*
+ * We use the low bit of the counter to indicate whether we're in atomic mode
+ * (low bit clear), or percpu mode (low bit set, counter is a pointer to actual
+ * percpu counters:
+ */
+#define COUNTER_IS_PCPU_BIT		1
+
+static inline u64 __percpu *lazy_percpu_counter_is_pcpu(u64 v)
+{
+	if (!(v & COUNTER_IS_PCPU_BIT))
+		return NULL;
+
+	v ^= COUNTER_IS_PCPU_BIT;
+	return (u64 __percpu *)(unsigned long)v;
+}
+
+/**
+ * __lazy_percpu_counter_add: Add a value to a lazy_percpu_counter
+ *
+ * @c: counter to modify
+ * @last_wrap: pointer to a timestamp, updated when mod counter wraps
+ * @i: value to add
+ */
+static inline void __lazy_percpu_counter_add(struct raw_lazy_percpu_counter *c,
+					     unsigned long *last_wrap, s64 i)
+{
+	u64 v = atomic64_read(&c->v);
+	u64 __percpu *pcpu_v = lazy_percpu_counter_is_pcpu(v);
+
+	if (likely(pcpu_v))
+		this_cpu_add(*pcpu_v, i);
+	else
+		lazy_percpu_counter_add_slowpath(c, last_wrap, i);
+}
 
 static inline void __lazy_percpu_counter_sub(struct raw_lazy_percpu_counter *c,
 					     unsigned long *last_wrap, s64 i)
--- lib/lazy-percpu-counter.c
+++ lib/lazy-percpu-counter.c
@@ -6,33 +6,6 @@
 #include <linux/lazy-percpu-counter.h>
 #include <linux/percpu.h>
 
-/*
- * We use the high bits of the atomic counter for a secondary counter, which is
- * incremented every time the counter is touched. When the secondary counter
- * wraps, we check the time the counter last wrapped, and if it was recent
- * enough that means the update frequency has crossed our threshold and we
- * switch to percpu mode:
- */
-#define COUNTER_MOD_BITS		8
-#define COUNTER_MOD_MASK		~(~0ULL >> COUNTER_MOD_BITS)
-#define COUNTER_MOD_BITS_START		(64 - COUNTER_MOD_BITS)
-
-/*
- * We use the low bit of the counter to indicate whether we're in atomic mode
- * (low bit clear), or percpu mode (low bit set, counter is a pointer to actual
- * percpu counters:
- */
-#define COUNTER_IS_PCPU_BIT		1
-
-static inline u64 __percpu *lazy_percpu_counter_is_pcpu(u64 v)
-{
-	if (!(v & COUNTER_IS_PCPU_BIT))
-		return NULL;
-
-	v ^= COUNTER_IS_PCPU_BIT;
-	return (u64 __percpu *)(unsigned long)v;
-}
-
 static inline s64 lazy_percpu_counter_atomic_val(s64 v)
 {
 	/* Ensure output is sign extended properly: */
@@ -100,15 +73,8 @@ s64 __lazy_percpu_counter_read(struct raw_lazy_percpu_counter *c)
 }
 EXPORT_SYMBOL_GPL(__lazy_percpu_counter_read);
 
-/**
- * __lazy_percpu_counter_add: Add a value to a lazy_percpu_counter
- *
- * @c: counter to modify
- * @last_wrap: pointer to a timestamp, updated when mod counter wraps
- * @i: value to add
- */
-void __lazy_percpu_counter_add(struct raw_lazy_percpu_counter *c,
-			       unsigned long *last_wrap, s64 i)
+void lazy_percpu_counter_add_slowpath(struct raw_lazy_percpu_counter *c,
+				      unsigned long *last_wrap, s64 i)
 {
 	u64 atomic_i;
 	u64 old, v = atomic64_read(&c->v);
@@ -138,4 +104,4 @@ void __lazy_percpu_counter_add(struct raw_lazy_percpu_counter *c,
 			*last_wrap = now;
 	}
 }
-EXPORT_SYMBOL(__lazy_percpu_counter_add);
+EXPORT_SYMBOL(lazy_percpu_counter_add_slowpath);
