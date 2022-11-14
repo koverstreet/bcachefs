2cb56c44e1a4eb08aed10a7ef74ce15a7cfe6a84
Kent Overstreet <kent.overstreet@linux.dev>
2022-11-13 20:01:42 -0500

    bcachefs: Fixes for building in userspace
     - Marking a non-static function as inline doesn't actually work and is
       now causing problems - drop that
    
     - Introduce BCACHEFS_LOG_PREFIX for when we want to prefix log messages
       with bcachefs (filesystem name)
    
     - Userspace doesn't have real percpu variables (maybe we can get this
       fixed someday), put an #ifdef around bch2_disk_reservation_add()
       fastpath
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/buckets.h
+++ fs/bcachefs/buckets.h
@@ -274,6 +274,7 @@ int __bch2_disk_reservation_add(struct bch_fs *,
 static inline int bch2_disk_reservation_add(struct bch_fs *c, struct disk_reservation *res,
 					    u64 sectors, int flags)
 {
+#ifdef __KERNEL__
 	u64 old, new;
 
 	do {
@@ -287,6 +288,9 @@ static inline int bch2_disk_reservation_add(struct bch_fs *c, struct disk_reserv
 	this_cpu_add(*c->online_reserved, sectors);
 	res->sectors			+= sectors;
 	return 0;
+#else
+	return __bch2_disk_reservation_add(c, res, sectors, flags);
+#endif
 }
 
 static inline struct disk_reservation
