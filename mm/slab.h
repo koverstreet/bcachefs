83240227c1153e933689cad3cbf922b98b380133
Suren Baghdasaryan <surenb@google.com>
2022-12-14 02:45:17 +0000

    codetag: debug: skip objext checking when it's for objext itself
    objext objects are created with __GFP_NO_OBJ_EXT flag and therefore have
    no corresponding objext themselves (otherwise we would get an infinite
    recursion). When freeing these objects their codetag will be empty and
    when CONFIG_ALLOC_TAGGING_DEBUG is enabled this will lead to false
    warnings. Introduce CODETAG_EMPTY special codetag value to mark
    allocations which intentionally lack codetag to avoid these warnings.
    Set objext codetags to CODETAG_EMPTY before freeing to indicate that
    the codetag is expected to be empty.
    
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- mm/slab.h
+++ mm/slab.h
@@ -464,6 +464,31 @@ static inline struct slabobj_ext *slab_obj_exts(struct slab *slab)
 int alloc_slab_obj_exts(struct slab *slab, struct kmem_cache *s,
 			gfp_t gfp, bool new_slab);
 
+
+#ifdef CONFIG_ALLOC_TAGGING_DEBUG
+
+static inline void mark_objexts_empty(struct slabobj_ext *obj_exts)
+{
+	struct slabobj_ext *slab_exts;
+	struct slab *obj_exts_slab;
+
+	obj_exts_slab = virt_to_slab(obj_exts);
+	slab_exts = slab_obj_exts(obj_exts_slab);
+	if (slab_exts) {
+		unsigned int offs = obj_to_index(obj_exts_slab->slab_cache,
+						 obj_exts_slab, obj_exts);
+		/* codetag should be NULL */
+		WARN_ON(slab_exts[offs].ref.ct);
+		set_codetag_empty(&slab_exts[offs].ref);
+	}
+}
+
+#else /* CONFIG_ALLOC_TAGGING_DEBUG */
+
+static inline void mark_objexts_empty(struct slabobj_ext *obj_exts) {}
+
+#endif /* CONFIG_ALLOC_TAGGING_DEBUG */
+
 static inline void free_slab_obj_exts(struct slab *slab)
 {
 	struct slabobj_ext *obj_exts;
@@ -472,6 +497,14 @@ static inline void free_slab_obj_exts(struct slab *slab)
 		return;
 
 	obj_exts = slab_obj_exts(slab);
+	/*
+	 * obj_exts was created with __GFP_NO_OBJ_EXT flag, therefore its
+	 * corresponding extension will be NULL. alloc_tag_sub() will throw a
+	 * warning if slab has extensions but the extension of an object is
+	 * NULL, therefore replace NULL with CODETAG_EMPTY to indicate that
+	 * the extension for obj_exts is expected to be NULL.
+	 */
+	mark_objexts_empty(obj_exts);
 	kfree(obj_exts);
 	slab->obj_exts = 0;
 }
