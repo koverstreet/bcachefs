a68d342dc0158cdf2abb6c967125fddab98f43ab
Suren Baghdasaryan <surenb@google.com>
2022-12-23 00:26:19 +0000

    codetag: debug: mark codetags for reserved pages as empty
    To avoid debug warnings while freeing reserved pages which were not
    allocated with usual allocators, mark their codetags as empty before
    freeing.
    Maybe we can annotate reserved pages correctly and avoid this?
    
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- include/linux/alloc_tag.h
+++ include/linux/alloc_tag.h
@@ -87,6 +87,7 @@ static inline void set_codetag_empty(union codetag_ref *ref)
 #else /* CONFIG_MEM_ALLOC_PROFILING_DEBUG */
 
 static inline bool is_codetag_empty(union codetag_ref *ref) { return false; }
+static inline void set_codetag_empty(union codetag_ref *ref) {}
 
 #endif /* CONFIG_MEM_ALLOC_PROFILING_DEBUG */
 
@@ -153,6 +154,7 @@ static inline void alloc_tag_sub(union codetag_ref *ref, size_t bytes) {}
 static inline void alloc_tag_sub_noalloc(union codetag_ref *ref, size_t bytes) {}
 static inline void alloc_tag_add(union codetag_ref *ref, struct alloc_tag *tag,
 				 size_t bytes) {}
+static inline void set_codetag_empty(union codetag_ref *ref) {}
 
 #endif
 
