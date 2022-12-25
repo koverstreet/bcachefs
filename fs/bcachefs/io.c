baf7ae9374bd652f49c6716d93da1a989b77326b
Kent Overstreet <kent.overstreet@linux.dev>
2022-12-24 22:53:24 -0500

    bcachefs: CONFIG_BCACHEFS_NO_IO
    This adds a kconfig option for disabling low level IO calls, for
    performance testing.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/io.c
+++ fs/bcachefs/io.c
@@ -183,6 +183,12 @@ void bch2_submit_wbio_replicas(struct bch_write_bio *wbio, struct bch_fs *c,
 
 			n->have_io_ref		= true;
 			bio_set_dev(&n->bio, ca->disk_sb.bdev);
+
+			if (IS_ENABLED(CONFIG_BCACHEFS_NO_IO) && type != BCH_DATA_BTREE) {
+				bio_endio(&n->bio);
+				continue;
+			}
+
 			submit_bio(&n->bio);
 		} else {
 			n->have_io_ref		= false;
@@ -1158,7 +1164,8 @@ static void __bch2_read_endio(struct work_struct *work)
 	}
 
 	csum = bch2_checksum_bio(c, crc.csum_type, nonce, src);
-	if (bch2_crc_cmp(csum, rbio->pick.crc.csum))
+	if (bch2_crc_cmp(csum, rbio->pick.crc.csum) &&
+	    !IS_ENABLED(CONFIG_BCACHEFS_NO_IO))
 		goto csum_err;
 
 	if (unlikely(rbio->narrow_crcs))
@@ -1402,6 +1409,11 @@ noclone:
 		     bio_sectors(&rbio->bio));
 
 	if (likely(!(flags & BCH_READ_IN_RETRY))) {
+		if (IS_ENABLED(CONFIG_BCACHEFS_NO_IO)) {
+			bio_endio(&rbio->bio);
+			return ret;
+		}
+
 		submit_bio(&rbio->bio);
 	} else {
 		submit_bio_wait(&rbio->bio);
