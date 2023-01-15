74e885cb19c03905d7338f12559d55ff3cd4f264
Kent Overstreet <kent.overstreet@linux.dev>
2023-01-15 06:42:36 -0500

    fixup mm: enable slab allocation tagging for kmalloc and friends

--- mm/slab.h
+++ mm/slab.h
@@ -493,9 +493,6 @@ static inline void free_slab_obj_exts(struct slab *slab)
 {
 	struct slabobj_ext *obj_exts;
 
-	if (!memcg_kmem_enabled() && is_kmem_only_obj_ext())
-		return;
-
 	obj_exts = slab_obj_exts(slab);
 	/*
 	 * obj_exts was created with __GFP_NO_OBJ_EXT flag, therefore its
@@ -504,7 +501,8 @@ static inline void free_slab_obj_exts(struct slab *slab)
 	 * NULL, therefore replace NULL with CODETAG_EMPTY to indicate that
 	 * the extension for obj_exts is expected to be NULL.
 	 */
-	mark_objexts_empty(obj_exts);
+	if (obj_exts)
+		mark_objexts_empty(obj_exts);
 	kfree(obj_exts);
 	slab->obj_exts = 0;
 }
@@ -514,10 +512,6 @@ static inline struct slabobj_ext *prepare_slab_obj_exts_hook(struct kmem_cache *
 	struct slab *slab;
 
 	if (!p)
-		return NULL;
-
-	/* If kmem is the only extension then the vector will be created conditionally */
-	if (is_kmem_only_obj_ext())
 		return NULL;
 
 	if (s->flags & SLAB_NO_OBJ_EXT)
