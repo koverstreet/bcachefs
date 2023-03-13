8178f49de019e05c1b17fdfae846d06dc559031e
Kent Overstreet <kent.overstreet@linux.dev>
2023-03-13 15:40:16 -0400

    fixup! lib: add allocation tagging support for memory allocation profiling
     - fix the warning on module unload
     - add a show_mem() hook
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- lib/show_mem.c
+++ lib/show_mem.c
@@ -7,6 +7,7 @@
 
 #include <linux/mm.h>
 #include <linux/cma.h>
+#include <linux/seq_buf.h>
 
 void __show_mem(unsigned int filter, nodemask_t *nodemask, int max_zone_idx)
 {
@@ -41,4 +42,16 @@ void __show_mem(unsigned int filter, nodemask_t *nodemask, int max_zone_idx)
 #ifdef CONFIG_MEMORY_FAILURE
 	printk("%lu pages hwpoisoned\n", atomic_long_read(&num_poisoned_pages));
 #endif
+	if (IS_ENABLED(CONFIG_MEM_ALLOC_PROFILING)) {
+		struct seq_buf s;
+		char *buf = kmalloc(4096, GFP_ATOMIC);
+
+		if (buf) {
+			printk("Memory allocations:\n");
+			seq_buf_init(&s, buf, 4096);
+			alloc_tags_show_mem_report(&s);
+			printk("%s", buf);
+			kfree(buf);
+		}
+	}
 }
