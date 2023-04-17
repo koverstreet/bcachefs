bd1f4873db0451a3f004062f9ca09a978605943a
John Johansen <john.johansen@canonical.com>
2023-04-17 02:57:55 -0700

    apparmor: fix profile verification and enable it
    The transition table size was not being set by compat mappings
    resulting in the profile verification code not being run. Unfortunately
    the checks were also buggy not being correctly updated from the old
    accept perms, to the new layout.
    
    Also indicate to userspace that the kernel has the permstable verification
    fixes.
    
    BugLink: http://bugs.launchpad.net/bugs/2017903
    Fixes: 670f31774ab6 ("apparmor: verify permission table indexes")
    Signed-off-by: John Johansen <john.johansen@canonical.com>
    Reviewed-by: Jon Tourville <jontourville@me.com>

--- security/apparmor/apparmorfs.c
+++ security/apparmor/apparmorfs.c
@@ -2354,7 +2354,7 @@ static struct aa_sfs_entry aa_sfs_entry_policy[] = {
 	AA_SFS_FILE_BOOLEAN("set_load",		1),
 	/* number of out of band transitions supported */
 	AA_SFS_FILE_U64("outofband",		MAX_OOB_SUPPORTED),
-	AA_SFS_FILE_U64("permstable32_version",	1),
+	AA_SFS_FILE_U64("permstable32_version",	2),
 	AA_SFS_FILE_STRING("permstable32", PERMS32STR),
 	{ }
 };
