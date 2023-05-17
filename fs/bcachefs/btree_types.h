d5e83f2e6602ae002017e3bb395720801eca8a4c
Kent Overstreet <kent.overstreet@linux.dev>
2023-05-17 01:25:14 -0400

    bcachefs: switch to jitalloc
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/btree_types.h
+++ fs/bcachefs/btree_types.h
@@ -19,6 +19,8 @@ struct open_bucket;
 struct btree_update;
 struct btree_trans;
 
+typedef void (*compiled_unpack_fn)(struct bkey *, const struct bkey_packed *);
+
 #define MAX_BSETS		3U
 
 struct btree_nr_keys {
@@ -86,6 +88,7 @@ struct btree {
 
 	struct btree_node	*data;
 	void			*aux_data;
+	compiled_unpack_fn	unpack;
 
 	/*
 	 * Sets of sorted keys - the real btree node - plus a binary search tree
