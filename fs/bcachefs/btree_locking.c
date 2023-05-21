44cc1ceb8863012dd30ccacb128fd5bdb51b3a15
Kent Overstreet <kent.overstreet@linux.dev>
2023-05-20 20:57:55 -0400

    six locks: Kill six_lock_pcpu_(alloc|free)
    six_lock_pcpu_alloc() is an unsafe interface: it's not safe to allocate
    or free the percpu reader count on an existing lock that's in use, the
    only safe time to allocate percpu readers is when the lock is first
    being initialized.
    
    This patch adds a flags parameter to six_lock_init(), and instead of
    six_lock_pcpu_free() we now expose six_lock_exit(), which does the same
    thing but is less likely to be misused.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/btree_locking.c
+++ fs/bcachefs/btree_locking.c
@@ -6,9 +6,13 @@
 
 static struct lock_class_key bch2_btree_node_lock_key;
 
-void bch2_btree_lock_init(struct btree_bkey_cached_common *b)
+void bch2_btree_lock_init(struct btree_bkey_cached_common *b,
+			  enum six_lock_init_flags flags)
 {
-	__six_lock_init(&b->lock, "b->c.lock", &bch2_btree_node_lock_key);
+	__six_lock_init(&b->lock, "b->c.lock", &bch2_btree_node_lock_key, flags);
+#ifdef CONFIG_DEBUG_LOCK_ALLOC
+	lockdep_set_no_check_recursion(&b->lock.dep_map);
+#endif
 }
 
 #ifdef CONFIG_LOCKDEP
