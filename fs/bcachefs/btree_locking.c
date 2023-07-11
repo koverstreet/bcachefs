45513192e055c10f4c3b31aa19777db5f0d79af0
Kent Overstreet <kent.overstreet@linux.dev>
2023-07-11 19:42:52 -0400

    bcachefs: Use lock_class_is_held() for btree locking asserts
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/btree_locking.c
+++ fs/bcachefs/btree_locking.c
@@ -16,10 +16,7 @@ void bch2_btree_lock_init(struct btree_bkey_cached_common *b,
 #ifdef CONFIG_LOCKDEP
 void bch2_assert_btree_nodes_not_locked(void)
 {
-#if 0
-	//Re-enable when lock_class_is_held() is merged:
 	BUG_ON(lock_class_is_held(&bch2_btree_node_lock_key));
-#endif
 }
 #endif
 
