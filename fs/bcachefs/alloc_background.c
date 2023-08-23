8ed33cae024bc7950726fa92f6faed7f689a7a2c
Tux3 <tux3@users.noreply.github.com>
2023-08-23 17:12:24 +0200

    bcachefs: Fix missing parens in stripe data_type check
    Suggested-by: gcc

--- fs/bcachefs/alloc_background.c
+++ fs/bcachefs/alloc_background.c
@@ -260,7 +260,7 @@ int bch2_alloc_v4_invalid(const struct bch_fs *c, struct bkey_s_c k,
 		return -BCH_ERR_invalid_bkey;
 	}
 
-	if (!!a.v->stripe != a.v->data_type == BCH_DATA_stripe) {
+	if (!!a.v->stripe != (a.v->data_type == BCH_DATA_stripe)) {
 		prt_printf(err, "data_type %s but stripe==0",
 			   bch2_data_types[a.v->data_type]);
 		return -BCH_ERR_invalid_bkey;
