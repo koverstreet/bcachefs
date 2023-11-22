4e12ba584c0579363bb16508bce749e46cb313a7
Kent Overstreet <kent.overstreet@linux.dev>
2023-11-22 18:17:06 -0500

    mm: shrinker: Add new stats for .to_text()
    Add a few new shrinker stats.
    
    number of objects requested to free, number of objects freed:
    
    Shrinkers won't necessarily free all objects requested for a variety of
    reasons, but if the two counts are wildly different something is likely
    amiss.
    
    .scan_objects runtime:
    
    If one shrinker is taking an excessive amount of time to free
    objects that will block kswapd from running other shrinkers.
    
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Qi Zheng <zhengqi.arch@bytedance.com>
    Cc: Roman Gushchin <roman.gushchin@linux.dev>
    Cc: linux-mm@kvack.org
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- include/linux/shrinker.h
+++ include/linux/shrinker.h
@@ -118,6 +118,12 @@ struct shrinker {
 #endif
 	/* objs pending delete, per node */
 	atomic_long_t *nr_deferred;
+
+	atomic_long_t	objects_requested_to_free;
+	atomic_long_t	objects_freed;
+	unsigned long	last_freed;	/* timestamp, in jiffies */
+	unsigned long	last_scanned;	/* timestamp, in jiffies */
+	atomic64_t	ns_run;
 };
 #define DEFAULT_SEEKS 2 /* A good number if you don't know better. */
 
