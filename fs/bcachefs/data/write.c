13d6fbba2ece6d43eecb07a7daeba593ce849066
Kent Overstreet <kent.overstreet@linux.dev>
2023-11-30 00:24:20 -0500

    bcachefs: extent_block_checksums
    todo:
     - nonces?
     - alignment
     - extent merging
     - need a better bch2_checksum_merge()
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/data/write.c
+++ fs/bcachefs/data/write.c
@@ -915,7 +915,7 @@ static void bch2_write_endio(struct bio *bio)
 static inline bool use_block_checksums(struct bch_write_op *op,
 				       struct bch_extent_crc_unpacked crc)
 {
-	if (!crc.csum_type &&
+	if (!crc.csum_type ||
 	    crc.compression_type ||
 	    !op->opts.checksum_blocksize)
 		return false;
@@ -961,8 +961,16 @@ static void init_append_extent(struct bch_write_op *op,
 		e->v.csum_blocksize_bits = ilog2(op->opts.checksum_blocksize >> 9);
 		e->v.nr_ptrs = wp->ptrs.nr;
 
+		struct bkey_s_extent_block_checksums e_s = extent_block_checksums_i_to_s(e);
+
+		unsigned nr_crcs = extent_block_checksums_nr_crcs(e_s.c);
+		for (unsigned i = 0; i < nr_crcs; i++) {
+			void *crc = extent_block_checksums_crc(e_s, i);
+
+		}
+
 		ptrs = e->v.ptrs;
-		set_bkey_val_u64s(&e->k, extent_block_checksums_val_u64s(extent_block_checksums_i_to_s_c(e)));
+		set_bkey_val_u64s(&e->k, extent_block_checksums_val_u64s(e_s.c));
 	}
 
 	bch2_alloc_sectors_append_ptrs_inlined(op->c, wp, crc.compressed_size,
