2f1c094e8b1bd2d91ca898f7cae5d1b33d677aae
Kent Overstreet <kent.overstreet@linux.dev>
2023-11-30 22:14:53 -0500

    bcachefs: Checksum blocksize option
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/bcachefs_format.h
+++ fs/bcachefs/bcachefs_format.h
@@ -889,6 +889,7 @@ LE64_BITMASK(BCH_SB_CASEFOLD,		struct bch_sb, flags[6], 22, 23);
 LE64_BITMASK(BCH_SB_REBALANCE_AC_ONLY,	struct bch_sb, flags[6], 23, 24);
 LE64_BITMASK(BCH_SB_IO_ERROR_THRESHOLD,	struct bch_sb, flags[6], 24, 40);
 LE64_BITMASK(BCH_SB_LAST_FSCK_VERSION,	struct bch_sb, flags[6], 40, 56);
+LE64_BITMASK(BCH_SB_CSUM_BLOCKSIZE,	struct bch_sb, flags[6], 56, 64);
 
 static inline __u64 BCH_SB_COMPRESSION_TYPE(const struct bch_sb *sb)
 {
