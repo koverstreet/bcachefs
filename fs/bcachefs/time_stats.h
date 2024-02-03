0429c6f8cb69debfdf46c04cedd48545b50a43a0
Darrick J. Wong <djwong@kernel.org>
2024-02-03 13:00:56 -0800

    time_stats: don't print any output if event count is zero
    There's no point in printing an empty report for no data, so add a flag
    that allows us to do that.
    
    Signed-off-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/time_stats.h
+++ fs/bcachefs/time_stats.h
@@ -143,8 +143,9 @@ static inline bool track_event_change(struct bch2_time_stats *stats, bool v)
 	return false;
 }
 
+#define TIME_STATS_PRINT_NO_ZEROES	(1U << 0)	/* print nothing if zero count */
 struct seq_buf;
-void bch2_time_stats_to_seq_buf(struct seq_buf *, struct bch2_time_stats *);
+void bch2_time_stats_to_seq_buf(struct seq_buf *, struct bch2_time_stats *, unsigned int);
 
 void bch2_time_stats_exit(struct bch2_time_stats *);
 void bch2_time_stats_init(struct bch2_time_stats *);
