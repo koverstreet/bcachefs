0d57f9938fec57a6f5e6ef08f7c3f3146a07c4b8
Kent Overstreet <kent.overstreet@linux.dev>
2024-02-07 00:45:09 -0500

    bcachefs: check dirent->d_parent_subvol
    Check that d_parent_subvol makes sense - the dirent's snapshot must be
    visible in d_parent_subvol (i.e. an ancestor of d_parent_subvol's
    snapshot) in order to be visible.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/sb-errors_types.h
+++ fs/bcachefs/sb-errors_types.h
@@ -256,7 +256,9 @@
 	x(inode_bi_subvol_wrong,				248)	\
 	x(inode_points_to_missing_dirent,			249)	\
 	x(inode_points_to_wrong_dirent,				250)	\
-	x(inode_bi_parent_nonzero,				251)
+	x(inode_bi_parent_nonzero,				251)	\
+	x(dirent_to_missing_parent_subvol,			252)	\
+	x(dirent_not_visible_in_parent_subvol,			253)
 
 enum bch_sb_error_id {
 #define x(t, n) BCH_FSCK_ERR_##t = n,
