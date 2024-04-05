b80d9fe56372931154a31c51623c700392e28652
Kent Overstreet <kent.overstreet@linux.dev>
2024-04-05 02:43:31 -0400

    foo

--- fs/bcachefs/btree_gc.c
+++ fs/bcachefs/btree_gc.c
@@ -209,7 +209,7 @@ static int btree_check_node_boundaries(struct btree_trans *trans, struct btree *
 	if (bpos_eq(expected_start, cur->data->min_key))
 		return 0;
 
-	prt_printf(&buf, "  at btree %s level %u:\n  parent: ",
+	prt_printf(&buf, " at btree %s level %u:\n  parent: ",
 		   bch2_btree_id_str(b->c.btree_id), b->c.level);
 	bch2_bkey_val_to_text(&buf, c, bkey_i_to_s_c(&b->key));
 
@@ -277,7 +277,7 @@ static int btree_repair_node_end(struct btree_trans *trans, struct btree *b,
 	if (bpos_eq(child->key.k.p, b->key.k.p))
 		return 0;
 
-	prt_printf(&buf, "at btree %s level %u:\n  parent: ",
+	prt_printf(&buf, " at btree %s level %u:\n  parent: ",
 		   bch2_btree_id_str(b->c.btree_id), b->c.level);
 	bch2_bkey_val_to_text(&buf, c, bkey_i_to_s_c(&b->key));
 
--- fs/bcachefs/btree_io.c
+++ fs/bcachefs/btree_io.c
@@ -1346,11 +1346,20 @@ start:
 	if (saw_error &&
 	    !btree_node_read_error(b) &&
 	    c->curr_recovery_pass != BCH_RECOVERY_PASS_scan_for_btree_nodes) {
+		if (b->c.level) {
+			/*
+			 * did we recover from good replica? otherwise,
+			 * kick off a btree node scan?
+			 */
+
+		}
+#if 0
 		printbuf_reset(&buf);
 		bch2_bpos_to_text(&buf, b->key.k.p);
 		bch_err_ratelimited(c, "%s: rewriting btree node at btree=%s level=%u %s due to error",
 			 __func__, bch2_btree_id_str(b->c.btree_id), b->c.level, buf.buf);
 
+#endif
 		bch2_btree_node_rewrite_async(c, b);
 	}
 
