947f2236a4aadb6208867f9d92d8f03c3e620a6d
Kent Overstreet <kent.overstreet@linux.dev>
2024-04-06 21:52:58 -0400

    foo

--- fs/bcachefs/btree/check.c
+++ fs/bcachefs/btree/check.c
@@ -461,7 +461,6 @@ again:
 
 	while ((k = bch2_btree_and_journal_iter_peek(c, &iter)).k) {
 		bch2_bkey_buf_reassemble(&cur_k, k);
-		bch2_btree_and_journal_iter_advance(&iter);
 
 		cur = bch2_btree_node_get_noiter(trans, cur_k.k,
 					b->c.btree_id, b->c.level - 1,
@@ -487,6 +486,8 @@ again:
 			goto err;
 
 		have_child = true;
+
+		bch2_btree_and_journal_iter_advance(&iter);
 	}
 
 	printbuf_reset(&buf);
