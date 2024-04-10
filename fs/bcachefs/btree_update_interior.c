ccc616d37af0c7c44df471100e7d390c9acb7ab6
Kent Overstreet <kent.overstreet@linux.dev>
2024-04-09 23:23:08 -0400

    bcachefs: bch2_trans_unlock() must always be followed by relock() or begin()
    We're about to add new asserts for btree_trans locking consistency, and
    part of that requires that aren't using the btree_trans while it's
    unlocked.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/btree_update_interior.c
+++ fs/bcachefs/btree_update_interior.c
@@ -729,9 +729,6 @@ err:
 	 */
 	b = READ_ONCE(as->b);
 	if (b) {
-		btree_path_idx_t path_idx = get_unlocked_mut_path(trans,
-						as->btree_id, b->c.level, b->key.k.p);
-		struct btree_path *path = trans->paths + path_idx;
 		/*
 		 * @b is the node we did the final insert into:
 		 *
@@ -755,6 +752,10 @@ err:
 		 * have here:
 		 */
 		bch2_trans_unlock(trans);
+		bch2_trans_begin(trans);
+		btree_path_idx_t path_idx = get_unlocked_mut_path(trans,
+						as->btree_id, b->c.level, b->key.k.p);
+		struct btree_path *path = trans->paths + path_idx;
 		btree_node_lock_nopath_nofail(trans, &b->c, SIX_LOCK_intent);
 		mark_btree_node_locked(trans, path, b->c.level, BTREE_NODE_INTENT_LOCKED);
 		path->l[b->c.level].lock_seq = six_lock_seq(&b->c.lock);
