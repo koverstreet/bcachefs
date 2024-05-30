9b594551f3ad6021e55cf5274fbb00ff7e390967
Kent Overstreet <kent.overstreet@linux.dev>
2024-05-30 15:40:25 -0400

    fork: Export create_io_thread()
    create_io_thread() creates a thread for doing work on behalf of some
    user process - export it so it can be used in thread_with_file, which
    spins up a kthread connected to a file descriptor.
    
    Cc: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- kernel/fork.c
+++ kernel/fork.c
@@ -2552,6 +2552,7 @@ struct task_struct *create_io_thread(int (*fn)(void *), void *arg, int node)
 
 	return copy_process(NULL, 0, node, &args);
 }
+EXPORT_SYMBOL_GPL(create_io_thread);
 
 /*
  *  Ok, this is the main fork-routine.
