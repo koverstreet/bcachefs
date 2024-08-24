e92c4112eca0a590fb6cf007115f2e7425dccdf2
Kent Overstreet <kent.overstreet@linux.dev>
2024-08-23 21:07:45 -0400

    fsck_trigger_fn

--- fs/bcachefs/bcachefs.h
+++ fs/bcachefs/bcachefs.h
@@ -465,6 +465,7 @@ enum bch_time_stats {
 };
 
 #include "alloc_types.h"
+#include "bkey.h"
 #include "btree_gc_types.h"
 #include "btree_types.h"
 #include "btree_node_scan_types.h"
@@ -692,6 +693,9 @@ enum bch_write_ref {
 #undef x
 	BCH_WRITE_REF_NR,
 };
+
+typedef void (*fsck_trigger_fn)(struct bch_fs *, enum btree_id,
+				struct bkey_s_c, struct bkey_s_c);
 
 struct bch_fs {
 	struct closure		cl;
@@ -942,8 +946,10 @@ struct bch_fs {
 	 */
 	seqcount_t		gc_pos_lock;
 	struct gc_pos		gc_pos;
-	struct percpu_rw_semaphore
 				check_allocations_done_lock;
+
+	fsck_trigger_fn		fsck_trigger;
+	void			*fsck_trigger_priv;
 
 	struct mutex		gc_gens_lock;
 
