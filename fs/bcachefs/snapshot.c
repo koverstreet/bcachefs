a4892f539b27ca1e8fbd1d1cd2c0b4fc1160ce1a
Kent Overstreet <kent.overstreet@linux.dev>
2024-12-09 00:06:34 -0500

    bcachefs: BTREE_UPDATE_no_snapshot_whitouts
    Add an explicit flag to disable snapshot whitouts; this fixes a sapshot
    deletion bug.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/snapshot.c
+++ fs/bcachefs/snapshot.c
@@ -1425,7 +1425,8 @@ static int delete_dead_snapshots_process_key(struct btree_trans *trans,
 
 	if (snapshot_list_has_id(&d->delete_leaves, k.k->p.snapshot))
 		return bch2_btree_delete_at(trans, iter,
-					    BTREE_UPDATE_internal_snapshot_node);
+					    BTREE_UPDATE_internal_snapshot_node|
+					    BTREE_UPDATE_no_snapshot_whiteouts);
 
 	u32 live_child = interior_delete_has_id(&d->delete_interior, k.k->p.snapshot);
 	if (live_child) {
@@ -1448,7 +1449,8 @@ static int delete_dead_snapshots_process_key(struct btree_trans *trans,
 					     BTREE_UPDATE_internal_snapshot_node)
 			 : 0) ?:
 			bch2_btree_delete_at(trans, iter,
-					     BTREE_UPDATE_internal_snapshot_node);
+					     BTREE_UPDATE_internal_snapshot_node|
+					     BTREE_UPDATE_no_snapshot_whiteouts);
 	}
 
 	return 0;
