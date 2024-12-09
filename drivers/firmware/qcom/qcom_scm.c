9dd42cd412c8b83c8ffe746c0a0b1a65f2f0cd2b
Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
2024-12-09 15:27:54 +0100

    firmware: qcom: scm: Fix missing read barrier in qcom_scm_is_available()
    commit 0a744cceebd0480cb39587b3b1339d66a9d14063 upstream.
    
    Commit 2e4955167ec5 ("firmware: qcom: scm: Fix __scm and waitq
    completion variable initialization") introduced a write barrier in probe
    function to store global '__scm' variable.  It also claimed that it
    added a read barrier, because as we all known barriers are paired (see
    memory-barriers.txt: "Note that write barriers should normally be paired
    with read or address-dependency barriers"), however it did not really
    add it.
    
    The offending commit used READ_ONCE() to access '__scm' global which is
    not a barrier.
    
    The barrier is needed so the store to '__scm' will be properly visible.
    This is most likely not fatal in current driver design, because missing
    read barrier would mean qcom_scm_is_available() callers will access old
    value, NULL.  Driver does not support unbinding and does not correctly
    handle probe failures, thus there is no risk of stale or old pointer in
    '__scm' variable.
    
    However for code correctness, readability and to be sure that we did not
    mess up something in this tricky topic of SMP barriers, add a read
    barrier for accessing '__scm'.  Change also comment from useless/obvious
    what does barrier do, to what is expected: which other parts of the code
    are involved here.
    
    Fixes: 2e4955167ec5 ("firmware: qcom: scm: Fix __scm and waitq completion variable initialization")
    Cc: stable@vger.kernel.org
    Reviewed-by: Bartosz Golaszewski <bartosz.golaszewski@linaro.org>
    Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
    Link: https://lore.kernel.org/r/20241209-qcom-scm-missing-barriers-and-all-sort-of-srap-v2-1-9061013c8d92@linaro.org
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/firmware/qcom/qcom_scm.c
+++ drivers/firmware/qcom/qcom_scm.c
@@ -1867,7 +1867,8 @@ static int qcom_scm_qseecom_init(struct qcom_scm *scm)
  */
 bool qcom_scm_is_available(void)
 {
-	return !!READ_ONCE(__scm);
+	/* Paired with smp_store_release() in qcom_scm_probe */
+	return !!smp_load_acquire(&__scm);
 }
 EXPORT_SYMBOL_GPL(qcom_scm_is_available);
 
@@ -2024,7 +2025,7 @@ static int qcom_scm_probe(struct platform_device *pdev)
 	if (ret)
 		return ret;
 
-	/* Let all above stores be available after this */
+	/* Paired with smp_load_acquire() in qcom_scm_is_available(). */
 	smp_store_release(&__scm, scm);
 
 	irq = platform_get_irq_optional(pdev, 0);
