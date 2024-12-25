e8a481f636ec4897aaa5e094e2e3a6d7c39a8e6d
Kent Overstreet <kent.overstreet@linux.dev>
2024-12-25 11:18:32 -0500

    lock seq debug

--- fs/bcachefs/btree/interior.c
+++ fs/bcachefs/btree/interior.c
@@ -1755,6 +1755,8 @@ static int btree_split(struct btree_update *as, struct btree_trans *trans,
 
 	bch2_btree_interior_update_will_free_node(as, b);
 
+	bch2_trans_verify_locks(trans);
+
 	if (n3) {
 		bch2_btree_update_get_open_buckets(as, n3);
 		bch2_btree_node_write_trans(trans, n3, SIX_LOCK_write, 0);
@@ -1765,6 +1767,8 @@ static int btree_split(struct btree_update *as, struct btree_trans *trans,
 	}
 	bch2_btree_update_get_open_buckets(as, n1);
 	bch2_btree_node_write_trans(trans, n1, SIX_LOCK_write, 0);
+
+	bch2_trans_verify_locks(trans);
 
 	/*
 	 * The old node must be freed (in memory) _before_ unlocking the new
@@ -1779,7 +1783,6 @@ static int btree_split(struct btree_update *as, struct btree_trans *trans,
 	if (n2)
 		bch2_trans_node_add(trans, trans->paths + path2, n2);
 	bch2_trans_node_add(trans, trans->paths + path1, n1);
-
 out:
 	if (path2) {
 		__bch2_btree_path_unlock(trans, trans->paths + path2);
