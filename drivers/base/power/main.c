46d6456e05e20e443d763daba15818b64d4c4eb5
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2025-03-13 17:00:00 +0100

    PM: sleep: Fix handling devices with direct_complete set on errors
    [ Upstream commit 03f1444016b71feffa1dfb8a51f15ba592f94b13 ]
    
    When dpm_suspend() fails, some devices with power.direct_complete set
    may not have been handled by device_suspend() yet, so runtime PM has
    not been disabled for them yet even though power.direct_complete is set.
    
    Since device_resume() expects that runtime PM has been disabled for all
    devices with power.direct_complete set, it will attempt to reenable
    runtime PM for the devices that have not been processed by device_suspend()
    which does not make sense.  Had those devices had runtime PM disabled
    before device_suspend() had run, device_resume() would have inadvertently
    enable runtime PM for them, but this is not expected to happen because
    it would require ->prepare() callbacks to return positive values for
    devices with runtime PM disabled, which would be invalid.
    
    In practice, this issue is most likely benign because pm_runtime_enable()
    will not allow the "disable depth" counter to underflow, but it causes a
    warning message to be printed for each affected device.
    
    To allow device_resume() to distinguish the "direct complete" devices
    that have been processed by device_suspend() from those which have not
    been handled by it, make device_suspend() set power.is_suspended for
    "direct complete" devices.
    
    Next, move the power.is_suspended check in device_resume() before the
    power.direct_complete check in it to make it skip the "direct complete"
    devices that have not been handled by device_suspend().
    
    This change is based on a preliminary patch from Saravana Kannan.
    
    Fixes: aae4518b3124 ("PM / sleep: Mechanism to avoid resuming runtime-suspended devices unnecessarily")
    Link: https://lore.kernel.org/linux-pm/20241114220921.2529905-2-saravanak@google.com/
    Reported-by: Saravana Kannan <saravanak@google.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Reviewed-by: Saravana Kannan <saravanak@google.com>
    Link: https://patch.msgid.link/12627587.O9o76ZdvQC@rjwysocki.net
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -929,6 +929,9 @@ static void device_resume(struct device *dev, pm_message_t state, bool async)
 	if (dev->power.syscore)
 		goto Complete;
 
+	if (!dev->power.is_suspended)
+		goto Complete;
+
 	if (dev->power.direct_complete) {
 		/* Match the pm_runtime_disable() in device_suspend(). */
 		pm_runtime_enable(dev);
@@ -946,9 +949,6 @@ static void device_resume(struct device *dev, pm_message_t state, bool async)
 	 * a resumed device, even if the device hasn't been completed yet.
 	 */
 	dev->power.is_prepared = false;
-
-	if (!dev->power.is_suspended)
-		goto Unlock;
 
 	if (dev->pm_domain) {
 		info = "power domain ";
@@ -989,7 +989,6 @@ static void device_resume(struct device *dev, pm_message_t state, bool async)
 	error = dpm_run_callback(callback, dev, state, info);
 	dev->power.is_suspended = false;
 
- Unlock:
 	device_unlock(dev);
 	dpm_watchdog_clear(&wd);
 
@@ -1649,6 +1648,7 @@ static int device_suspend(struct device *dev, pm_message_t state, bool async)
 			pm_runtime_disable(dev);
 			if (pm_runtime_status_suspended(dev)) {
 				pm_dev_dbg(dev, state, "direct-complete ");
+				dev->power.is_suspended = true;
 				goto Complete;
 			}
 
