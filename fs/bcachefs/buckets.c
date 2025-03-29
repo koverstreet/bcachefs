028abafbe4a7a98adde846b5df820d9bad7e4a83
Kent Overstreet <kent.overstreet@linux.dev>
2025-03-29 14:39:24 -0400

    fixup! bcachefs: Better printing of inconsistency errors

--- fs/bcachefs/buckets.c
+++ fs/bcachefs/buckets.c
@@ -413,7 +413,7 @@ static int bucket_ref_update_err(struct btree_trans *trans, struct printbuf *buf
 {
 	struct bch_fs *c = trans->c;
 
-	prt_printf(buf, "\nwhile marking ");
+	prt_printf(buf, "\nwhile marking %s ", insert ? "insert" : "delete");
 	bch2_bkey_val_to_text(buf, c, k);
 	prt_newline(buf);
 
@@ -450,6 +450,7 @@ int bch2_bucket_ref_update(struct btree_trans *trans, struct bch_dev *ca,
 
 	if (unlikely(gen_after(ptr->gen, b_gen))) {
 		bch2_log_msg_start(c, &buf);
+		prt_printf(&buf, "%s -> %s\n", trans->fn, __func__);
 		prt_printf(&buf,
 			"bucket %u:%zu gen %u data type %s: ptr gen %u newer than bucket gen",
 			ptr->dev, bucket_nr, b_gen,
@@ -463,6 +464,7 @@ int bch2_bucket_ref_update(struct btree_trans *trans, struct bch_dev *ca,
 
 	if (unlikely(gen_cmp(b_gen, ptr->gen) > BUCKET_GC_GEN_MAX)) {
 		bch2_log_msg_start(c, &buf);
+		prt_printf(&buf, "%s -> %s\n", trans->fn, __func__);
 		prt_printf(&buf,
 			"bucket %u:%zu gen %u data type %s: ptr gen %u too stale",
 			ptr->dev, bucket_nr, b_gen,
@@ -481,6 +483,7 @@ int bch2_bucket_ref_update(struct btree_trans *trans, struct bch_dev *ca,
 
 	if (unlikely(b_gen != ptr->gen)) {
 		bch2_log_msg_start(c, &buf);
+		prt_printf(&buf, "%s -> %s\n", trans->fn, __func__);
 		prt_printf(&buf,
 			"bucket %u:%zu gen %u (mem gen %u) data type %s: stale dirty ptr (gen %u)",
 			ptr->dev, bucket_nr, b_gen,
@@ -495,6 +498,7 @@ int bch2_bucket_ref_update(struct btree_trans *trans, struct bch_dev *ca,
 
 	if (unlikely(bucket_data_type_mismatch(bucket_data_type, ptr_data_type))) {
 		bch2_log_msg_start(c, &buf);
+		prt_printf(&buf, "%s -> %s\n", trans->fn, __func__);
 		prt_printf(&buf, "bucket %u:%zu gen %u different types of data in same bucket: %s, %s",
 			   ptr->dev, bucket_nr, b_gen,
 			   bch2_data_type_str(bucket_data_type),
@@ -507,6 +511,7 @@ int bch2_bucket_ref_update(struct btree_trans *trans, struct bch_dev *ca,
 
 	if (unlikely((u64) *bucket_sectors + sectors > U32_MAX)) {
 		bch2_log_msg_start(c, &buf);
+		prt_printf(&buf, "%s -> %s\n", trans->fn, __func__);
 		prt_printf(&buf,
 			"bucket %u:%zu gen %u data type %s sector count overflow: %u + %lli > U32_MAX",
 			ptr->dev, bucket_nr, b_gen,
