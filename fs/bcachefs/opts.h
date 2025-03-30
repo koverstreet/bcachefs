180276e8939a92d023f63964933b3866aa3639a6
Kent Overstreet <kent.overstreet@linux.dev>
2025-03-30 19:16:45 -0400

    make more opts persistent WIP

--- fs/bcachefs/opts.h
+++ fs/bcachefs/opts.h
@@ -514,7 +514,7 @@ enum fsck_err_opts {
 	x(rebalance_enabled,		u8,				\
 	  OPT_FS|OPT_MOUNT|OPT_RUNTIME,					\
 	  OPT_BOOL(),							\
-	  BCH2_NO_SB_OPT,			true,			\
+	  BCH2_SB_REBALANCE_ENABLED,		true,			\
 	  NULL,		"Enable rebalance: disable for debugging, or to\n"\
 			"quiet the system when doing performance testing\n")\
 	x(rebalance_on_ac_only,		u8,				\
