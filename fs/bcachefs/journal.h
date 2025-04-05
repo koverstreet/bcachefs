db5797bae98164e7c3c566eff0e2d8b1a3cd1432
Kent Overstreet <kent.overstreet@linux.dev>
2025-04-05 19:30:43 -0400

    bcachefs: Move various init code to _init_early()
    _init_early() is for initialization that cannot fail, and often must
    happen for teardown partway through initialization to work.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/journal.h
+++ fs/bcachefs/journal.h
@@ -463,6 +463,7 @@ int bch2_fs_journal_start(struct journal *, u64);
 void bch2_dev_journal_exit(struct bch_dev *);
 int bch2_dev_journal_init(struct bch_dev *, struct bch_sb *);
 void bch2_fs_journal_exit(struct journal *);
+void bch2_fs_journal_init_early(struct journal *);
 int bch2_fs_journal_init(struct journal *);
 
 #endif /* _BCACHEFS_JOURNAL_H */
