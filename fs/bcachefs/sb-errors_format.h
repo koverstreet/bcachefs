dc52625cb8b6c76e4d7b574a6df3495f8c5200e8
Kent Overstreet <kent.overstreet@linux.dev>
2025-05-01 15:14:04 -0400

    bcachefs: BCH_FSCK_ERR_snapshot_key_missing_inode_snapshot
    We're going to be doing some snapshot deletion performance improvements,
    and those will strictly require that if an extent/dirent/xattr is
    present, an inode is present in that snapshot ID.
    
    We already check for this, but we don't repair it on disk: this patch
    adds that repair and turns it into a real fsck_err().
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/sb-errors_format.h
+++ fs/bcachefs/sb-errors_format.h
@@ -216,6 +216,7 @@ enum bch_fsck_flags {
 	x(inode_str_hash_invalid,				194,	0)		\
 	x(inode_v3_fields_start_bad,				195,	0)		\
 	x(inode_snapshot_mismatch,				196,	0)		\
+	x(snapshot_key_missing_inode_snapshot,			314,	0)		\
 	x(inode_unlinked_but_clean,				197,	0)		\
 	x(inode_unlinked_but_nlink_nonzero,			198,	0)		\
 	x(inode_unlinked_and_not_open,				281,	0)		\
@@ -323,7 +324,7 @@ enum bch_fsck_flags {
 	x(dirent_stray_data_after_cf_name,			305,	0)		\
 	x(rebalance_work_incorrectly_set,			309,	FSCK_AUTOFIX)	\
 	x(rebalance_work_incorrectly_unset,			310,	FSCK_AUTOFIX)	\
-	x(MAX,							314,	0)
+	x(MAX,							315,	0)
 
 enum bch_sb_error_id {
 #define x(t, n, ...) BCH_FSCK_ERR_##t = n,
