6927c6624c4738b3e365e2eb7a7980a00c07cc65
Kent Overstreet <kent.overstreet@linux.dev>
2025-05-02 12:37:36 -0400

    bcachefs: bcachefs_metadata_version_snapshot_deletion_v2
    We're going to be speeding up snapshot deletion, by only having it
    process the extents/dirents/xattrs btrees if an inode of a given
    snapshot ID was present.
    
    This raises the possibility of 'bkey_in_missing_snapshot' errors popping
    up, if we ever accidentally don't do the corresponding inode update, or
    if the new algorithm has bugs.
    
    So instead of deleting snapshot IDs, add a new deleted flag, so that
    'key in missing snapshot' errors can more definitively tell what
    happened and automatically repair.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/sb-errors_format.h
+++ fs/bcachefs/sb-errors_format.h
@@ -209,6 +209,7 @@ enum bch_fsck_flags {
 	x(subvol_to_missing_root,				188,	0)		\
 	x(subvol_root_wrong_bi_subvol,				189,	FSCK_AUTOFIX)	\
 	x(bkey_in_missing_snapshot,				190,	0)		\
+	x(bkey_in_deleted_snapshot,				315,	0)		\
 	x(inode_pos_inode_nonzero,				191,	0)		\
 	x(inode_pos_blockdev_range,				192,	0)		\
 	x(inode_alloc_cursor_inode_bad,				301,	0)		\
@@ -324,7 +325,7 @@ enum bch_fsck_flags {
 	x(dirent_stray_data_after_cf_name,			305,	0)		\
 	x(rebalance_work_incorrectly_set,			309,	FSCK_AUTOFIX)	\
 	x(rebalance_work_incorrectly_unset,			310,	FSCK_AUTOFIX)	\
-	x(MAX,							315,	0)
+	x(MAX,							316,	0)
 
 enum bch_sb_error_id {
 #define x(t, n, ...) BCH_FSCK_ERR_##t = n,
