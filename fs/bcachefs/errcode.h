a458c1d813e74a94def231e6e5f8287d683f83bf
Kent Overstreet <kent.overstreet@linux.dev>
2025-06-02 17:43:36 -0400

    bcachefs: Run may_delete_deleted_inode() checks in bch2_inode_rm()
    We had a bug where bch2_evict_inode() incorrectly called bch2_inode_rm()
    - the journal clearly showed the inode was not unlinked.
    
    We've got checks that we use in recovery when cleaning up deleted
    inodes, lift them to bch2_inode_rm() as well.
    
    (cherry picked from commit 09fb85ae565645b982e9030dbb2ff6707f2cdddc)
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/errcode.h
+++ fs/bcachefs/errcode.h
@@ -209,6 +209,8 @@
 	x(EINVAL,			remove_would_lose_data)			\
 	x(EINVAL,			no_resize_with_buckets_nouse)		\
 	x(EINVAL,			inode_unpack_error)			\
+	x(EINVAL,			inode_not_unlinked)			\
+	x(EINVAL,			inode_has_child_snapshot)		\
 	x(EINVAL,			varint_decode_error)			\
 	x(EINVAL,			erasure_coding_found_btree_node)	\
 	x(EOPNOTSUPP,			may_not_use_incompat_feature)		\
