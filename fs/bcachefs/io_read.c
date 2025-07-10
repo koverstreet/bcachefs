688ce5302ae9e6d283ac661081b270f198ba1e01
Kent Overstreet <kent.overstreet@linux.dev>
2025-07-10 16:27:29 -0400

    bcachefs: io_read: remove from async obj list in rbio_done()
    Previously, only split rbios allocated in io_read.c would be removed
    from the async obj list.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/io_read.c
+++ fs/bcachefs/io_read.c
@@ -465,6 +465,10 @@ static void bch2_rbio_done(struct bch_read_bio *rbio)
 	if (rbio->start_time)
 		bch2_time_stats_update(&rbio->c->times[BCH_TIME_data_read],
 				       rbio->start_time);
+#ifdef CONFIG_BCACHEFS_ASYNC_OBJECT_LISTS
+	if (rbio->list_idx)
+		async_object_list_del(rbio->c, rbio, rbio->list_idx);
+#endif
 	bio_endio(&rbio->bio);
 }
 
