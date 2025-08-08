4b1a5ac608877023466ac953d5f45d563372ee6f
Kent Overstreet <kent.overstreet@linux.dev>
2025-08-07 20:07:11 -0400

    bcachefs: Never run check_backpointers_to_extents by default anymore
    It's not needed anymore, we check backpointers as we use them, and the
    performance overhead is too high for modern filesystems.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/recovery.c
+++ fs/bcachefs/recovery.c
@@ -64,7 +64,6 @@ int bch2_btree_lost_data(struct bch_fs *c,
 	 * but in debug mode we want the next fsck run to be clean:
 	 */
 	ret = __bch2_run_explicit_recovery_pass(c, msg, BCH_RECOVERY_PASS_check_lrus, 0, &write_sb) ?: ret;
-	ret = __bch2_run_explicit_recovery_pass(c, msg, BCH_RECOVERY_PASS_check_backpointers_to_extents, 0, &write_sb) ?: ret;
 #endif
 
 	write_sb |= !__test_and_set_bit_le64(BCH_FSCK_ERR_lru_entry_bad, ext->errors_silent);
