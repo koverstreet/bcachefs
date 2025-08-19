642632db437389237cc5c9e28fd234d3d59046d6
Kent Overstreet <kent.overstreet@linux.dev>
2025-08-19 17:21:39 -0400

    bcachefs: Rename, split out bch2_extent_get_io_opts()
    Move to rebalance.c, where it more properly belongs, and split out
    getting an extent's io options from re-propagating back to the extent -
    prep work for new fsck code.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/rebalance.h
+++ fs/bcachefs/rebalance.h
@@ -41,9 +41,40 @@ enum set_needs_rebalance_ctx {
 int bch2_bkey_set_needs_rebalance(struct bch_fs *, struct bch_inode_opts *,
 				  struct bkey_i *, enum set_needs_rebalance_ctx, u32);
 
-int bch2_get_update_rebalance_opts(struct btree_trans *, struct bch_inode_opts *,
-				   struct btree_iter *, struct bkey_s_c,
-				   enum set_needs_rebalance_ctx);
+/* Inodes in different snapshots may have different IO options: */
+struct snapshot_io_opts_entry {
+	u32			snapshot;
+	struct bch_inode_opts	io_opts;
+};
+
+struct per_snapshot_io_opts {
+	u64			cur_inum;
+	struct bch_inode_opts	fs_io_opts;
+	DARRAY(struct snapshot_io_opts_entry) d;
+};
+
+static inline void per_snapshot_io_opts_init(struct per_snapshot_io_opts *io_opts, struct bch_fs *c)
+{
+	memset(io_opts, 0, sizeof(*io_opts));
+	bch2_inode_opts_get(c, &io_opts->fs_io_opts);
+}
+
+static inline void per_snapshot_io_opts_exit(struct per_snapshot_io_opts *io_opts)
+{
+	darray_exit(&io_opts->d);
+}
+
+struct bch_inode_opts *bch2_extent_get_apply_io_opts(struct btree_trans *,
+			  struct per_snapshot_io_opts *, struct bpos,
+			  struct btree_iter *, struct bkey_s_c,
+			  enum set_needs_rebalance_ctx);
+
+int bch2_extent_get_io_opts_one(struct btree_trans *, struct bch_inode_opts *,
+				struct btree_iter *, struct bkey_s_c,
+				enum set_needs_rebalance_ctx);
+int bch2_extent_get_apply_io_opts_one(struct btree_trans *, struct bch_inode_opts *,
+				      struct btree_iter *, struct bkey_s_c,
+				      enum set_needs_rebalance_ctx);
 
 int bch2_set_rebalance_needs_scan_trans(struct btree_trans *, u64);
 int bch2_set_rebalance_needs_scan(struct bch_fs *, u64 inum);
