977aa38ab36c721273cacc4bfd6595f3f5496b81
Kent Overstreet <kent.overstreet@linux.dev>
2025-08-23 20:05:08 -0400

    bcachefs: bch2_set_rebalance_needs_scan_device()
    Rebalance can now evacuate devices in response to state changes.
    
    This obsoletes BCH_DATA_OP_migrate; setting a device to
    BCH_MEMBER_STATE_failed (perhaps we should rename this) will cause it to
    be evacuated (and the evacuate will resume if e.g. we crash or shutdown
    and restart).
    
    Additionally, we'll now be able to automatically evacuate failing
    devices. Currently we only set devices read-only in response to IO
    errors; we'll need to add configuration/policy/good heuristics (and
    clearly document them) for deciding when a device is failing and should
    be evacuated.
    
    This works with rebalance scan cookies; these are currently used to
    respond to filesystem/inode option changes. Cookies in the range of
    1-4095 now refer to devices; when rebalance sees one of those it will
    walk backpointers on that device and update bch_extent_rebalance, which
    will react to the new device state (or durability setting change).
    
    Performance implications: with BCH_DATA_OP_migrate, we walk backpointers
    and do the data moves directly, meaning they happen in device LBA order.
    
    However, by walking backpointers to queue up rebalance work entries and
    then doing the work from the rebalance_work btree, we'll do the data
    moves in logical key order.
    
    Pro: doing data moves in logical key order will help with
    fragmentation/data locality: extents from the same inode will be moved
    at the same time, we'll get a bit of defragmentation and do better at
    keeping related data together
    
    Con: reads from the device being evacuated will no longer be sequential,
    this will hurt performance on spinning rust.
    
    Perhaps add a mode where we kick off data moves from
    do_rebalance_scan_bp()? Would be pretty easy
    
    XXX: slurp backpointers into a darray and sort before processing extents
    in do_rebalance_scan_device: we recently saw a very slow evacuate that
    was mostly just dropping cached data, on a huge filesystem entirely on
    spinning rust with only 8GB of ram in the server - the backpointers ->
    extents lookups are fairly random, batching + sorting will greatly
    improve performance
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/data/rebalance.c
+++ fs/bcachefs/data/rebalance.c
@@ -3,6 +3,7 @@
 #include "bcachefs.h"
 
 #include "alloc/background.h"
+#include "alloc/backpointers.h"
 #include "alloc/buckets.h"
 #include "alloc/disk_groups.h"
 #include "alloc/foreground.h"
@@ -267,6 +268,29 @@ static int check_rebalance_scan_cookie(struct btree_trans *trans, u64 inum, bool
 	return ret;
 }
 
+static int check_dev_rebalance_scan_cookie(struct btree_trans *trans, struct bkey_s_c k,
+					   struct bch_devs_mask *v)
+{
+	struct bkey_ptrs_c ptrs = bch2_bkey_ptrs_c(k);
+
+	bkey_for_each_ptr(ptrs, ptr)
+		if (v && test_bit(ptr->dev, v->d))
+			return 1;
+
+	bkey_for_each_ptr(ptrs, ptr) {
+		int ret = check_rebalance_scan_cookie(trans, ptr->dev + 1, NULL);
+		if (ret < 0)
+			return ret;
+		if (ret) {
+			if (v)
+				__set_bit(ptr->dev, v->d);
+			return ret;
+		}
+	}
+
+	return 0;
+}
+
 static int new_needs_rb_allowed(struct btree_trans *trans,
 				struct per_snapshot_io_opts *s,
 				struct bkey_s_c k,
@@ -327,6 +351,12 @@ static int new_needs_rb_allowed(struct btree_trans *trans,
 		  check_rebalance_scan_cookie(trans, k.k->p.inode,	s ? &s->inum_scan_cookie : NULL);
 	if (ret)
 		return min(ret, 0);
+
+	if (new_need_rb == BIT(BCH_REBALANCE_data_replicas)) {
+		ret = check_dev_rebalance_scan_cookie(trans, k, s ? &s->dev_cookie : NULL);
+		if (ret)
+			return min(ret, 0);
+	}
 
 	CLASS(printbuf, buf)();
 
@@ -553,6 +583,11 @@ int bch2_set_rebalance_needs_scan(struct bch_fs *c, u64 inum)
 			    bch2_set_rebalance_needs_scan_trans(trans, inum));
 }
 
+int bch2_set_rebalance_needs_scan_device(struct bch_fs *c, unsigned dev)
+{
+	return bch2_set_rebalance_needs_scan(c, dev + 1);
+}
+
 int bch2_set_fs_needs_rebalance(struct bch_fs *c)
 {
 	return bch2_set_rebalance_needs_scan(c, 0);
@@ -711,6 +746,32 @@ static int do_rebalance_extent(struct moving_context *ctxt,
 	return 0;
 }
 
+static int do_rebalance_scan_bp(struct btree_trans *trans,
+				struct bkey_s_c_backpointer bp,
+				struct wb_maybe_flush *last_flushed)
+{
+	struct bch_fs *c = trans->c;
+	struct bch_fs_rebalance *r = &c->rebalance;
+
+	if (bp.v->level) /* metadata not supported yet */
+		return 0;
+
+	CLASS(btree_iter_uninit, iter)(trans);
+	struct bkey_s_c k = bkey_try(bch2_backpointer_get_key(trans, bp, &iter, BTREE_ITER_intent,
+							      last_flushed));
+	if (!k.k)
+		return 0;
+
+	atomic64_add(!bp.v->level ? k.k->size : c->opts.btree_node_size >> 9,
+		     &r->scan_stats.sectors_seen);
+
+	struct bch_inode_opts opts;
+	try(bch2_bkey_get_io_opts(trans, NULL, k, &opts));
+
+	return bch2_update_rebalance_opts(trans, NULL, &opts, &iter, k,
+					  SET_NEEDS_REBALANCE_opt_change);
+}
+
 static int do_rebalance_scan_indirect(struct btree_trans *trans,
 				      struct bkey_s_c_reflink_p p,
 				      struct per_snapshot_io_opts *snapshot_io_opts,
@@ -788,6 +849,7 @@ static int do_rebalance_scan(struct moving_context *ctxt,
 	r->state = BCH_REBALANCE_scanning;
 
 	if (!inum) {
+		/* Filesystem: */
 		r->scan_start	= BBPOS_MIN;
 		r->scan_end	= BBPOS_MAX;
 
@@ -799,7 +861,30 @@ static int do_rebalance_scan(struct moving_context *ctxt,
 			try(do_rebalance_scan_btree(ctxt, snapshot_io_opts, btree, 0,
 						    POS_MIN, SPOS_MAX));
 		}
+	} else if (inum < BCACHEFS_ROOT_INO) {
+		/* Device: */
+		unsigned dev = inum - 1;
+		r->scan_start	= BBPOS(BTREE_ID_backpointers, POS(dev, 0));
+		r->scan_end	= BBPOS(BTREE_ID_backpointers, POS(dev, U64_MAX));
+
+		struct wb_maybe_flush last_flushed __cleanup(wb_maybe_flush_exit);
+		wb_maybe_flush_init(&last_flushed);
+
+		bch2_btree_write_buffer_flush_sync(trans);
+
+		try(for_each_btree_key_max_commit(trans, iter, BTREE_ID_backpointers,
+						  POS(dev, 0), POS(dev, U64_MAX),
+						  BTREE_ITER_prefetch, k,
+						  NULL, NULL, BCH_TRANS_COMMIT_no_enospc, ({
+			ctxt->stats->pos = BBPOS(iter.btree_id, iter.pos);
+
+			if (k.k->type != KEY_TYPE_backpointer)
+				continue;
+
+			do_rebalance_scan_bp(trans, bkey_s_c_to_backpointer(k), &last_flushed);
+		})));
 	} else {
+		/* Inode: */
 		r->scan_start	= BBPOS(BTREE_ID_extents, POS(inum, 0));
 		r->scan_end	= BBPOS(BTREE_ID_extents, POS(inum, U64_MAX));
 
