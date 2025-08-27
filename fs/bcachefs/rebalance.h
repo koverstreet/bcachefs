7771ebd9470dab0e2a4c1b27147b28e05f65ad51
Kent Overstreet <kent.overstreet@linux.dev>
2025-08-27 14:27:16 -0400

    bcachefs: bch2_extent_rebalance_to_text() -> rebalance.c
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/rebalance.h
+++ fs/bcachefs/rebalance.h
@@ -26,6 +26,9 @@ static inline struct bch_extent_rebalance io_opts_to_rebalance_opts(struct bch_f
 	return r;
 };
 
+void bch2_extent_rebalance_to_text(struct printbuf *, struct bch_fs *,
+				   const struct bch_extent_rebalance *);
+
 u64 bch2_bkey_sectors_need_rebalance(struct bch_fs *, struct bkey_s_c);
 int bch2_bkey_set_needs_rebalance(struct bch_fs *, struct bch_io_opts *, struct bkey_i *);
 int bch2_get_update_rebalance_opts(struct btree_trans *,
