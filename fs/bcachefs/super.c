3500f07c65855d283b864658b3f4f8da16c151c6
Kent Overstreet <kent.overstreet@linux.dev>
2025-09-02 13:22:46 -0400

    bcachefs: include stacktrace on ERO
    We have an ERO with insufficient context we're trying to debug, and EROs
    should be rare enough that perhaps this is a good idea.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/super.c
+++ fs/bcachefs/super.c
@@ -461,9 +461,11 @@ static bool __bch2_fs_emergency_read_only2(struct bch_fs *c, struct printbuf *ou
 	bch2_fs_read_only_async(c);
 	wake_up(&bch2_read_only_wait);
 
-	if (ret)
+	if (ret) {
 		prt_printf(out, "emergency read only at seq %llu\n",
 			   journal_cur_seq(&c->journal));
+		bch2_prt_task_backtrace(out, current, 2, out->atomic ? GFP_ATOMIC : GFP_KERNEL);
+	}
 
 	return ret;
 }
