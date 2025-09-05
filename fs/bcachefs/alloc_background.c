a00ff8a4f053003a18f45a3643de3280206be9d4
Kent Overstreet <kent.overstreet@linux.dev>
2025-09-05 16:58:00 -0400

    bcachefs: bch2_dev_remove_lrus()
    Fix a device removal bug - the deletion of LRU entries was completely
    wrong.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/alloc_background.c
+++ fs/bcachefs/alloc_background.c
@@ -2384,8 +2384,7 @@ int bch2_dev_remove_alloc(struct bch_fs *c, struct bch_dev *ca)
 	 * We clear the LRU and need_discard btrees first so that we don't race
 	 * with bch2_do_invalidates() and bch2_do_discards()
 	 */
-	ret =   bch2_btree_delete_range(c, BTREE_ID_lru, start, end,
-					BTREE_TRIGGER_norun, NULL) ?:
+	ret =   bch2_dev_remove_lrus(c, ca) ?:
 		bch2_btree_delete_range(c, BTREE_ID_need_discard, start, end,
 					BTREE_TRIGGER_norun, NULL) ?:
 		bch2_btree_delete_range(c, BTREE_ID_freespace, start, end,
