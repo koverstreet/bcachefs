aed4fab12bad84894df626b9255385ac6e7545e1
Kent Overstreet <kent.overstreet@linux.dev>
2025-10-21 11:49:29 -0400

    bcachefs: BCH_FSCK_ERR_validate_error_in_commit
    Log these errors in the superblock.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/sb/errors_format.h
+++ fs/bcachefs/sb/errors_format.h
@@ -340,7 +340,8 @@ enum bch_fsck_flags {
 	x(dirent_stray_data_after_cf_name,			305,	0)		\
 	x(rebalance_work_incorrectly_set,			309,	FSCK_AUTOFIX)	\
 	x(rebalance_work_incorrectly_unset,			310,	FSCK_AUTOFIX)	\
-	x(MAX,							329,	0)
+	x(validate_error_in_commit,				329,	0)		\
+	x(MAX,							330,	0)
 
 enum bch_sb_error_id {
 #define x(t, n, ...) BCH_FSCK_ERR_##t = n,
