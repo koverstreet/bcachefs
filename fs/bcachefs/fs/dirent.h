0c01dbb69a9ce2bd977705fc0de81c5acca973b8
Kent Overstreet <kent.overstreet@linux.dev>
2025-10-24 04:44:12 -0400

    bcachefs: kill redundant trans_iter_exit() in dirent_get_by_pos()
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/fs/dirent.h
+++ fs/bcachefs/fs/dirent.h
@@ -62,10 +62,7 @@ static inline struct bkey_s_c_dirent dirent_get_by_pos(struct btree_trans *trans
 						struct bpos pos)
 {
 	bch2_trans_iter_init(trans, iter, BTREE_ID_dirents, pos, 0);
-	struct bkey_s_c_dirent d = bch2_bkey_get_typed(iter, dirent);
-	if (bkey_err(d.s_c))
-		bch2_trans_iter_exit(iter);
-	return d;
+	return bch2_bkey_get_typed(iter, dirent);
 }
 
 int bch2_dirent_read_target(struct btree_trans *, subvol_inum,
