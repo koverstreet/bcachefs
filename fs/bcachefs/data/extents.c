7bacc80108a22ec91e2596c5223e2caa1ec15ff9
Kent Overstreet <kent.overstreet@linux.dev>
2025-11-12 18:06:45 -0500

    fixup! bcachefs: improve indentation of multi line bkeys

--- fs/bcachefs/data/extents.c
+++ fs/bcachefs/data/extents.c
@@ -1397,7 +1397,6 @@ void bch2_bkey_ptrs_to_text(struct printbuf *out, struct bch_fs *c,
 	if (c)
 		prt_printf(out, "durability: %u ", bch2_bkey_durability_safe(c, k));
 
-	prt_newline(out);
 	guard(printbuf_atomic)(out);
 	guard(rcu)();
 
