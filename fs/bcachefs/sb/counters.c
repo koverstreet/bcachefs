d3c5001430d65f33ccc5d08a68b451909e59fccd
Kent Overstreet <kent.overstreet@linux.dev>
2025-11-22 11:48:41 -0500

    bcachefs: Track recent counter activity, print on srcu warning
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/sb/counters.c
+++ fs/bcachefs/sb/counters.c
@@ -92,8 +92,50 @@ int bch2_sb_counters_from_cpu(struct bch_fs *c)
 	return 0;
 }
 
+static void bch2_sb_counters_work(struct work_struct *work)
+{
+	struct bch_fs_counters *c = container_of(work, struct bch_fs_counters, work.work);
+
+	memmove((void *) c->recent + sizeof(u64) * BCH_COUNTER_NR,
+		c->recent,
+		sizeof(u64) *
+		BCH_COUNTER_NR *
+		(NR_RECENT_COUNTERS - 1));
+
+	for (unsigned i = 0; i < BCH_COUNTER_NR; i++)
+		c->recent[0][i] = percpu_u64_get(&c->now[i]);
+
+	queue_delayed_work(system_unbound_wq, &c->work, HZ / 2);
+}
+
+void bch2_sb_recent_counters_to_text(struct printbuf *out, struct bch_fs_counters *c)
+{
+	unsigned long active[BITS_TO_LONGS(BCH_COUNTER_NR)];
+	memset(active, 0, sizeof(active));
+
+	for (unsigned i = 0; i < BCH_COUNTER_NR; i++)
+		if (c->recent[NR_RECENT_COUNTERS - 1][i] != percpu_u64_get(&c->now[i]))
+			__set_bit(i, active);
+
+	for (unsigned i = 0; i < BCH_COUNTER_NR; i++) {
+		if (!test_bit(i, active))
+			continue;
+
+		prt_printf(out, "%s:", bch2_counter_names[i]);
+
+		u64 prev = percpu_u64_get(&c->now[i]);
+
+		for (unsigned j = 0; j < NR_RECENT_COUNTERS; j++) {
+			prt_printf(out, "\t%llu", prev - c->recent[j][i]);
+			prev = c->recent[j][i];
+		}
+		prt_newline(out);
+	}
+}
+
 void bch2_fs_counters_exit(struct bch_fs *c)
 {
+	cancel_delayed_work_sync(&c->counters.work);
 	free_percpu(c->counters.now);
 }
 
@@ -103,7 +145,11 @@ int bch2_fs_counters_init(struct bch_fs *c)
 	if (!c->counters.now)
 		return -BCH_ERR_ENOMEM_fs_counters_init;
 
-	return bch2_sb_counters_to_cpu(c);
+	try(bch2_sb_counters_to_cpu(c));
+
+	INIT_DELAYED_WORK(&c->counters.work, bch2_sb_counters_work);
+	queue_delayed_work(system_unbound_wq, &c->counters.work, HZ / 2);
+	return 0;
 }
 
 const struct bch_sb_field_ops bch_sb_field_ops_counters = {
