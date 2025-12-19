bf6d694cdd99939d667f6e81bbf2dff86c12e1ba
Kent Overstreet <kent.overstreet@linux.dev>
2025-12-19 13:31:26 -0500

    bcachefs: subvol_to_missing_root autofix
    We recreate missing root inodes, this is a reasonably safe repair path.
    
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>

--- fs/bcachefs/sb/errors_format.h
+++ fs/bcachefs/sb/errors_format.h
@@ -218,7 +218,7 @@ enum bch_fsck_flags {
 	x(snapshot_bad_skiplist,				185,	FSCK_AUTOFIX)	\
 	x(subvol_pos_bad,					186,	0)		\
 	x(subvol_not_master_and_not_snapshot,			187,	FSCK_AUTOFIX)	\
-	x(subvol_to_missing_root,				188,	0)		\
+	x(subvol_to_missing_root,				188,	FSCK_AUTOFIX)	\
 	x(subvol_root_wrong_bi_subvol,				189,	FSCK_AUTOFIX)	\
 	x(bkey_in_missing_snapshot,				190,	0)		\
 	x(bkey_in_deleted_snapshot,				315,	FSCK_AUTOFIX)	\
