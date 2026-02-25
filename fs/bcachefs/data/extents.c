ff0d94a8454b05876d5616ca427faee3db3bce48
Jul Lang <jullanggit@proton.me>
2026-02-25 23:25:28 +0100

    bcachefs: shrink: drop cached data after shrink cutoff

--- fs/bcachefs/data/extents.c
+++ fs/bcachefs/data/extents.c
@@ -1349,6 +1349,8 @@ static bool maybe_drop_cached_ptr(struct bch_fs *c, struct bch_inode_opts *opts,
 			return drop_cached_pointer_trace(c, k, ptr, "pointer is stale");
 		if (!ca || ca->mi.state == BCH_MEMBER_STATE_evacuating)
 			return drop_cached_pointer_trace(c, k, ptr, "device bad or evacuating");
+		if (ca->mi.target_nbuckets && ca->mi.target_nbuckets <= sector_to_bucket(ca, ptr->offset))
+			return drop_cached_pointer_trace(c, k, ptr, "past shrink cutoff");
 
 		unsigned target = opts->promote_target ?: opts->foreground_target;
 		if (target && !bch2_dev_in_target_rcu(c, ptr->dev, target))
