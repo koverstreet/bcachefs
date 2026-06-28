248722f0e1925dbb877a3aef5813801d3a4aba27
Darafei Praliaskouski <me@komzpa.net>
2026-06-29 01:04:13 +0400

    bcachefs: fix offline write-device mask

--- fs/bcachefs/init/dev.c
+++ fs/bcachefs/init/dev.c
@@ -1271,7 +1271,7 @@ static int bch2_dev_may_offline(struct bch_fs *c, struct bch_dev *ca, int flags,
 	__clear_bit(ca->dev_idx, new_devs.d);
 
 	struct bch_devs_mask new_rw_devs = c->allocator.rw_devs[0];
-	__clear_bit(ca->dev_idx, new_devs.d);
+	__clear_bit(ca->dev_idx, new_rw_devs.d);
 
 	if (!bch2_can_read_fs_with_devs(c, &new_devs, flags, err) ||
 	    (!c->opts.read_only &&
